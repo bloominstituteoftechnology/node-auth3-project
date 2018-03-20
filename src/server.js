b89a2cbbda05534ce8063bbc0ebb2496c6046f33
Charlie Sparks <csparks@antioch.edu>
2018-03-20 14:35:59 -0700

    refactored middleware to make it more functional

--- src/server.js
+++ src/server.js
@@ -11,27 +11,51 @@ const server = express();
 server.use(bodyParser.json());
 
 const idMiddleware = (req, res, next) => {
-  const newSoID = req.params.soID;
-  if (!newSoID){
+  const questionId = req.params.soID;
+  if (!questionId){
     res.status(STATUS_USER_ERROR);
-    res.send(`There was an error`);
+    res.send(`No post ID was provided`);
     return;
   } else {
-    Post.find({soID: newSoID})
+    Post.find({soID: questionId})
     .then(response => {
-        if (!response[0]){
-         console.log("There was no post with that ID");
-         req.stat = 404;
-         req.foundPost = `There was no post found with that ID`;
-         next();
-      } else {
-        console.log(`The post was found`);
+      if (response){
         req.stat = 200;
-        req.foundPost = response[0];
-        next();  
-      }      
+        req.question = response[0];
+        next();
+      } else {
+        req.stat = 404;
+        req.question = "No post found with that ID";
+      }
+    })
+    .catch(err => {
+      req.stat = 500;
+      req.question = `There was a server error`;
+      next();
     })
   }
+
+  // const newSoID = req.params.soID;
+  // if (!newSoID){
+  //   res.status(STATUS_USER_ERROR);
+  //   res.send(`There was an error`);
+  //   return;
+  // } else {
+  //   Post.find({soID: newSoID})
+  //   .then(response => {
+  //       if (!response[0]){
+  //        console.log("There was no post with that ID");
+  //        req.stat = 404;
+  //        req.foundPost = `There was no post found with that ID`;
+  //        next();
+  //     } else {
+  //       console.log(`The post was found`);
+  //       req.stat = 200;
+  //       req.foundPost = response[0];
+  //       next();  
+  //     }      
+  //   })
+  // }
 }
 
 const sendUserError = (err, res) => {
@@ -54,24 +78,30 @@ const queryAndThen = (query, res, cb) => {
 };
 
 server.get('/accepted-answer/:soID', idMiddleware, (req, res) => {
-  const newPost = req.foundPost;
+  const newQuestion = req.question;
   const newStatus = req.stat;
-  const answerId = req.foundPost.acceptedAnswerID;
+  const answerId = newQuestion.acceptedAnswerID;
   
   if (!answerId){
     res.status(404);
     res.send(`No accepted answer found`);
     return;
-  } 
+  }
 
   Post.find({soID: answerId})
   .then(response => {
-    res.status(200);
-    res.json(response[0])
+    if (!response[0]){
+      res.status(404);
+      res.send(`No matching answer found`)
+    } else {
+      res.status(200);
+      res.json(response[0])
+    }
   })
 });
 
 server.get('/top-answer/:soID', idMiddleware, (req, res) => {
+
 });
 
 server.get('/popular-jquery-questions', (req, res) => {
