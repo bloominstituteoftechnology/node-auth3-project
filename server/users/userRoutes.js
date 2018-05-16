7b30fc27ecbd89e5be360e673b7a4d806da0f853
Luis Hernandez <luishrd@gmail.com>
2018-05-16 10:10:38 -0600

    adds starter code

---
+++ server/users/userRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+const User = require('./User');
+
+router.get('/', (req, res) => {
+  User.find()
+    .select('-password')
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
