303ec15ac4d072cd131d9aba226b11053996f42b
AmyShackles <amyshackles@gmail.com>
2018-06-13 15:07:44 -0700

    Login now works
    - Users who are logged in can see other users of their race (which sounds bad)
    - Login gives you a cookie
    - Register gives you a cookie
    - By cookie, I mean token.

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -1,11 +1,30 @@
 const router = require('express').Router();
-
+const secret = "IDun01Fw3C4NEvEnD0TH1ZButTHatz0K1H4V3MYH0M135"
 const User = require('./User');
+const jwt = require('jsonwebtoken');
 
-router.get('/', (req, res) => {
+function restricted (req, res, next) {
+  const token = req.headers.authorization;
+console.log('Look at me, being all restricted')
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      req.jwtPayload = decodedToken;
+      console.log('decodedtoken', decodedToken)
+      if (err) {
+        return res.status(401).json({ message: 'You shall not pass!  Your decoder ring has failed thee!'})
+      }
+      next();
+    })
+  } else {
+    res.status(401).json({ message: "You shall not pass!  Thou hast no token!"})
+  }
+}
+router.get('/', restricted, (req, res) => {
   User.find()
+    .where('race').equals(req.jwtPayload.race)
     .select('-password')
     .then(users => {
+      console.log('users', users)
       res.json(users);
     })
     .catch(err => {
