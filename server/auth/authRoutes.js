cb229e599634471158b30f02995fe9e768f85bca
Chris Honda <christopher.honda92@gmail.com>
2018-06-13 12:55:30 -0700

    Initial commit with JWT

--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-
+const jwt = require('jsonwebtoken');
 const User = require('../users/User');
+const secret = 'braden govi xang chris';
 
 router.post('/register', function(req, res) {
   User.create(req.body)
@@ -12,5 +13,44 @@ router.post('/register', function(req, res) {
     })
     .catch(err => res.status(500).json(err));
 });
+
+function generateToken(user) {
+  const options = {
+    expiresIn: '1hr',
+  };
+  const payload = {
+    name: user.username
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  User
+      .findOne({ username })
+        .then(user => {
+          if (user) {
+            user.validatePassword(password)
+              .then(passwordsMatch => {
+                if (passwordsMatch) {
+                  const token = generateToken(user);
+
+                  res.status(200).json({ message: `Hello there ${username}`}, token)
+                } else {
+                  res.status(401).send('Invalid credentials.');
+                }
+              })
+              .catch(error => {
+                res.send('Invalid credentials.');
+              });
+          } else {
+            res.status(401).send('Invalid credentials.')
+          }
+        })
+        .catch(error => {
+          res.send(error);
+        })
+})
 
 module.exports = router;
