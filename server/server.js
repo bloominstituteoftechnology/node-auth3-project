91f1d7be62d603d30d787165cfd0c4abaa3f604e
jesuarva <jean.ariza@jesuarva.com>
2018-06-13 21:58:02 +0200

    SET UP Dev Enviroment

--- server/server.js
+++ server/server.js
@@ -12,9 +12,7 @@ setupRoutes(server);
 db.connectTo('authii')
   .then(() => {
     console.log('\n... API Connected to authii Database ...\n');
-    server.listen(5500, () =>
-      console.log('\n=== API running on port 5500 ===\n')
-    );
+    server.listen(5500, () => console.log('\n=== API running on port 5500 ===\n'));
   })
   .catch(err => {
     console.log('\n*** ERROR Connecting to MongoDB, is it running? ***\n', err);
