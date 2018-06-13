3c96faecb764b2266d3f7253743caeb3443a5002
AmyShackles <amyshackles@gmail.com>
2018-06-13 12:37:57 -0700

    Added dependencies
    - Added a dependency for connect-mongo that I'm realizing now might be moot
    - Added a dependency for jsonwebtoken

--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -6,7 +6,6 @@ router.post('/register', function(req, res) {
   User.create(req.body)
     .then(({ username, race }) => {
       // we destructure the username and race to avoid returning the hashed password
-
       // then we assemble a new object and return it
       res.status(201).json({ username, race });
     })
