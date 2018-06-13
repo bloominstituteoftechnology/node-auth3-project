4388e4227060129f1c4d36d098694d9cf81b7e21
jesuarva <jean.ariza@jesuarva.com>
2018-06-14 00:09:38 +0200

    IMPLEMENT a Validator for 'Restricted' endpoints

--- server/_config/routes.js
+++ server/_config/routes.js
@@ -2,13 +2,18 @@ const userRoutes = require('../users/userRoutes');
 const authRoutes = require('../auth/authRoutes');
 const authLogin = require('../auth/loginRouter');
 
+const helpers = require('../tools/helperFunctions');
+
 module.exports = function(server) {
   // sanity check route
   server.get('/', function(req, res) {
     res.send({ api: 'up and running' });
   });
 
-  server.use('/api/users', userRoutes);
   server.use('/api/auth', authRoutes);
   server.use('/api/auth', authLogin);
+
+  // Use for 'restricted' endpoints the Token Validator middleware
+  server.use(helpers.userHasToken);
+  server.use('/api/users', userRoutes);
 };
--- server/tools/helperFunctions.js
+++ server/tools/helperFunctions.js
@@ -9,4 +9,14 @@ module.exports = {
     const payload = { ...data };
     return jwt.sign(payload, secrets.jsk_secret, options);
   },
+  userHasToken: function(req, res, next) {
+    const token = req.headers.authorization;
+    if (!token) res.status(401).json('You must be registered/logged in before go on');
+    jwt.verify(token, secrets.jsk_secret, (err, tokenDecoded) => {
+      req.token = token;
+      req.decodedToken = tokenDecoded;
+      // req.JWT(token);
+      err ? res.status(500).json({ Error: 'Oh ohh, We can not validate your credentials, try again!' }) : next();
+    });
+  },
 };
