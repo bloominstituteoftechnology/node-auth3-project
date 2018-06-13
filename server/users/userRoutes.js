6ce04b19917459850fda5b0432b9467a72aa0d91
A.D.Faris <adfaris@gmail.com>
2018-06-13 17:34:23 -0400

    AD Faris auth ii

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -1,16 +1,22 @@
-const router = require('express').Router();
+const router = require('express').Router()
 
-const User = require('./User');
+const User = require('./User')
 
-router.get('/', (req, res) => {
+router.route('/').get((req, res) => {
   User.find()
     .select('-password')
     .then(users => {
-      res.json(users);
+      res.json(users)
     })
     .catch(err => {
-      res.status(500).json(err);
-    });
-});
+      res.status(500).json(err)
+    })
+})
+router.route('/').post((req, res) => {
+  const { username, password, race } = req.body
+  User.create({ username, password, race })
+    .then(saveduser => res.status(200).json(saveduser))
+    .catch(error => res.status(500).json({ error: error.message }))
+})
 
-module.exports = router;
+module.exports = router
