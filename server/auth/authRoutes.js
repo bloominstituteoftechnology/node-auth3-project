49884f734889575cbc96a941329e623f25bf45cf
Danny Vail <danny_vail@outlook.com>
2018-06-13 14:41:31 -0400

    Add to do for login route - minor formatting edits

--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -1,15 +1,19 @@
 const router = require('express').Router();
 const User = require('../users/User');
 
-router.post('/register', function(req, res) {
-  User.create(req.body)
-    .then(({ username, race }) => {
-      // we destructure the username and race to avoid returning the hashed password
+router
+  .post('/register', (req, res) => {
+    User.create(req.body)
+      .then(({ username, race }) => {
+        // we destructure the username and race to avoid returning the hashed password
 
-      // then we assemble a new object and return it
-      res.status(201).json({ username, race });
-    })
-    .catch(err => res.status(500).json(err));
-});
+        // then we assemble a new object and return it
+        res.status(201).json({ username, race });
+      })
+      .catch(err => res.status(500).json(err));
+  })
+  .post('/login', (req, res) => {
+
+  })
 
 module.exports = router;
