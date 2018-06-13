6d56202a68c9b145021e6dc398113bf18dd826e4
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-06-13 12:35:39 -0700

    validate password and generate token for login

--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -18,6 +18,30 @@ router.post('/register', function(req, res) {
       res.status(201).json({ username, race, token })
     })
     .catch(err => res.status(500).json(err))
-});
+})
+
+router.post('/login', (req, res) => {
+  const { username, race, password } = req.body
+  User.findOne({ username })
+    .then( user => {
+      if (user) {
+        user.validatePassword(password)
+          .then( match => {
+            if (match) {
+              const { username, race } = user
+              const token = generateToken({ username, race })
+              res.status(200).json({ username, race, token })
+            } else {
+              res.status(404).json({ error: 'username or password is incorrect' })
+            }
+          })
+          .catch( err => {
+            res.status(500).json({ error: 'error processing information' })
+          })
+      } else {
+        res.status(404).json({ error: 'username or password is incorrect' })
+      }
+    })
+})
 
 module.exports = router
