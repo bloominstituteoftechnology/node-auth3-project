131f0cc60416a0df3283a974a0be7427c02ca125
Brandon Hopper <brandonhopper0@gmail.com>
2018-06-13 17:42:02 -0400

    installed react router dom and axios and render user the App component

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,47 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+
+
 class App extends Component {
-  render() {
+  constructor(){
+    super();
+    this.state = {
+      users: [],
+      username: '',
+      race: ''
+    }
+  }
+
+componentDidMount() {
+  axios
+    .get('http://localhost:5500/api/users')
+    .then(response => {
+      this.setState(() => ({users: [...response.data]}));
+    })
+    .catch(err => {
+      console.log("error", err)
+    });
+}
+
+  render(){
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Authentication using JWTs</h1>
+          <h1 className="App-title">Authenticating With JWS</h1>
         </header>
-        <p className="App-intro">
-          Please implement the required code for the assignment.
-        </p>
+        <ul>
+          {this.state.users.map(user => {
+            return (
+              <li key={user}>
+                <p>Username: {user.username}</p>
+                <p>Race: {user.race}</p>
+              </li>
+            )
+          })}
+        </ul>
       </div>
     );
   }
