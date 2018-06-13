8e1b6880d03d3cbfa3dfb11ef7571dc711d0d65f
Zach Campbell <beazlebubbles@gmail.com>
2018-06-13 14:56:32 -0600

    Added Login endpoint

--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -22,4 +22,34 @@ router.post('/register', function(req, res) {
     .catch(err => res.status(500).json(err));
 });
 
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  User.findOne({ username })
+    .then(user => {
+      if (user) {
+        user
+          .validatePassword(password)
+          .then(passwordsMatch => {
+            if (passwordsMatch) {
+              const token = generateToken(user);
+              res.status(200).json({ message: `welcome ${username}!`, token });
+            } else {
+              res.status(401).send('invalid credentials');
+            }
+          })
+          .catch(err => {
+            res.send('error comparing passwords');
+          });
+      } else {
+        res.status(401).send('invalid credentials');
+      }
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
+
+
+
 module.exports = router;
