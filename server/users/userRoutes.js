c77fab2a248277700248e2c92b28faeb6d542d8c
maxwashington <mnwashington18@gmail.com>
2018-06-13 16:51:57 -0500

    modules installed routes for user and auth

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -1,12 +1,31 @@
 const router = require('express').Router();
-
+const jwt = require('jsonwebtoken');
 const User = require('./User');
 
-router.get('/', (req, res) => {
+const secret = 'one does not simply walk into mordor';
+
+function restricted(req, res, next) {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, secret, (err, verifiedToken) => {
+      if(err) {
+	res.status(401).json({ error: 'You must be logged in to access this resource.' });
+      } else {
+	req.jwtPayload = verifiedToken;
+	next();
+      }
+    });
+  } else {
+    res.status(401).json({ error: 'You must be logged in to access this resource.' });
+  }
+}
+
+
+router.get('/', restricted, (req, res) => {
   User.find()
     .select('-password')
     .then(users => {
-      res.json(users);
+      res.status(200).json(users);
     })
     .catch(err => {
       res.status(500).json(err);
