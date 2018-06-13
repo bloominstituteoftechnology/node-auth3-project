4dd5981414de7338c5e3cca304c5f142037ed749
Danny Vail <danny_vail@outlook.com>
2018-06-13 17:35:02 -0400

    Built SignUp component - rearranged file structure -- got rid of views folder

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Routes from '../Routes/Routes';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import SignIn from '../SignIn/SignIn';
+import SignUp from '../SignUp/SignUp';
+import Users from '../Users/Users';
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,7 +15,11 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Authentication using JWTs</h1>
           </header>
-          <Routes />
+
+          <Route exact path="/" component={SignIn} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/users" component={Users} />
         </div>
       </Router>
     );
---
+++ client/src/components/SignUp/SignUp.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      race: ''
+    }
+  }
+  handleOnChange = event => {
+    this.setState({ [event.target.name]:event.target.value });
+  }
+  signUp = event => {
+    event.preventDefault();
+    const user =  {
+      username: this.state.username,
+      password: this.state.password,
+      race: this.state.race
+    };
+    axios.post('http://localhost:5500/api/auth/register', user)
+      .then(response => {
+        localStorage.setItem('authiiToken', response.data.token);
+        this.props.history.push('/users');
+      })
+      .catch(error => {
+        // TO DO: Show error to user
+        console.log('Could not sign user up', error);
+      });
+  }
+  render() { 
+    return (
+      <div>
+        <h1>Sign Up</h1>
+        <form>
+          <input
+            type="text"
+            name="username"
+            placeholder="User Name"
+            value={this.state.username}
+            onChange={this.handleOnChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handleOnChange}
+          />
+          <input
+            type="text"
+            name="race"
+            placeholder="Race"
+            value={this.state.race}
+            onChange={this.handleOnChange}
+          />
+          <button onClick={this.signUp}>Sign Up</button>
+        </form>
+      </div>
+    )
+  }
+}
+ 
+export default SignUpForm;
