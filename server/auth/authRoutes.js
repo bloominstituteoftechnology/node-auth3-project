7b54697694b8094009fe8416ab0186229aa9c20a
MacBook <LonnieDavis@MacBookPros-MacBook-Pro.local>
2018-06-13 16:58:09 -0500

    EndPoint /login
    EndPoint /login implementation of signed token in file authRoutes.js

--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
 
 const User = require('../users/User');
 
@@ -12,5 +13,42 @@ router.post('/register', function(req, res) {
     })
     .catch(err => res.status(500).json(err));
 });
+
+router.post('/login', (req, res) => {
+  const { username, password, race } = req.body;
+  User.findOne({ username })
+  .then(user => {
+      if(user) {
+      user
+      .validatePassword(password)
+      .then(passwordsMatch => {
+        if(passwordsMatch) {
+            const token = generateToken(user);//generate token
+        res.status(200).json({ message: `Welcome ${username}!`, token });
+      } else {
+          res.status(401).send('invalid credentials');
+      }
+  })
+  .catch(err => {
+      res.send('error comparing passwords');
+  });
+  } else {
+      res.status(401).send('invalid credentials');
+  }
+})
+  .catch(err => {
+      res.send(err);
+  });
+});
+
+function generateToken(user) {
+  const options = {
+    expiresIn: '1h'
+  };
+  const secret = "toss me, but don't tell the elf!";
+  const payload = { name: user.name };
+  return jwt.sign(payload, secret, options);
+  //sign the token
+}
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+//const jwt = require('jsonwebtoken');
+const User = require('./users/User.js');
 
 const db = require('./_config/db');
 const setupMiddleware = require('./_config/middleware');
 const setupRoutes = require('./_config/routes');
 
 const server = express();
+//const secret = "toss me, but don't tell the elf!";
+
+const corsOptions = {
+  origin: 'http://localhost:3000', // allow only the React application to connect 
+  credentials: true,// sets the Access-Control-Allow-Credentials CORS header
+};
+
+//server.use(express.json());
+//server.use(cors(corsOptions));
 
 setupMiddleware(server);
 setupRoutes(server);
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 5500;//process is a global variable that has info on ports
 
 db.connectTo('authii')
   .then(() => {
