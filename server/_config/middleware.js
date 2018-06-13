54fcc93a2ad594a73ad3da46f5db046bd4fa9b0a
Danny Vail <danny_vail@outlook.com>
2018-06-13 15:24:59 -0400

    Setup middleware for authentication

--- server/_config/middleware.js
+++ server/_config/middleware.js
@@ -2,6 +2,28 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
+const secret = 'BqhfgTZKTKDEg6sudxFv';
+
+const generateToken = payload => {
+  const options = {
+    expiresIn: '1h'
+  }
+  return jwt.sign(payload, secret, options);
+};
+
+const authenticate = (req, res, next) => {
+  const token = req.headers.authorization;
+  jwt.verify(token, secret, (error, decodedToken) => {
+    if(error){
+      return res.status(401).json({ error: 'Invalid token' });
+    } else {
+      req.tokenPayload = decodedToken;
+      next();
+    }
+  });
+};
 
 module.exports = {
   server: function (server) {
@@ -9,5 +31,7 @@ module.exports = {
     server.use(morgan('dev'));
     server.use(express.json());
     server.use(cors());
-  }
+  },
+  generateToken: generateToken,
+  authenticate: authenticate
 };
