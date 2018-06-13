d1cd2e8f89a23dff6de2fa1492cd685f11db9fb6
udykas <udykas@gmail.com>
2018-06-13 16:07:07 -0400

    Can now log in with valid username and password

--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
 
 const User = require('../users/User');
+
+const secret = "toss me, but don't tell the elf!!";
 
 router.post('/register', function(req, res) {
   User.create(req.body)
@@ -12,5 +15,42 @@ router.post('/register', function(req, res) {
     })
     .catch(err => res.status(500).json(err));
 });
+
+router.post('/login', function(req, res) {
+  const { username, password } = req.body;
+  User.findOne({ username })
+    .then(user => {
+      if(user) {
+        user 
+          .validatePassword(password)
+          .then(passwordMatch => {
+            if(passwordMatch){
+              //generate token
+              const token = generateToken(user);
+              //send token to client
+              res.status(200).json({ message: `welcome ${username}!`, token })
+            } else {
+              res.status(401).json({ error: "Invalid Credentials" })
+            }
+          })
+          .catch(err => {
+            res.status(500).json({ error: err.message })
+          })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message })
+    })
+})
+
+function generateToken(user) {
+  const options = {
+    expiresIn: '1h',
+  };
+  const payload = { name: user.username };
+
+  //sign the token
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
