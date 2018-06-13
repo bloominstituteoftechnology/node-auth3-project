54ca8e7d1acecd05bf6cd03b2e70a22d39643ae4
Luis Hernandez <luishrd@gmail.com>
2018-06-13 08:05:46 -0600

    start api only on successful connection to db

--- server/server.js
+++ server/server.js
@@ -6,14 +6,16 @@ const setupRoutes = require('./_config/routes');
 
 const server = express();
 
-db
-  .connectTo('jwtauth')
-  .then(() => console.log('\n... API Connected to jwtauth Database ...\n'))
-  .catch(err => {
-    console.log('\n*** ERROR Connecting to MongoDB, is it running? ***\n', err);
-  });
-
 setupMiddleware(server);
 setupRoutes(server);
 
-server.listen(5500, () => console.log('\n=== API running on port 5500 ===\n'));
+db.connectTo('jwtauth')
+  .then(() => {
+    console.log('\n... API Connected to jwtauth Database ...\n');
+    server.listen(5500, () =>
+      console.log('\n=== API running on port 5500 ===\n')
+    );
+  })
+  .catch(err => {
+    console.log('\n*** ERROR Connecting to MongoDB, is it running? ***\n', err);
+  });
