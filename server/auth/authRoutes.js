3054fa9bbd9ec9993b1531508bd09729b0e3fece
Zach Campbell <beazlebubbles@gmail.com>
2018-06-13 14:16:02 -0600

    Added token to register route

--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -16,10 +16,8 @@ function generateToken(username) {
 router.post('/register', function(req, res) {
   User.create(req.body)
     .then(({ username, race }) => {
-      // we destructure the username and race to avoid returning the hashed password
-
-      // then we assemble a new object and return it
-      res.status(201).json({ username, race });
+      const token = generateToken(username);
+      res.status(201).json({ username, race, token });
     })
     .catch(err => res.status(500).json(err));
 });
