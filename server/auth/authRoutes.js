90b384f957db08bde04b3a46bff82471c9acea30
bbenefield89 <bsquared18@gmail.com>
2018-06-13 17:55:16 -0400

    feat(authRoutes.js): log user in and create JWT

--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const User = require('../users/User');
 const { secret } = require('../secret.json');
@@ -15,5 +16,34 @@ router.post('/register', function(req, res) {
     })
     .catch(err => res.status(500).json(err));
 });
+
+router.post('/login', (req, res) => {
+  const { username, password, race } = req.body;
+  
+  User.findOne()
+    .where({ username })
+    .exec((err, user) => {
+      if (err)
+        return res.status(500).json(err);
+
+      if (!user)
+        return res.status(404).json({ err: 'No user with that name' });
+
+      bcrypt.compare(password, user.password)
+        .then(data => {
+            if (!data)
+              res.status(401).json({ err: 'Wrong password' });
+
+              
+          const payload = { username, race };
+          const options = { expiresIn: '1h' };
+          const token = jwt.sign(payload, secret, options);
+              
+          console.log({ username, race, token });
+          res.status({ username, race, token });
+        })
+        .catch(err => res.status(500).json(err));
+    })
+})
 
 module.exports = router;
