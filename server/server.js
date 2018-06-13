5d2943c43488be630fe6e73b8d24e7d663ca6f4b
maribelcuales <maribelcuales@gmail.com>
2018-06-14 07:02:02 +0800

    added /login and /users routes

--- server/server.js
+++ server/server.js
@@ -20,26 +20,28 @@ server.use(cors(corsOptions));
 setupMiddleware(server);
 setupRoutes(server);
 
-server.post('api/register', (req, res) => {
+server.post('/api/register', (req, res) => {
   User.create(req.body)
     .then(user => {
       const token = generateToken(user); 
 
       res.status(201).json({ username: user.username, token });
     })
-    .catch(err => res.status(500).json(err)); 
+    .catch(error => res.status(500).json({ error: error.message })); 
 }); 
 
 server.post('/api/login', (req, res) => {
-  const { username, password } = req.body; 
+  const { username, password, race } = req.body; 
+  
   User.findOne({ username })
     .then(user => {
       if(user) {
         user
           .validatePassword(password)
-          .then(passwordMatch => {
-            if(passwordMatch) {
+          .then(passwordsMatch => {
+            if(passwordsMatch) {
               const token = generateToken(user); 
+
               res.status(200).json({ message: `welcome ${username}!`, token});
             } else {
               res.status(401).send('invalid credentials');
@@ -62,18 +64,61 @@ function generateToken(user) {
     expiresIn: '1h',
   };
   const payload = { name: user.username };
+
   return jwt.sign(payload, secret, options);
 }
 
+function restricted(req, res, next) {
+  const token = req.headers.authorization;
 
+  if(token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      req.jwtPayload(decodedToken); 
+      if(err) {
+        return res
+          .status(401)
+          .json({ message: 'you shall not pass! not decoded' });
+      }
 
-db.connectTo('authii')
+      next();
+    });
+  } else {
+    res.status(401).json({ message: 'you shall not pass! no token' });
+  }
+}
+
+server.get('/api/users', restricted, (req, res) => {
+  User.find({})
+    .select('username')
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      return res.status(500).json(err); 
+    });
+});
+
+const port = process.env.PORT || 5500; 
+
+mongoose
+  .connect('mongodb://localhost/authii')
   .then(() => {
-    console.log('\n... API Connected to authii Database ...\n');
-    server.listen(5500, () =>
-      console.log('\n=== API running on port 5500 ===\n')
-    );
+    console.log('n=== Connected to MongoDB ===');
+    server.listen(port, (req, res) => {
+      console.log(`\n=== API up on port ${port} ===\n`);
+    });
   })
-  .catch(err => {
-    console.log('\n*** ERROR Connecting to MongoDB, is it running? ***\n', err);
-  });
+  .catch(err => 
+    console.log('\n=== Error connecting to MongoDB, is it running? ===\n', err)
+  );
+
+// db.connectTo('authii')
+//   .then(() => {
+//     console.log('\n... API Connected to authii Database ...\n');
+//     server.listen(5500, () =>
+//       console.log('\n=== API running on port 5500 ===\n')
+//     );
+//   })
+//   .catch(err => {
+//     console.log('\n*** ERROR Connecting to MongoDB, is it running? ***\n', err);
+//   });
