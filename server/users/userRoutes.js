afbf27bd5e20d296c612695a4ee9ea966224cd22
jaklundun <govi.escudero@gmail.com>
2018-06-13 13:45:40 -0700

    Set up login endpoint with jwt

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -5,7 +5,26 @@ const User = require('./User');
 
 //base prefix is '/api/users'
 
-router.get('/', (req, res) => {
+function restricted (req, res, next) {
+  const token = req.headers.authorization 
+
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      req.jwtPayload(decodedToken);
+        if (err) {
+          return res
+          .status(401)
+          .json({ message: 'Please sign in'})
+        }
+        next();
+    });
+  } else {
+    res.status(401).json({message: 'Please do something right'})
+  }
+}
+
+
+router.get('/', restricted, (req, res) => {
   User.find()
     .select('-password')
     .then(users => {
