4a78bc2e8dc02e07370ff7c6b64a3c29c372fe07
Danny Vail <danny_vail@outlook.com>
2018-06-13 18:26:08 -0400

    Built Users component

--- client/src/components/Users/Users.js
+++ client/src/components/Users/Users.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Users = () => {
-  return (
-    <div>Users</div>
-  )
+class Users extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    }
+  }
+  componentDidMount(){
+    const config = {
+      headers: {
+        'Authorization': localStorage.authiiToken
+      }
+    };
+    axios.get('http://localhost:5500/api/users', config)
+      .then(response => {
+        this.setState({ users: response.data });
+      })
+      .catch(error => {
+        // TO DO: Show error to user
+        console.log('Could not retrieve users', error);
+      });
+  }
+  render() { 
+    return (
+      <div>
+        <h1>Users</h1>
+        <ul>
+          {this.state.users.map(user => {
+            return (
+              <li key={user.username}>
+                <div>
+                  <h2>{user.username}</h2>
+                  <p>{user.race}</p>
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
 }
-
+ 
 export default Users;
