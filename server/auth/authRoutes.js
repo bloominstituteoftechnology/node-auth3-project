25351b18de1d3c7c3228f534a68f70632dc00270
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-06-13 12:06:10 -0700

    intial setup and jwt token for registered user

--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -1,16 +1,23 @@
-const router = require('express').Router();
+const router = require('express').Router()
+const jsw = require('jsonwebtoken')
+const User = require('../users/User')
 
-const User = require('../users/User');
+const secret = 'Speak "friend" and enter'
+
+function generateToken(user) {
+  const payload = { name: user.username, race: user.race }
+  return jwt.sign(payload, secret)
+}
 
 router.post('/register', function(req, res) {
   User.create(req.body)
     .then(({ username, race }) => {
       // we destructure the username and race to avoid returning the hashed password
-
+      const token = generateToken({ username, race })
       // then we assemble a new object and return it
-      res.status(201).json({ username, race });
+      res.status(201).json({ username, race, token })
     })
-    .catch(err => res.status(500).json(err));
+    .catch(err => res.status(500).json(err))
 });
 
-module.exports = router;
+module.exports = router
