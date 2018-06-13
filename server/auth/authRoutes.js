a82a4a085e0b3e5df89275a5354851e265b9be55
Anne Courtney <annecourtney_1@yahoo.com>
2018-06-13 17:15:14 -0500

    met mvp for server side

--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -1,16 +1,67 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
 
+const secret = "I am a secret";
 const User = require('../users/User');
 
-router.post('/register', function(req, res) {
-  User.create(req.body)
-    .then(({ username, race }) => {
-      // we destructure the username and race to avoid returning the hashed password
 
-      // then we assemble a new object and return it
-      res.status(201).json({ username, race });
-    })
-    .catch(err => res.status(500).json(err));
+//generate token function 
+function generateToken(username) {
+const options = {
+  expiresIn: '1hr',
+};
+const payload = {
+  name: username,
+};
+//signing the token
+return jwt.sign(payload, secret, options);
+};
+
+
+router
+.post('/register', function(req, res) {
+User
+.create(req.body)
+.then(({ username, race }) => {
+  
+  const token = generateToken(username);
+  console.log(race);
+  console.log(token);
+    res.status(201).json({ username, race, token });
+  })
+  .catch(err => res.status(500).json(err));
 });
+
+
+router.post ('/login', (req, res) => {
+  const { username, password } = req.body;
+  User
+  .findOne({ username })
+  .then(user => {
+    if(user) {
+      user
+      .validatePassword(password)
+      .then(passwordsMatch => {
+        if(passwordsMatch) {
+          const token = generateToken(username, user.race)
+          res.send('have a cookie');
+        }
+        else {
+          res.status(401)
+          res.json({ message: "invalid credentials" })
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: "error comparing passwords"})
+      })
+      }
+      else {
+        res.status(401).json({ message: "invalid credentials" })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "error" });
+  })
+})
 
 module.exports = router;
--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -1,8 +1,41 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
 
+const secret = "I am a secret";
 const User = require('./User');
 
-router.get('/', (req, res) => {
+function restricted(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      req.jwtPayload = decodedToken;
+      if (err) {
+        return 
+        res.status(401)
+        res.json({ message: 'you shall not pass! not decoded' });
+      }
+      next();
+    });
+  } else {
+    res.status(401).json({ message: 'you shall not pass! no token' });
+  }
+}
+
+router.get('/api/users', restricted, (req, res,) => {
+  User
+  .find()
+  .then(users => {
+    res.status(200)
+    res.json({ user })
+  })
+  .catch(err => {
+    return res.status(500).json({ message: "error" })
+  });
+});
+
+
+router.get('/', restricted, (req, res) => {
   User.find()
     .select('-password')
     .then(users => {
