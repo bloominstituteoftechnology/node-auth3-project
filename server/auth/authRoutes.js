8f61c725829e0bd9a1d24055732cc49acec24864
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-06-13 14:50:51 -0700

    generateToken created

--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -1,12 +1,24 @@
 const router = require('express').Router();
-
+const jwt = require('jsonwebtoken');
 const User = require('../users/User');
+
+const secret = 'hasta siempre Comandante';
+
+function generateToken(user) {
+  const options = {
+    expiresIn: '1h',
+  };
+  const payload = { name: user.username };
+
+  // sign the token
+  return jwt.sign(payload, secret, options);
+}
 
 router.post('/register', function(req, res) {
   User.create(req.body)
-    .then(({ username, race }) => {
+    .then((user) => {
       // we destructure the username and race to avoid returning the hashed password
-
+      const token = generateToken(user);
       // then we assemble a new object and return it
       res.status(201).json({ username, race });
     })
--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-
 const User = require('./User');
+
 
 router.get('/', (req, res) => {
   User.find()
