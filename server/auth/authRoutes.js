dd224daa2c135527695dae25537b1693090c6c6f
kelly <kelfro@fastmail.com>
2018-06-13 13:31:21 -0700

    finished jwt register and login endpoints working on restricting /api/users

--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
-
+const jwt = require('jsonwebtoken');
 const User = require('../users/User');
+
+const secret = 'one does not simply walk into mordor';
+
+function generateToken(user) {
+  const payload = { name: user.username, race: user.race };
+  return jwt.sign(payload, secret);
+};
 
 router.post('/register', function(req, res) {
   User.create(req.body)
@@ -8,9 +15,34 @@ router.post('/register', function(req, res) {
       // we destructure the username and race to avoid returning the hashed password
 
       // then we assemble a new object and return it
-      res.status(201).json({ username, race });
+      const token = generateToken({ username, race });
+      res.status(201).json({ username, race, token });
     })
     .catch(err => res.status(500).json(err));
+});
+
+router.post('/login', (req, res) => {
+  const { username, race, password } = req.body;
+  User.findOne({ username })
+    .then(user => {
+      if (user) {
+	user.validatePassword(password)
+	  .then(match => {
+	    if(match) {
+	      const { username, race } = user;
+	      const token = generateToken({ username, race });
+	      res.status(200).json({ username, race, token });
+	    } else {
+	      res.status(404).json({ error: 'invalid credentials' });
+	    }
+	  })
+	  .catch(err => {
+	    res.status(500).json({ error: 'error processing login' });
+	  });
+      } else {
+	res.status(404).json({ error: 'invalid credentials' });
+      }
+    });
 });
 
 module.exports = router;
--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
-
+const jwt = require('jsonwebtoken');
 const User = require('./User');
+
+const secret = 'one does not simply walk into mordor';
+
+function restricted(req, res, next) {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, secret, (err, verifiedToken) => {
+      if(err) {
+	
+
 
 router.get('/', (req, res) => {
   User.find()
