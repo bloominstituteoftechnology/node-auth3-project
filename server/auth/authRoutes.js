e357aa16641c69e713b6fa00ded6ba08d8bb82b9
unknown <sam.park.email@gmail.com>
2018-06-13 16:58:10 -0400

    inital commit, login/register routes working

--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -1,16 +1,55 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+const User = require("../users/User");
 
-const User = require('../users/User');
+const secret = "its a secret";
 
-router.post('/register', function(req, res) {
+function generateToken(user) {
+  const options = {
+    expiresIn: "1h"
+  };
+  const payload = { name: user.username };
+  
+  return jwt.sign(payload, secret, options);
+}
+
+router.post("/register", function(req, res) {
   User.create(req.body)
     .then(({ username, race }) => {
       // we destructure the username and race to avoid returning the hashed password
-
       // then we assemble a new object and return it
-      res.status(201).json({ username, race });
+      const token = generateToken(username);
+
+      res.status(201).json({ username, token });
     })
     .catch(err => res.status(500).json(err));
+});
+
+router.post("/login", function(req, res) {
+  const { username, password } = req.body;
+  User.findOne({ username })
+    .then(user => {
+      if (user) {
+        user
+          .validatePassword(password)
+          .then(passwordsMatch => {
+            if (passwordsMatch) {
+              const token = generateToken(user);
+              res.status(200).json({ message: `welcome ${username}`, token });
+            } else {
+              res.status(401).send("invalid credentials");
+            }
+          })
+          .catch(err => {
+            res.send("error comparing passwords");
+          });
+      } else {
+        res.status(401).send("invalid creds");
+      }
+    })
+    .catch(err => {
+      res.send(err);
+    });
 });
 
 module.exports = router;
