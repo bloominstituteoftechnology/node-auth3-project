2c5587422a6a29fd92bf47745c5f74a9355e761e
Danny Vail <danny_vail@outlook.com>
2018-06-13 15:46:03 -0400

    Implement authentication for login

--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -16,7 +16,35 @@ router
       .catch(err => res.status(500).json(err));
   })
   .post('/login', (req, res) => {
+    const { username, password } = req.body;
 
+    if(!username || !password){
+      res.status(400).json({ error: 'Please include both a username and password' });
+      return;
+    }
+
+    User.findOne({username})
+      .then(user => {
+        if(user){
+          user.validatePassword(password)
+            .then(login => {
+              if(login){
+                const token = generateToken({
+                  username: user.username,
+                  race: user.race
+                });
+                res.status(201).json({ username: user.username, token });
+              } else {
+                res.status(401).json({ error: 'Invalid credentials' });
+              }
+            });
+        } else {
+          res.status(401).json({ error: 'Invalid credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ error: error.message });
+      });
   })
 
 module.exports = router;
