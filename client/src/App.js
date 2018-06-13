cc305d557d154bfc75172e4dbd661327b76c8a59
Steve <shalomlandsman@gmail.com>
2018-06-13 18:56:32 -0400

    signup component; add axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
 
 import Signup from "./components/signup";
 
@@ -12,7 +13,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Authentication using JWTs</h1>
         </header>
-        <Signup />
+        <Route exact path="/" component={ Signup }/>
       </div>
     );
   }
--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -1,17 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Signup extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+          username: '',
+          password: '',
+          race: ''
+      };
+  }  
+
+  handleChange = e => {
+      this.setState({ [e.target.name] : e.target.value });
+  }  
+
   render() {
     return (
       <div>
         <form>
             <h1>Sign Up for... </h1>
-            <h2>Name:</h2>
-            <input name="username"></input>
+            <h2>Username:</h2>
+            <input name="username" onChange={this.handleChange} value={this.state.username} /> 
             <h2>Password:</h2>
-            <input name="password"></input>
+            <input name="password" onChange={this.handleChange} value={this.state.password} />
             <h2>Race:</h2>
-            <input name="race"></input>
+             <input name="race" onChange={this.handleChange} value={this.state.race} />
         </form>
       </div>
     )
