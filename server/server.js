0e2d079fc9244ac39194fd5728f59dff73ed697d
maribelcuales <maribelcuales@gmail.com>
2018-06-14 05:53:28 +0800

    installed dependencies and added /register route

--- server/server.js
+++ server/server.js
@@ -1,13 +1,71 @@
 const express = require('express');
+const mongoose = require('mongoose'); 
+const cors = require('cors');
+const jwt = require('jsonwebtoken'); 
 
 const db = require('./_config/db');
 const setupMiddleware = require('./_config/middleware');
 const setupRoutes = require('./_config/routes');
+const User = require('./users/User');
 
 const server = express();
+const secret = "toss me, but don't tell the elf!";
 
+const corsOptions = {
+  origin: 'http://localhost:3000', 
+  credentials: true,
+}; 
+
+server.use(cors(corsOptions)); 
 setupMiddleware(server);
 setupRoutes(server);
+
+server.post('api/register', (req, res) => {
+  User.create(req.body)
+    .then(user => {
+      const token = generateToken(user); 
+
+      res.status(201).json({ username: user.username, token });
+    })
+    .catch(err => res.status(500).json(err)); 
+}); 
+
+server.post('/api/login', (req, res) => {
+  const { username, password } = req.body; 
+  User.findOne({ username })
+    .then(user => {
+      if(user) {
+        user
+          .validatePassword(password)
+          .then(passwordMatch => {
+            if(passwordMatch) {
+              const token = generateToken(user); 
+              res.status(200).json({ message: `welcome ${username}!`, token});
+            } else {
+              res.status(401).send('invalid credentials');
+            }
+          })
+          .catch(err => {
+            res.send('error comparing passwords');
+          });
+      } else {
+        res.status(401).send('invalid credentials');
+      }
+    })
+    .catch(err => {
+      res.send(err); 
+    });
+}); 
+
+function generateToken(user) {
+  const options = {
+    expiresIn: '1h',
+  };
+  const payload = { name: user.username };
+  return jwt.sign(payload, secret, options);
+}
+
+
 
 db.connectTo('authii')
   .then(() => {
