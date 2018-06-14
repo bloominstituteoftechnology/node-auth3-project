b33094b342e91b02e2858a024ba2534b12ecfef7
Matthias Bakken <matthias.bakken@gmail.com>
2018-06-14 12:22:50 -0700

    added components folder and home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import SignupForm from './components/SignupForm';
+import Home from "./components/Home";
+import LoginForm from "./components/LoginForm";
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Authentication using JWTs</h1>
-        </header>
-        <p className="App-intro">
-          Please implement the required code for the assignment.
-        </p>
+        <Route exact path='/' component={Home} />
+        <Route path="/login" component={LoginForm} />
+        <Route path="/register" component={SignupForm} />
       </div>
     );
   }
