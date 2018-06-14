5d689907cf43f0f8f270b2e5fb99d5add830f8a6
Anne Courtney <annecourtney_1@yahoo.com>
2018-06-14 17:12:34 -0500

    completed users and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom';
+
 import './App.css';
+import Signin from './signin.js';
+import Users from './users.js';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Authentication using JWTs</h1>
-        </header>
-        <p className="App-intro">
-          Please implement the required code for the assignment.
-        </p>
+        
+
+      <Route path="/signin" component={Signin} />
+      <Route path="/users" component={Users} />
       </div>
     );
   }
---
+++ client/src/users.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import axios from 'axios'; 
+
+class Users extends Component {
+state = {
+    users: []
+}
+
+render() {
+    console.log(this.state.users);
+    return (
+      <ul>
+        {this.state.users.map(user => <li key={user._id}>{user.username}</li>)}
+      </ul>
+    );
+  }
+
+componentDidMount() {
+     // get the token from somewhere
+    const token = localStorage.getItem('jwt');
+
+    // attach the token as the Authorization header
+    const requestOptions = {
+        headers: {
+            Authorization: token,
+        }
+    };
+
+    axios 
+    .get('http://localhost:5500/api/users', requestOptions)
+    .then(response => {
+        console.log(response);
+        this.setState({ users: response.data});
+        console.log(response.data);
+    })
+    .catch(err => {
+        console.log("err");
+    })
+}
+}
+
+export default Users
