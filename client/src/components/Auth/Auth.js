d538370f1707bc887927f24151b935043b895ba2
jesuarva <jean.ariza@jesuarva.com>
2018-06-14 23:20:37 +0200

    CLIENT: Implement 'Logout'

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
 
 const Auth = () => {
   return (
     <React.Fragment>
+      {localStorage.getItem('jwt') && (
+        <Link to="/register">
+          <button onClick={logOut}>Log out</button>
+        </Link>
+      )}
       <Route path="/register" component={Register} />
       <Route path="/login" component={Login} />
     </React.Fragment>
   );
+};
+
+const logOut = () => {
+  localStorage.removeItem('jwt');
+  !localStorage.getItem('jwt') ? console.log('logged out') : null;
 };
 
 export default Auth;
