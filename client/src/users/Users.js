f4e341e5fa6002a3ba1d0ad36ad3e8b71ec5b9df
udykas <udykas@gmail.com>
2018-06-14 16:20:53 -0400

    Can now sign in, see the users, and sign out!

---
+++ client/src/users/Users.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Users extends Component {
+  state = {
+    users: [],
+  };
+
+  render() {
+    return (
+      <ul>
+        {this.state.users.map(user => <li key={user._id}>{user.username}</li>)}
+      </ul>
+    );
+  }
+
+  componentDidMount() {
+    const token = localStorage.getItem('jwt');
+    const requestOptions = {
+      headers: {
+        Authorization: token,
+      },
+    };
+
+    axios
+      .get('http://localhost:5500/api/users', requestOptions)
+      .then(response => {
+        this.setState({ users: response.data });
+      })
+      .catch(err => {
+        console.error("Error retrieving Users.", err.message)
+      });
+  }
+}
+
+export default Users;
