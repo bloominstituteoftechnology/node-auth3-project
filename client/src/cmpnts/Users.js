87d2b4342e2af790c219101d66608343f870be0c
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-06-14 19:43:32 -0700

    upgraded the user list that displays upon login

--- client/src/cmpnts/Users.js
+++ client/src/cmpnts/Users.js
@@ -32,7 +32,7 @@ class Users extends Component {
             <ul className="user-list">
                 {this.state.users.map(user => {
                     return (
-                        <h3 key={user._id} className="indv-usr">{user.username}</h3>
+                        <p key={user._id} className="indv-usr">{user.username}</p>
                     )
                 })}
             </ul>
