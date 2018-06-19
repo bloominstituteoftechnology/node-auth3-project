dd9fa1143b1d10c9e436bb797aa7cc5f6b1beeda
ubuntu <irishseanvaldivia@gmail.com>
2018-06-19 15:18:55 -0500

    Fleshed out user routes, now on to something else.

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -3,3 +3,32 @@ const User = require('./User');
 const jwt = require('jsonwebtoken');
 const secret = 'I am an anarchist.';
 
+function restricted(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, secret, (error, decodeURIComponent) => {
+      if (error) {
+        res.status(401).json('You cannot view users without a valid token.');
+        return;
+      }
+      next();
+    });
+  }
+  else {
+    res.status(401).json('You cannot view users with a valid token.');
+  }
+}
+
+router.get('/', restricted, (req, res) => {
+  User.find()
+    .select('-password')
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+});
+
+module.exports = router;
