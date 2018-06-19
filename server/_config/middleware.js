7977ea317c6a2fbeeffdefb59c85efebda5d7f72
FireInjun <Fireinjun291@gmail.com>
2018-06-19 18:16:55 -0400

    Server Middleware Modified

--- server/_config/middleware.js
+++ server/_config/middleware.js
@@ -2,10 +2,16 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const URL = 'http://localhost:778'
+
+const corsOptions = {
+  origin: URL,
+  credentials: true,
+}
 
 module.exports = function(server) {
   server.use(helmet());
   server.use(morgan('dev'));
   server.use(express.json());
-  server.use(cors());
+  server.use(cors(corsOptions));
 };
