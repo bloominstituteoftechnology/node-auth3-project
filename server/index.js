f0568188f60d68c7f2d4dbc34db55626d56d5665
FireInjun <Fireinjun291@gmail.com>
2018-06-19 17:38:29 -0400

    Server Updated
    on Index and the new package-lock

--- server/index.js
+++ server/index.js
@@ -1,21 +1,30 @@
 const express = require('express');
+const helmet = require( 'helmet' );
+const cors = require( 'cors' );
+const mongoose = require( 'mongoose' );
+const bodyparser = require( 'body-parser' );
+const session = require( 'express-session' );
+const axios = require( 'axios' );
+const bcrypt = require( 'bcrypt' );
+const cookieparser = require( 'cookie-parser' );
 
 const db = require('./_config/db');
 const setupMiddleware = require('./_config/middleware');
 const setupRoutes = require('./_config/routes');
 
 const server = express();
+const port = process.env.PORT || 777;
 
 setupMiddleware(server);
 setupRoutes(server);
 
-db.connectTo('authii')
+db.connectTo('authiiproj')
   .then(() => {
-    console.log('\n... API Connected to authii Database ...\n');
-    server.listen(5500, () =>
-      console.log('\n=== API running on port 5500 ===\n')
+    console.log('\n... API Hooked Up To The Database ...\n');
+    server.listen(port, () =>
+      console.log(`\n=== API running on port ${ port } ===\n`)
     );
   })
   .catch(err => {
-    console.log('\n*** ERROR Connecting to MongoDB, is it running? ***\n', err);
+    console.log("\n*** ERROR Connecting to MongoDB! Don't Panic! Try turning it off And Back On Again. ***\n", err);
   });
