c7bfadd711828c5c45e93b785b1b104d1af28d1a
FireInjun <Fireinjun291@gmail.com>
2018-06-19 20:08:08 -0400

    Server Routes Updated

--- server/_config/routes.js
+++ server/_config/routes.js
@@ -4,7 +4,7 @@ const authRoutes = require('../auth/authRoutes');
 module.exports = function(server) {
   // sanity check route
   server.get('/', function(req, res) {
-    res.send({ api: 'up and running' });
+    res.send({ api: 'What business do you have here?' });
   });
 
   server.use('/api/users', userRoutes);
