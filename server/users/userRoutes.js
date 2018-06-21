2b64ecdb819a37cdf274246940be86b6a8a2ca62
kammagik <kammagik@yahoo.com>
2018-06-21 13:00:21 -0700

    added authentication

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
-
+const jwt = require('jsonwebtoken')
 const User = require('./User');
 
+const secret = 'This is a secret'
+function restricted(req, res, next) {
+  const token = req.headers.authorization
+  if (token) {
+    jwt.verify(token, secret, (err, verifiedToken) => {
+     if (err) {
+       return res.status(401).json({ message: 'can not enter, not decoded' });
+     } 
+     next();
+    });
+  } else {
+    res.status(401).json({ message: 'can not enter, no token'});
+  }
+}
 router.get('/', (req, res) => {
   User.find()
     .select('-password')
