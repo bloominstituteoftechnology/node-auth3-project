c4463fd762bcbc3d5bb46626e27393d3f1df693c
april <martinezapril56@yahoo.com>
2018-06-20 23:08:27 -0400

    got my token

--- server/_config/middleware.js
+++ server/_config/middleware.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const cors = require('cors');
+const cors = require( 'cors' );
+
+const corsOptions = {
+  //origin: 'http://localhost:3000',
+  credentails:true,
+}
 
 module.exports = function(server) {
   server.use(helmet());
   server.use(morgan('dev'));
   server.use(express.json());
-  server.use(cors());
+  server.use(cors(corsOptions));
 };
