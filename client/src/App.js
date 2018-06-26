a78269a70aeaf4e00d5887af275b46fd1e861814
Kevin Mei <derrick.mei.168@gmail.com>
2018-06-25 22:31:13 -0500

    finished app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-import { Route, withRouter } from "react-router";
+import { Route, withRouter } from "react-router-dom";
 
 import "./App.css";
 
