6ff46c19f3d169b955716e1d3bc4031bcfb6f101
Simon <Simon_Freelance@hotmail.com>
2018-08-08 15:42:46 -0400

    Added a GET request for all users without authentication

--- api/userRouter.js
+++ api/userRouter.js
@@ -6,4 +6,13 @@ const db = require("../data/dbConfig");
 
 const router = express.Router();
 
+router.get("/", (req, res, next) => {
+  db("users")
+    .then(response => {
+      res.status(codes.OK).json(response);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 module.exports = router;
