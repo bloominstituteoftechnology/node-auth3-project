83715c106ac690b8598cf8fcbe3138687bacbb74
Simon <Simon_Freelance@hotmail.com>
2018-08-08 17:41:38 -0400

    Added unauthorized status code

--- data/statusCodes.js
+++ data/statusCodes.js
@@ -1,6 +1,7 @@
 const OK = 200;
 const CREATED = 201;
 const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
 const NOT_FOUND = 404;
 const INTERNAL_SERVER_ERROR = 500;
 
@@ -9,6 +10,7 @@ module.exports = {
     OK,
     CREATED,
     BAD_REQUEST,
+    UNAUTHORIZED,
     NOT_FOUND,
     INTERNAL_SERVER_ERROR
 }
