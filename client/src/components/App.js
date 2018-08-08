59a27907adad74420bf3e9f9c422db5ec7ea5147
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-08-08 17:39:38 -0600

    Created handleSignup function and passed it to the button on Signup.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -19,12 +19,16 @@ class App extends Component {
   }
 
   handleChange = (e) => {this.setState({ [e.target.name]: e.target.value });}
+  handleSignup = () => {
+    // Put POST request to /register here.
+    this.setState({ username: '', password: '', department: '' })
+  }
 
   render() {
     return (
       <div className="App">
         <h1>Home/Root</h1>
-        <Route path='/signup' render={() => <Signup state={this.state} handleChange={this.handleChange} />} />
+        <Route path='/signup' render={() => <Signup state={this.state} handleChange={this.handleChange} handleSignup={this.handleSignup} />} />
         <Route path='/signin' component={Signin} />
         <Route path='/users' component={Users} />
       </div>
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -3,10 +3,11 @@ import React from 'react';
 const Signup = (props) => {
     return (
         <div>
-            <form>
+            <form onSubmit={props.handleSignup} >
                 <input name='username' value={props.username} placeholder='Username...' onChange={props.handleChange} />
                 <input name='password' value={props.password} placeholder='Password...' onChange={props.handleChange} />
                 <input name='department' value={props.department} placeholder='Department...' onChange={props.handleChange} />
+                <button onClick={props.handleSignup}>Sign Up</button>
             </form>
         </div>
     );
