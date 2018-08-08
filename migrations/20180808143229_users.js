0f320d539ca9d0ad45c34ceda5e8c0407eb31836
markdavidwilliams <markwilliams.aus@gmail.com>
2018-08-08 14:37:31 -0500

    init commit

---
+++ migrations/20180808143229_users.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', tbl => {
+    tbl.increments();
+    tbl.string('username').notNullable();
+    tbl.string('department').notNullable();
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
+};
