6569c3b802b8149d456e644d561c9e05c9d7e78f
Simon <Simon_Freelance@hotmail.com>
2018-08-08 15:35:37 -0400

    User migration table completed

---
+++ data/migrations/20180808153439_users.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('users', function(tbl) {
+        tbl.increments();
+    
+        tbl.string('username')
+        .notNullable()
+        .unique();
+        tbl.string('password')
+        .notNullable();
+    
+        tbl.string('role')
+        .notNullable()
+        .defaultTo('free');
+      });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
