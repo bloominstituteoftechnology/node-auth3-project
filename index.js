2ffc7f8c94a3d7f49d2210b2bc60194c1b8ef0aa
chainchompa <chainchompa@gmail.com>
2018-08-08 15:30:26 -0400

    api up and running

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const db = require('./data/db');
+const server = express();
+const bcrypt = require("bcrypt");
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send('Its Alive!');
+});
+
+const port = 8080;
+server.listen(port, function() {
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
