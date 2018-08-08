d950bbfaf4952bc397daac48706fe31dc3d2fcb4
Kevin <brackkevin@gmail.com>
2018-08-08 16:44:35 -0500

    Add login endpoint and respond with token

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require("express");
 const server = express();
 const bcrypt = require("bcryptjs");
 const db = require("./data/db");
+const jwt = require("jsonwebtoken");
+const secret = "nobody tosses a dwarf!";
+
+// token generator
+function generateToken(user) {
+  const payload = {
+    username: user.user_name
+  };
+
+  const options = {
+    expiresIn: "4h"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 // use middleware
 server.use(express.json());
@@ -14,14 +29,40 @@ server.get("/", (req, res) => {
 // POST /api/register
 server.post("/api/register", (req, res) => {
   const new_user = req.body;
+  const hash = bcrypt.hashSync(new_user.password, 10);
+  new_user.password = hash;
   db("users")
     .insert(new_user)
     .into("users")
-    .then(res.status(200).json("USEER CREATED SUCCESSFULLY"))
+    .then(user_id => {
+      // generate the token
+      const token = generateToken(new_user);
+      // attatch the token to the response
+      res.status(200).json({ token, user_id });
+    })
     .catch(error => res.status(500).json(error.message));
 });
 
 // POST /api/login
+server.post("/api/login", (req, res) => {
+  const credentials = req.body;
+
+  db("users")
+    .where({ user_name: credentials.user_name })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(credentials.password, user.password)) {
+        // generate the token
+        const token = generateToken(user);
+
+        // attach the token to the response
+        res.send(token);
+      } else {
+        return res.status(401).json({ error: "Incorrect credentials" });
+      }
+    })
+    .catch(error => res.status(500).json(error.message));
+});
 
 // GET /api/users
 
