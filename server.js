f65a9931cb7d51641f5a8c1d2eba4745c698616c
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-08-08 11:36:16 -0700

    post for /api/register works

--- server.js
+++ server.js
@@ -7,5 +7,47 @@ const db = require('./data/db');
 
 const server = express();
 
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('<h1>Home Page</h1>')
+})
+// **** user *****
+
+server.post('/api/register', function(req, res) {
+    const user = req.body;
+
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+
+    db('users')
+        .insert(user)
+        .then(function(ids) {
+            db('users')
+            .where({ id: ids[0] })
+            .first()
+            .then(user => {
+                const token = generateToken(user);
+                res.status(201).json(token);
+            })
+        })
+        .catch(err => res.status(500).json({ err }))
+
+})
+
+const secret = 'nobody tosses a dwarf!!';
+
+function generateToken(user) {
+    const payload = {
+        username: user.username,
+    }
+
+    const options = {
+        expiresIn: '1h',
+        jwtid: '2345678'
+    };
+
+    return jwt.sign(payload, secret, options)
+}
 
 server.listen(port, () => console.log(`running on port ${port}`));
