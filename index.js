b78996056b454c85397a723abbd9c0a26676473b
Mariana Cerda <raymond_grc@yahoo.com>
2018-08-08 16:28:23 -0500

    Add registration

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ server.use(express.json());
 
 const port = 8000;
 
+// root view users table
 server.get("/", (req, res) => {
   db.select().from('users')
     .then(response => {
@@ -17,5 +18,19 @@ server.get("/", (req, res) => {
       res.status(500).send({ error: error })
     })
 })
+// register user, hash password
+server.post("/api/register", (req, res) => {
+  const { username, password, department } = req.body;
+  const hashedPass = bcrypt.hashSync(password, 10);
 
+  db.insert({
+    "username": username,
+    "password": hashedPass,
+    "department": department
+  }).into('users')
+   .then(response => (res.json(response)))
+   .catch(error => {
+     res.status(500).send({ error: error })
+   })
+});
 server.listen(port, () => { console.log(`Server is running on port ${port}`)});
