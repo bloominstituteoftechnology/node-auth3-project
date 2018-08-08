8e3a3c3d56adb5d705102e44924a8e1adc898033
Simon <Simon_Freelance@hotmail.com>
2018-08-08 15:43:48 -0400

    Added starting code for register and login

--- server.js
+++ server.js
@@ -1,36 +1,53 @@
-const codes = require('./data/statusCodes');
+const codes = require("./data/statusCodes");
 
-const express = require('express');
+const express = require("express");
 const server = express();
 
-
-const userRoutes = require('./api/userRouter');
-
+const userRoutes = require("./api/userRouter");
 
 server.use(express.json());
 
-server.use('/api/users', userRoutes);
+server.use("/api/users", userRoutes);
 
+server.post("/register", (req, res, next) => {
+  db("users")
+    .then(response => {
+      res.status(codes.OK).json(response);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 
+server.post("/login", (req, res, next) => {
+    db("users")
+      .then(response => {
+        res.status(codes.OK).json(response);
+      })
+      .catch(err => {
+        next(err);
+      });
+  });
+  
 
 server.use((err, req, res, next) => {
-    err.code = err.code !== undefined ? err.code : codes.INTERNAL_SERVER_ERROR;
-    const errorInfo = {
-        status: err.code,
-        errorMessage: err.message,
-        success: false,
-        route: err.route
-    }
-    switch (errorInfo.code) {
-        case codes.BAD_REQUEST: 
-            res.status(codes.BAD_REQUEST).json(errorInfo);
-            return;
-        case codes.NOT_FOUND: 
-            res.status(codes.NOT_FOUND).json(errorInfo);
-            return;
-        default:
-        res.status(codes.INTERNAL_SERVER_ERROR).json(errorInfo);
-    }
+  err.code = err.code !== undefined ? err.code : codes.INTERNAL_SERVER_ERROR;
+  const errorInfo = {
+    status: err.code,
+    errorMessage: err.message,
+    success: false,
+    route: err.route
+  };
+  switch (errorInfo.code) {
+    case codes.BAD_REQUEST:
+      res.status(codes.BAD_REQUEST).json(errorInfo);
+      return;
+    case codes.NOT_FOUND:
+      res.status(codes.NOT_FOUND).json(errorInfo);
+      return;
+    default:
+      res.status(codes.INTERNAL_SERVER_ERROR).json(errorInfo);
+  }
 });
 const port = 8001;
 server.listen(port, (req, res) => console.log(`Port ${port} is in use`));
