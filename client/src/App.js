b9c70f14fd2aa4a5acbc2ee1b1adda039f3ef34d
Wdifulvio523 <w.difulvio523@gmail.com>
2018-08-08 17:09:18 -0400

    Set up tomorrow's assignment by using create-react-app to set up my front end application

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
 import './App.css';
+import Signup from './components/signup';
+import Signin from './components/signin';
+import Users from './components/users';
+
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    };
+  }
   render() {
     return (
       <div className="App">
+      <div>Home Route</div>
+      <Route path='/users' component={Users}/>
+      <Route path='/signin' component={Signin}/>
+      <Route path='/signup' component={Signup}/>
 
       </div>
     );
---
+++ client/src/components/signup.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Signup = () => {
+    return ( 
+        <div>Sign Up</div>
+     );
+}
+ 
+export default Signup;
