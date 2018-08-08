aa1963e129f733f3f531b404c8244941eb4b75db
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-08-08 14:38:17 -0600

    created JWT tokens to keep users logged in across requests

--- migrations/20180807141412_users.js
+++ migrations/20180807141412_users.js
@@ -9,6 +9,9 @@ exports.up = function (knex, Promise) {
       tbl
         .string('password')
         .notNullable()
+      tbl
+        .string('department')
+        .notNullable();
     });
   };
   
