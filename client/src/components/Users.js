f46641ccde926edc98fe41bf2566fb0232964d7c
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-08-09 13:04:07 -0600

    Created /users function and attached to render and to api

--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Users = () => {
+class Users extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    }
+  }
+  componentDidMount() {
+    const token = localStorage.getItem('jwt');
+    const requestOptions = {
+      headers: {
+        Authorization: token,
+      },
+    };
+    axios.get('http://localhost:3300/users', requestOptions)
+      .then(response => {
+        this.setState({ users: response.data });
+      })
+      .catch(err => {
+        console.error('Axios Failed');
+      })
+  }
+  render() {
     return (
-        <div>Users</div>
+      <div>
+        <ul>
+          {this.state.users.map(user => {
+            return (
+              <li key={user.id}>{user.username}</li>  
+            )
+          })}
+        </ul>
+      </div>
     );
+  }
 }
- 
+
 export default Users;
