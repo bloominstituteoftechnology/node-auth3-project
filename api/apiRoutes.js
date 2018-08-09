5a47cf398de3d6a849ac238a9496ec834054d928
Tyson Gushiken <tysonism@gmail.com>
2018-08-09 03:48:23 -0700

    Create database

---
+++ api/apiRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const db = require('../data/dbConfig.js');
+const bcrypt = require('bcryptjs');
+const router = express.Router();
+
+router.post('/register', (req, res) => {
+  
+});
+
+router.post('/login', (req, res) => {
+
+});
+
+router.get('/users', (req, res) => {
+
+});
+
+module.exports = router;
