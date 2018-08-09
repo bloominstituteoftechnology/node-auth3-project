3010870359304cbd9120788dfb56bef64f0ff9b0
emaquino44 <prexy44@yahoo.com>
2018-08-09 16:14:26 -0700

    add login component

--- react-auth-ii/src/components/Login/Login.js
+++ react-auth-ii/src/components/Login/Login.js
@@ -1,21 +1,65 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+class Login extends Component {
+    constructor(){
+        super();
+        this.state = {
+            username:'',
+            password:''
+        }
+    }
+
+    inputChangeHandler = event => {
+        const { name, value } = event.target;
+        // console.log('name', name, 'value', value)
+        this.setState({ [name]: value });
+    }
+
+    submitHandler = event => {
+        event.preventDefault();
+
+        axios
+        .post('http://localhost:3300/api/login', this.state)
+        .then(res => {
+            console.log('data', res.data);
+            const token = res.data;
+
+            localStorage.setItem('jwt', token)
+        })
+        .catch(err => {
+            console.error('Axios falied');
+        });
+        console.log('state', this.state)
+       }
 
 
+  render() {
+    return (
+        <div>
+            <h1>Signin Form</h1>
+            <form onSubmit={this.submitHandler}>
+                <div>
+                    <input
 
-class Login extends React.Component {
-   render () {
-     return (
-       <div className="sign-in">
-         <h1>Log In</h1>
-		       <form>
-		         <div>
-				        <input type='text' name='name' placeholder='name' />
-				       	<input type='text' name='password' placeholder='password' />
-		            <button type="button" >Log in</button>
-		         </div>
-		        </form>
-       </div>
-    )}
+                    value={this.state.name}
+                    type="text"
+                    onChange={this.inputChangeHandler} />
+                </div>
+                <div>
+                    <input
+
+                    value={this.state.password}
+                    type="text"
+                    onChange={this.inputChangeHandler} />
+                </div>
+                <div>
+                    <button type="submit"> Signin </button>
+                </div>
+            </form>
+        </div>
+    );
+  }
 }
 
 export default Login;
