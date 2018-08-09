4f55bafdbc0f329b0ed9c28b92a47050cbf1ceae
Simon <Simon_Freelance@hotmail.com>
2018-08-09 17:40:43 -0400

    Prettified register form

--- office-management/src/components/register/RegisterForm.js
+++ office-management/src/components/register/RegisterForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "bulma/css/bulma.css";
-import axios from 'axios';
+import axios from "axios";
 class RegisterForm extends Component {
   constructor(props) {
     super(props);
@@ -14,25 +14,24 @@ class RegisterForm extends Component {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-  registerUserHandler = (e) => {
+  registerUserHandler = e => {
     e.preventDefault();
     const requestBody = {
       ...this.state
-    }
-    axios.post('http://localhost:8001/api/register', requestBody)
-    .then(res => {
-      console.log(res);
-    })
-    .catch(err => {
-      console.error(err);
-    })
-    this.props.history.push('/login');
+    };
+    axios
+      .post("http://localhost:8001/api/register", requestBody)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    this.props.history.push("/login");
   };
   render() {
     return (
-      <form
-        onSubmit={this.registerUserHandler}
-      >
+      <form onSubmit={this.registerUserHandler}>
         <div className="container">
           <div className="field">
             <label className="label">Create Username</label>
@@ -59,17 +58,20 @@ class RegisterForm extends Component {
               />
             </div>
           </div>
-          <select name = "department" onChange = {this.changeHandler}>
-  <option value="admin">Admin</option>
-  <option value="normal" selected>Normal</option>
-  <option value="plebian">Plebian</option>
-</select>
-          <button type="submit" className = "btn is-primary">Create user</button>
+          <select name="department" onChange={this.changeHandler}>
+            <option value="admin">Admin</option>
+            <option value="normal" selected>
+              Normal
+            </option>
+            <option value="plebian">Plebian</option>
+          </select>
+          <button type="submit" className="btn is-primary">
+            Create user
+          </button>
         </div>
       </form>
     );
   }
 }
-
 
 export default RegisterForm;
