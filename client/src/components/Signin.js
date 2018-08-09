2e69c2c1efdb35edc02e5c6ef036188f373d9309
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-08-09 13:03:55 -0600

    Created /signin function and attached to render and to api

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Signin = () => {
+
+class Signin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+  handleChange = (e) => {this.setState({ [e.target.name]: e.target.value });}
+  handleSignin = (e) => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:3300/login', this.state)
+      .then(response => {
+        console.log('SIGNIN RESPONSE: ', response.data)
+        localStorage.setItem('jwt', response.data);
+      })
+      .catch(err => {console.log('Axios failed.', err)})
+    this.setState({ username: '', password: '' })
+  }
+  render() {
     return (
-        <div>Signin</div>
+      <div>
+        <form>
+          <input name='username' value={this.state.username} placeholder='Username...' onChange={this.handleChange} />
+          <input name='password' type='password' value={this.state.password} placeholder='Password...' onChange={this.handleChange} />
+          <button onClick={this.handleSignin}>Login</button>
+        </form>
+      </div>
     );
+  }
 }
- 
+
 export default Signin;
