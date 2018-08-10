3e933c883a0c99fb44683ab9de1eeca812c9b2f8
Tyson Gushiken <tysonism@gmail.com>
2018-08-10 07:02:49 -0700

    Create /signin route

--- client/src/components/Page/SignInView.js
+++ client/src/components/Page/SignInView.js
@@ -1,11 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const SignInView = props => {
-  return (
-    <main className="sign-in-view">
-      <h2>Sign In</h2>
-    </main>
-  );
+const API_URL = 'http://localhost:8000/api/login';
+
+class SignInView extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleInputChange = e => {
+    const stateObj = this.state;
+    stateObj[e.target.name] = e.target.value;
+    this.setState(stateObj);
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    const user = {...this.state};
+    axios
+    .post(API_URL, user)
+    .then(res=> {
+      console.log('posted', res);
+      const token = res.data.token;
+      localStorage.setItem('jwt', token);
+      this.setState({
+        username: '',
+        password: ''   
+      });
+      window.location.href = '/users';
+    })
+    .catch(error => { 
+      console.log(error);
+      alert('There was an error logging in');
+    });    
+    console.log('click sign up', this.state);
+  };
+
+  render() {
+    return (
+      <main className="sign-up-view">
+        <h2>Sign In</h2>
+        <form>
+          <input 
+            name="username" 
+            type="text" 
+            placeholder="Username"
+            value={this.state.username}
+            onChange={this.handleInputChange} />
+          <input 
+            name="password" 
+            type="password" 
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handleInputChange} />
+          <button type="submit" onClick={this.onSubmit}>Sign In</button>
+        </form>
+      </main>
+    );
+  }
 };
 
 export default SignInView;
