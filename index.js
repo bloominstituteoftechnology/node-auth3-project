2599e280998565b5bbca8be43eb7e294b95220b9
Katie Gorbell <katiegorbell@gmail.com>
2018-08-15 16:19:19 -0500

    made get and post requests without JWTs

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const db = require('./data/db');
 const port = 8000;
 
@@ -6,6 +9,45 @@ const server = express();
 
 server.use(express.json());
 
+
+server.post('/register', (req, res) => {
+    const user = req.body;
+
+    const hash = bcrypt.hashSync(user.password, 14);
+    user.password = hash;
+
+    db('users')
+        .insert(user)
+        .then( ids => {
+            db('users')
+                .where({ id: ids[0]})
+                .first()
+                .then( user => {
+                    res.status(201).json(user);
+                })
+        })
+        .catch(err => res.status(500).json(err))
+});
+
+server.post('/login', (req, res) => {
+    const credentials = req.body;
+
+    db('users')
+        .where({ username: credentials.username })
+        .first()
+        .then(user => {
+            res.status(200).json(`Success! ${user.name} is logged in`)
+        })
+        .catch(err => res.status(500).json(err))
+});
+
+server.get('/users', (req, res) => {
+    db('users')
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(err => res.status(500).json(err))
+})
 
 
 server.listen(port, () => console.log(`\n==== API is running on port ${port} ====\n`));
