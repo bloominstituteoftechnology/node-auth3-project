6046b08b778abd5495bb739c06c5f2a41f120cef
Tmbrwolf <timberwolf.eric@gmail.com>
2018-08-15 15:18:19 -0500

    Set up initial server & added starting dependencies

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const db = require('./data/dbConfig');
+
+const server = express();
+
+server.use(express.json());
+
+const port = 8000;
+server.listen(port, () => {console.log(`API server runnning on ${port]}`)});
