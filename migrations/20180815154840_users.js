ba5d01f5d24016880a429813123012939fb16137
Tmbrwolf <timberwolf.eric@gmail.com>
2018-08-15 16:05:26 -0500

    Corrected typo, continuing to endpoints

--- migrations/20180815154840_users.js
+++ migrations/20180815154840_users.js
@@ -1,9 +1,8 @@
-
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', users => {
     users.increments();
 
-    users.string('username').notNullable.unique();
+    users.string('username').notNullable().unique();
     users.string('password').notNullable();
     users.string('department');
   });
