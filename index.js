379036f4136ce284094d659ca37aaa91b721a793
Daniel Chamorro <dan@dcitllc.com>
2018-08-15 17:31:25 -0400

    Added users via register with hased password

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const db = require("./data/db");
+const bcrypt = require("bcryptjs");
 
 const server = express();
 
@@ -7,6 +8,32 @@ server.use(express.json());
 
 server.get("/", (req, res) => {
   res.send("up and running...");
+});
+
+// POST register
+server.post("/api/register", (req, res) => {
+  const user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 10);
+
+  user.password = hash;
+
+  db("users")
+    .insert(user)
+    .then(ids => {
+      const id = ids[0];
+      res.status(201).json({ id, ...user });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+//GET users
+server.get("/api/users", (req, res) => {
+  db("users")
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => res.status(500).json(err));
 });
 
 const port = 3300;
