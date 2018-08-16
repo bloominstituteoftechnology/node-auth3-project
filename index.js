6ceae14904463913609111e9bc55fa1cb82062e7
sean chavez <seanchavez@seanchavezsMBP.home>
2018-08-15 21:15:11 -0700

    Implement 'register' endpoint 'Register' hashes pasword, POSTs it to DB and creates JWT

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const db = require('./data/db');
 
 const server = express();
 server.use(express.json());
@@ -8,6 +9,24 @@ const PORT = 8000;
 
 server.get('/', (req, res) => {
   res.send('Sanity Check');
+});
+
+server.post('/register', (req, res) => {
+  const credentials = req.body;
+  const hash = bcrypt.hashSync(credentials.password, 14);
+  credentials.password = hash;
+
+  db('users').insert(credentials)
+   .then(ids => {
+     db('users').where('id', ids[0]).first()
+       .then(user => {
+         const token = generateToken(user);
+         res.status(201).json(user);
+       });
+   })
+   .catch(err => {
+     res.send(err);
+   });
 });
 
 server.listen(PORT, () => {
