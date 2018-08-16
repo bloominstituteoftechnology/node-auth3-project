2869064f6bf8800474ff22b916c99e29b17820e5
Daniel Chamorro <dan@dcitllc.com>
2018-08-16 13:00:16 -0400

    Refactored- added protected fucntion, generateToken function and refactored /api/register to taje in user with token

--- index.js
+++ index.js
@@ -7,25 +7,62 @@ const server = express();
 
 server.use(express.json());
 
+const secret = "idk what im doing";
+
+function protected(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        return res
+          .status(401)
+          .json({ error: "You shall not pass! Wrong token!" });
+      }
+      req.jwtToken = decodedToken;
+      next();
+    });
+  } else {
+    return res.status(401).json({ error: "You shall not pass! No token!" });
+  }
+}
+
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  };
+  const options = {
+    expiresIn: "1h",
+    jwtid: "8728391"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+
 server.get("/", (req, res) => {
   res.send("up and running...");
 });
 
 // POST register
-server.post("/api/register", (req, res) => {
+server.post("/api/register", function(req, res) {
   const user = req.body;
-
   const hash = bcrypt.hashSync(user.password, 10);
-
   user.password = hash;
 
   db("users")
     .insert(user)
-    .then(ids => {
-      const id = ids[0];
-      res.status(201).json({ id, ...user });
+    .then(function(ids) {
+      db("users")
+        .where({ id: ids[0] })
+        .first()
+        .then(user => {
+          const token = generateToken(user);
+          res.status(201).json(token);
+        });
     })
-    .catch(err => res.status(500).json(err));
+    .catch(function(error) {
+      res.status(500).json({ error: "Could not register user" });
+    });
 });
 
 //GET users
