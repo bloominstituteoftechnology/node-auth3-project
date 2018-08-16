3cee7b1788fceaf2d0a8626660258990147d2a30
sean chavez <seanchavez@seanchavezsMBP.home>
2018-08-15 20:19:35 -0700

    Implement users migration and create database

--- migrations/20180815200154_users.js
+++ migrations/20180815200154_users.js
@@ -1,6 +1,11 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', user => {
+    user.increments();
+    user.string('username').notNullable().unique();
+    user.string('password').notNullable().unique();
+    user.string('department').notNullable()
+  })
 };
 
 exports.down = function(knex, Promise) {
