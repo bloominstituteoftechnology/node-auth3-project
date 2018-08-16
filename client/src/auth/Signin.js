0cadb026268f04b49e9c96b0b686b209cd116ac3
Daniel Chamorro <dan@dcitllc.com>
2018-08-16 18:41:19 -0400

    Added Sign in form to Signin component

--- client/src/auth/Signin.js
+++ client/src/auth/Signin.js
@@ -5,6 +5,17 @@ class Signin extends Component {
     return (
       <div className="Signin">
         <h1>Signin Component</h1>
+        <form>
+          <div>
+            <input type="text" id="username" />
+          </div>
+          <div>
+            <input type="text" />
+          </div>
+          <div>
+            <button type="button">Sign In</button>
+          </div>
+        </form>
       </div>
     );
   }
