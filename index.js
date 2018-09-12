987afeb96ffe6a30902b19b5357af85e9b3b7d76
Charlie Winslow <cwinslow22@yahoo.com>
2018-09-12 15:49:53 -0500

    added POST api/register endpoint

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const db = require('./database/dbConfig.js');
 
 const server = express();
 server.use(express.json());
 server.use(cors());
+
+const secret = 'my voice is my passport';
 
 
 // Endpoints 
@@ -17,8 +20,25 @@ server.get('/', (req, res) => {
 server.post('/api/register', (req, res) => {
     const creds = req.body;
 
+    const hash = bcrypt.hashSync(creds.password, 11);
+    // replace user's password with the hash
+    creds.password = hash;
+    db('users')
+        .insert(creds)
+        .then(ids => {
+            const id = ids[0];
 
-})
+            db('users')
+                .where({ id })
+                .first()
+                .then(user => {
+                    const token = generateToken(user);
+                    res.status(201).json({ id: user.id, token });
+                })
+                .catch(err => res.status(500).send(err));
+        })
+        .catch(err => res.status(500).send(err));
+});
 
 
 server.listen(3900, () => console.log('\nrunning on port 3900\n'));
