d3d50e64bf85f66eb6953eedd9f611df87760379
sca4cs <shannon.atkinson.ca@gmail.com>
2018-09-12 16:14:06 -0400

    user endpoints

---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const db = require('./database/dbConfig.js');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+const secret = 'seecreeettt';
+
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+  };
+  const options = {
+    expiresIn: '1h',
+    jwtid: '12345',
+  };
+  return jwt.sign(payload, secret, options);
+}
+
+server.post('/api/register', (req, res) => {
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 10);
+  creds.password = hash;
+  db('users')
+    .insert(creds)
+    .then(ids => {
+      const id = ids[0];
+
+      db('users')
+        .where({ id })
+        .first()
+        .then(user => {
+          const token = generateToken(user);
+          res.status(201).json({ id: user.id, token });
+        })
+        .catch(err => res.status(500).send(err));
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+function protected(req, res, next) {
+  // read the token string from the Authorization header
+  const token = req.headers.authorization;
+  if (token) {
+    // verify the token
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        // token is invalid
+        res.status(401).json({ message: 'Invalid Token' });
+      } else {
+        // token is valid
+        req.user = { username: decodedToken.username };
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: 'no token provided' });
+  }
+}
+
+server.post('/api/login', (req, res) => {
+  const creds = req.body;
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ token });
+      } else {
+        res.status(401).json({ message: 'You shall not pass!' });
+      }
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+// protect this route, only authenticated users should see it
+server.get('/api/users', protected, (req, res) => {
+  db('users')
+    .select('id', 'username', 'password')
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+
+server.listen(3300, () => console.log('\nrunning on port 3300\n'));
