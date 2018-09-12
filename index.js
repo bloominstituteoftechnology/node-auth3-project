6c990c242360e85ce6ae356c2f3dbf3b83475f31
adamcglee <adamcglee@gmail.com>
2018-09-12 14:45:44 -0700

    updated code to try and get MVP

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ function generateToken(user) {
   };
   const options = {
     expireIn: "1h",
-    jwtid: "12345"
+    jwtid: "12345",
+    subject: `${user.id}` 
   };
   return jwt.sign(payload, secret, options);
 }
@@ -40,8 +41,59 @@ function protected(req, res, next) {
   }
 }
 
-server.get('/', (req, res) => {
-    res.send('Its Alive');
+server.get("/", (req, res) => {
+  res.send("Its Alive!!!");
 });
 
-server.listen(2100, () => console.log("\nrunning on port 3300\n"));
+server.post("/api/register", (req, res) => {
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 10);
+  creds.password = hash;
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      const id = ids[0];
+      db("users")
+        .where({ id })
+        .first()
+        .then(users => {
+          const token = generateToken(user);
+          res
+            .status(201)
+            .json({
+              id: user.id,
+              token,
+              message: "User Registration Successful"
+            });
+        })
+        .catch(err => res.status(500).send(err));
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+server.post("/api/login", (req, res) => {
+  const creds = req.body;
+  db("users")
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ message: 'Login Successful', token });
+      } else {
+        res.status(401).json({ message: "You shall not pass!" });
+      }
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+server.get("api/users", protected, (req, res) => {
+  db("users")
+    .select("id", "username", "password")
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+server.listen(2100, () => console.log("\nrunning on port 2100\n"));
