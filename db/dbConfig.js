9bd6a1c01d370a462ce7bc258f06cf4f70c0eab7
szincone <sawyerzincone@gmail.com>
2018-09-12 13:39:51 -0600

    created migration file and dbconfig

---
+++ db/dbConfig.js
@@ -0,0 +1,6 @@
+"use strict";
+const knex = require("knex");
+
+const knexConfig = require("../knexfile.js");
+
+module.exports = knex(knexConfig.development);
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cores = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-// const db = require("./db/dbConfig.js");
+const db = require("./db/dbConfig.js");
 
 const server = express();
 const PORT = 4000;
