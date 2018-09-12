c1a9b2bfdbb302bc4999f40b29b7a6c3f7bc095e
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-09-12 16:18:17 -0400

    knex migration create_users_table made and used

---
+++ data/migrations/20180912161446_create_user_table.js
@@ -0,0 +1,23 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(tbl) {
+    tbl.increments();
+
+    tbl
+      .string('username', 255)
+      .notNullable()
+      .unique('uq_username');
+
+    tbl
+      .string('password')
+      .notNullable();
+
+    tbl
+      .string('department')
+      .notNullable()
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
+};
