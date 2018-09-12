8674ce142a7cb2250b3ec970193cac4a768a3a7c
jaymzsocool <james.mcraejr@gmail.com>
2018-09-12 16:57:50 -0400

    added middleware and reccreated endpoints

--- index.js
+++ index.js
@@ -10,6 +10,36 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+const secret = 'you will never know'
+
+generateToken = (user) => {
+    const payload = {
+        username: user.username,
+    };
+    const options = {
+        expiresIn: '1h',
+        jwtid: '12345', // jti
+    };
+    return jwt.sign(payload, secret, options);
+};
+
+protected = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (token) {
+        jwt.verify(token, secret, (err, decodedToken) => {
+            if (err) {            
+            res.status(401).json({ message: 'Invalid Token' });
+            } else {            
+            console.log(decodedToken);
+            req.user = { username: decodedToken.username };    
+            next();
+            }
+        });
+    } else {
+      res.status(401).json({ message: 'no token provided' });
+    }
+}
+
 server.get('/', (req, res) =>{
     res.send('running')
 })
@@ -22,11 +52,33 @@ server.post('/api/register', (req, res) =>{
         .insert(creds)
         .then(ids => {
             const id = ids[0]
-            res.status(201).json(id)
+            db('users')
+                .where({ id })
+                .first()
+                .then(user => {
+                    const token = generateToken(user);
+                    res.status(201).json({ id: user.id, token });
+                }).catch(err => res.status(500).send(err));
         }).catch(err=> res.status(500).send(err))
 })
 
-server.get('/api/users', (req, res) => {    
+server.post('/api/login', (req, res) => {
+    const creds = req.body;
+  
+    db('users')
+      .where({username: creds.username})
+      .first()
+      .then(user =>{
+        if (user && bcrypt.compareSync(creds.password, user.password)){
+            const token = generateToken(user);
+            res.status(200).send({token})
+        } else {
+            res.status(401).json({message: 'The username or password incorrect.'})
+        }
+      })
+  })
+
+server.get('/api/users', protected, (req, res) => {    
     db('users')
         .select('id', 'username')
         .then(users => {
