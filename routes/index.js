5f0ed906e52b1cc0c78fb7460f2e2b2d41e88d8a
kamalnrf <kamalnrf@hotmail.com>
2018-09-13 00:53:14 +0530

    CHANES get/user endpoint returns the loged in user department members

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const secret = process.env.SECRET || "jwt-secret";
-function genToken(username) {
+function genToken(username, department) {
   const options = {
     expiresIn: "1h",
     jwtid: "1998"
   };
-  return jwt.sign({ username }, secret, options);
+  return jwt.sign({ username, department }, secret, options);
 }
 
 function getRandomArbitrary(min, max) {
@@ -49,7 +49,7 @@ router.post("/login", async (req, res, next) => {
     if (await bcrypt.compare(req.body.password, hashPass[0].password))
       res
         .status(200)
-        .json({ status: true, token: genToken(req.body.username) });
+        .json({ status: true, token: genToken(req.body.username, req.body.department) });
     else res.status(200).json({ status: false });
   } catch (err) {
     next(err);
--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,32 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const db = require("../database/dbConfig");
 const jwt = require("jsonwebtoken");
 
 const secret = process.env.SECRET || "jwt-secret";
 
-function _protected (req, res, next) {
-  const token = req.headers.authorization
-  if (token) jwt.verify(token, secret, (err, decodeToken) => err ? next(err): next())
-  else next(new Error('No token in header'))
+function _protected(req, res, next) {
+  const token = req.headers.authorization;
+  if (token)
+    jwt.verify(token, secret, (err, decodeToken) => {
+      if (err) next(err);
+      req.username = decodeToken.username;
+      req.department = decodeToken.department;
+      next();
+    });
+  else next(new Error("No token in header"));
 }
 
-router.get('/', _protected, async (req, res, next) => {
+router.get("/", _protected, async (req, res, next) => {
   try {
-    res.status(200).json(await db(`users`).select("id", "username"));
+    res.status(200).json(
+      await db(`users`)
+        .where({ department: req.department })
+        .select("id", "username", "department")
+    );
   } catch (err) {
     next(err);
   }
-})
+});
 
 module.exports = router;
