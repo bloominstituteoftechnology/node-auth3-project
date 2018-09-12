647c2cabba50c8e61bd4f4d1e06ffa90f7f1eadc
Charles Cortinas <crcortinas@gmail.com>
2018-09-12 17:20:22 -0500

    Day 1 mvp
    Day 1 mvp complete.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const dbConfig = require('./knexfile');
 const cors = require('cors');
 const helmet = require('helmet');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const db = knex(dbConfig.development);
 const server = express();
@@ -12,7 +13,41 @@ server.use(express.json());
 server.use(cors());
 server.use(helmet());
 
-server.get('/', (req, res) => {
+const secret = 'the road to hell is paved with good intentions';
+
+function generateToken (user) {
+  const payload = {
+    username: user.username,
+    department: user.department
+  }
+
+  const options = {
+    expiresIn: '1hr',
+    jwtid: '12345'
+  }
+
+  return jwt.sign(payload, secret, options);
+}
+
+function protected(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: 'You shall not pass!' });
+      } else {
+        req.user = { username: decodedToken.username };
+
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: 'no token provided' });
+  }
+}
+
+server.get('/api/users', protected, (req, res) => {
   db('users')
     .then((data) => {
       res.status(200).json(data);
@@ -22,18 +57,47 @@ server.get('/', (req, res) => {
     })
 });
 
-server.post('/', (req, res) => {
+server.post('/api/register', (req, res) => {
   let user = req.body;
   user.password = bcrypt.hashSync(user.password, 8);
 
   db('users').insert(user)
-    .then((data) => {
-      res.status(201).json(data);
+    .then((ids) => {
+      id = ids[0];
+
+      db('users')
+        .where({ id: id })
+        .first()
+        .then((user) => {
+          const token = generateToken(user);
+          res.status(201).json({ id: user.id, token})
+        })
+        .catch((err) => {
+          res.status(500).send(err);
+        })
     })
     .catch((err) => {
-      console.error(err);
+      res.status(500).send(err);
     })
 })
+
+server.post('/api/login', (req, res) => {
+  const creds = req.body;
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({ token });
+      } else {
+        res.status(401).json({ message: 'You shall not pass!' });
+      }
+    })
+    .catch(err => res.status(500).send(err));
+});
 
 server.listen(8000, () => {
   console.log('== LISTENING ON PORT 8K ==');
