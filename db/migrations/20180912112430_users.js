508a33bccedcda6cac487bcfd36bdbb0a30cf068
dasgma <dasgrigoma@gmail.com>
2018-09-12 13:19:36 -0700

    MVP done

--- db/migrations/20180912112430_users.js
+++ db/migrations/20180912112430_users.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex, Promise) {
+    return knex.schema.createTable("users", table => {
+      table.increments()
+      table.string('username', 128)
+          .notNullable()
+          .unique()
+      table.string('password')
+          .notNullable()
+      table.string('department', 50)
+          .notNullable()
+    })
+  };
   
-};
-
-exports.down = function(knex, Promise) {
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('users');
+  };
   
-};
--- index.js
+++ index.js
@@ -13,32 +13,34 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+const secret = 'secret';
 
 // ####### Protected middleware ##########
-function protected (req, res, next) {
+function protected(req, res, next) {
     const token = req.headers.authorization;
-
+  
     if (token) {
-        jwt.verify(token, secret, (err, decodedToken) => {
-            if (error) {
-                return res.status(400).json({Message: 'Invalid token'});
-            }
-            req.jwtToken - decodedToken;
-            next();
-        });
+      jwt.verify(token, secret, (error, decodedToken) => {
+        if (error) {
+          return res
+            .status(400)
+            .json({ Message: ' Invalid token' })
+        }
+  
+        req.jwtToken = decodedToken;
+        next()
+      })
+    } else {
+      return res.status(400).json({ Message: 'No token found' })
     }
-    else {
-        return res.status(500).json({Message: 'No token found!'});
-    }
-}
+  }
 
 // ########## Generating token ###########
 function generateToken(user){
     const payload = {
-        username: user.username
+        "username": user.username
     }
 
-    const secret = 'secret';
     const options = {
         expiresIn: '1h',
         jwtid: '12345'
@@ -52,38 +54,59 @@ server.get('/', (req, res) => {
 });
 
 // ########## Getting the users ############
-server.get('/users', protected, (req, res) => {
+server.get('/users', (req, res) => {
     db('users')
         .then(users => {
             res.status(200).json(users)
+        })
+        .catch(error => {
+            res.status(500).json({Message: 'Can not get users!', error})
+        })
+})
+
+// ###### Registering newUser ############
+server.post('/register', (req, res) => {
+    const newUser = req.body;
+    const hash = bcrypt.hashSync(newUser.password, 14);
+    newUser.password = hash;
+
+    db('users')
+    .insert(newUser)
+    .then(ids => {
+      db('users')
+        .where({ id: ids[0] })
+        .first()
+        .then(newUser => {
+          const token = generateToken(newUser);
+          res.status(201).json(token);
+        });
+    })
+    .catch(function(error) {
+      res.status(500).json({ error });
+    });
+})
+
+// ########### Login ##############
+server.post('/login', (req, res) => {
+    const creds = req.body;
+
+    db('users')
+        .where({username: creds.username})
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(creds.password, user.password)) {
+                const token = generateToken(user);
+                res.status(200).json(`Hello ${user.username}, you are logged in!!!`)
+            } 
+            else {
+                return res.status(400).json({Message: 'Wrong credentials'})
+            }
         })
         .catch(error => {
             res.status(500).json(error)
         })
 })
 
-// ###### Registering newUser ############
-server.post('/register', (req, res) => {
-    req.body.password = bcrypt.hashSync(req.body.password, 14);
-
-    let { username, password } = req.body;
-    let user = { username, password };
-
-    db('users')
-        .insert(user)
-        .then(ids => {
-            db('users')
-            .where({ id: ids[0] })
-            .first()
-            .then(user => {
-                const token = generateToken(user);
-                res.status(201).json(token);
-            })
-            .catch(error => {
-                res.status(500).json(error);
-            })
-        })
-})
 
 
 server.listen(4000);
