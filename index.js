9c540951d6a0073bfaca5f6d0c7e61af9f7a49d2
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-09-12 15:01:44 -0500

    Added a new function
    created the protected function.

--- index.js
+++ index.js
@@ -1,44 +1,65 @@
-const express = require("express"); 
-const cors = require("cors"); 
-const bcrypt = require("bcrypt"); 
-const jwt = require("jsonwebtoken"); 
-const session = require('express-session'); 
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
 
-const KnexSessionStore = require('connect-session-knex')(session); 
+const KnexSessionStore = require("connect-session-knex")(session);
 
-const db = require('./database/dbConfig'); 
+const db = require("./database/dbConfig");
 
-const server = express(); 
+const server = express();
 
-server.use(express.json()); 
-server.use(cors()); 
+server.use(express.json());
+server.use(cors());
 
-const secret = "one two three"; 
+const secret = "one two three";
 
-function generateToken(user){
-    const payload={
-        username: user.username, 
-    };
-    const options = {
-        expiresIn: "1hr",
-        jwtid: "12345", 
-    }; 
-    return kwt.sign(payload, secret, options); 
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  };
+  const options = {
+    expiresIn: "1hr",
+    jwtid: "12345"
+  };
+  return kwt.sign(payload, secret, options);
 }
-server.post('/api/register', (req, res)=>{
-    const creds = req.body;
-    const hash = bcrypt.hashSync(creds.password, 10); 
-    creds.password = hash; 
-    db('users')
-        .insert(creds)
+server.post("/api/register", (req, res) => {
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 10);
+  creds.password = hash;
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      const id = ids[0];
+      db("users")
+        .where({ id })
+        .first()
         .then(user => {
-                const token = generateToken(user);
-                res.status(201).json({id:user.id, token}); 
+          const token = generateToken(user);
+          res.status(201).json({ id: user.id, token });
         })
-})
+        .catch(err => res.status(500).send(err));
+    })
+    .catch(err => res.status(500).send(err));
+});
 
-server.get("/", (req, res)=>{
-    res.send("welcome"); 
-}); 
+function protected(req, res, next){
+    const token = req.headers.authorization;
+    if(token){
+        jwt.verify(token, secret, (err, decodedToken)=>{
+            if(err){
+                res.status(401).json({message: "Invalid Token"});
+            }else{
+                req.user = { username: decodedToken.username}; 
+            }
+        })
+    }
+}
 
-server.listen(3300, () => console.log("\n Server 3300 is running \n"))
+server.get("/", (req, res) => {
+  res.send("welcome");
+});
+
+server.listen(3300, () => console.log("\n Server 3300 is running \n"));
