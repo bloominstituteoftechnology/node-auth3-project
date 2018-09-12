82722b90667a09063fad57d767173931da5e9eca
Carey <careycbaldwin@gmail.com>
2018-09-12 14:54:39 -0400

    set up server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex');
+const sessions = require('express-session');
+const jwt = require('jsonwebtoken');
+
+const dbConfig = require('./knexfile');
+
+const db = knex(dbConfig.development);
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json()); // don't forget this
+
+//start server
+server.get('/', (req, res) => {
+res.send('API Running...');
+});
+
+server.listen(9000);
