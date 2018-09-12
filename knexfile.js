a5a4afa4ac2f548bf2d6c2817bdfa6de66f98565
Grant Reighard <grant.reighard@gmail.com>
2018-09-12 15:11:19 -0400

    Initial commit.

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/lambda.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    }
+  },
+
+  
+
+};
