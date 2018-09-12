bda9a1357b0fc1402a121feaf08d4b279614fbb4
MichaelBoegner <cmikeb@gmail.com>
2018-09-12 15:46:13 -0400

    API Built and Functioning with Requested User Schema

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
+const bcrypt = require("bcryptjs");
+
+const dbConfig = require("./knexfile");
+
+const db = knex(dbConfig.development);
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+server.get("/", (req, res) => {
+    res.send('hallo, it be working mon!')
+})
+
+const port = 9800;
+server.listen(port, function() {
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
