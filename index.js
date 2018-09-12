715ab5779c0c2e939a8abf0b6b09e02aa7004562
Erik Kimsey <erik@erikkimsey.com>
2018-09-12 16:56:16 -0400

    Created POST for register, generateToken method, and started on protected check middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // https://github.com/LambdaSchool/auth-ii/pull/172
 
 const express = require('express');
+const db = require('./dbConfig.js');
 const jwt = require('jsonwebtoken');
 const express_jwt = require('express-jwt');
 const bcrypt = require('bcryptjs');
@@ -12,7 +13,51 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req,res)=>{
+    res.send('YOYO');
+})
+
 const secret = 'somesecret';
+
+function generateToken(user){
+    let payload = {
+        name: user.name
+    };
+    let options = {
+        expiresIn: '2h',
+        jwtid:'1212'
+    };
+    return jwt.sign(payload, secret, options);
+}
+
+app.post('/api/register', (req,res)=>{
+    const creds = req.body;
+    const hash = bcrypt.hashSync(creds.password, 10);
+    creds.password = hash;
+    db('users')
+        .insert(creds)
+        .then(ids => {
+            const id = ids[0];
+            db('users')
+                .where({'id':id})
+                .first()
+                .then(user => {
+                    const token = generateToken(user);
+                    res.status(201).json({id: user.id, token})
+                })
+                .catch(err => {
+                    console.log(`Error registering: ${err}`);
+                    res.status(500).json({message: `Sorry`})
+                })
+        })
+});
+
+function protected(req, res, next){
+    const token = req.headers.authorization;
+    if(token){
+        jwt.verify(token, secret)
+    }
+}
 
 app.listen(PORT, console.log(`Listening on port ${PORT}`)
 );
