c1df258bd5ede8d459e93bb8dd46b8710f4a326a
codejoncode <jonathanjholloway@gmail.com>
2018-09-12 14:33:08 -0500

    warning message due to useNullasDefault instead of useNullAsDefault  typo fixed warning message  gone.

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
     connection: {
       filename: './dev.sqlite3'
     },
-    useNullasDefault: true, 
+    useNullAsDefault: true, 
     migrations : {
       directory: "./db/migrations"
     },
