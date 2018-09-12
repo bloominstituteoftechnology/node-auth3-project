35d3d8bd9ebfdaed7659d83017cb6e9708966027
Erik Kimsey <erik@erikkimsey.com>
2018-09-12 19:11:53 -0400

    Commiting before standup.  Created POST for login and began building out the GET users request.

--- index.js
+++ index.js
@@ -55,9 +55,50 @@ app.post('/api/register', (req,res)=>{
 function protected(req, res, next){
     const token = req.headers.authorization;
     if(token){
-        jwt.verify(token, secret)
+        jwt.verify(token, secret, (err, decodedPayload)=>{
+            if(err){
+                res.status(401).json({message: 'Invalid token'})
+            } else {
+                console.log(`Decoded token: ${decodedPayload}`);
+                req.user = { name: decodedPayload.name };
+                next();
+            }
+        })
+    } else {
+        res.status(401).json({message: 'Sorry, bub. No token was provided.'});
     }
 }
+
+app.post('/api/login', (req,res)=>{
+    const creds = req.body;
+    db('users')
+        .where('name', creds.name)
+        .first()
+        .then(user => {
+            if(user && bcrypt.compareSync(user.password, user.hash)){
+                const token = generateToken(user);
+                res.status(201).json({message: `Successful login, here have a token: ${token}`});
+            } else {
+                res.status(401).json({message: `Invalid login info.`});
+            }
+        })
+        .catch(err => {
+            console.log(`Error: ${err}`);
+            res.status(501).json({message: 'Sorry, '})
+        });
+});
+
+app.get('api/users', protected, (req,res)=>{
+    // I don't understand -->  "Use this endpoint to verify that the password is hashed before it is saved."
+    db('users')
+        .select('name', 'department')
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({message: `Sorry, unable to complete request for users`})
+        })
+})
 
 app.listen(PORT, console.log(`Listening on port ${PORT}`)
 );
