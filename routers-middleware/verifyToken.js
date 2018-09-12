fca6f2d0db020634f9fa68dc6a618b410a327e45
codejoncode <jonathanjholloway@gmail.com>
2018-09-12 15:01:05 -0500

    created the get users route still ned to connect it to the server though

---
+++ routers-middleware/verifyToken.js
@@ -0,0 +1,19 @@
+function verifyToken(req, res, next) {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "Invalid Token" });
+      } else {
+        console.log(decodedToken);
+        req.user = { username: decodedToken.username };
+
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "no token provided" });
+  }
+}
+
+module.exports = verifyToken; 
--- routers/router-get-users.js
+++ routers/router-get-users.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
-const generateToken = require("../routers-middleware/generateToken.js")
-const usersRouter = express.Router(); 
+const verifyToken = require("../routers-middleware/verifyToken.js");
+const usersRouter = express.Router();
 
 const db = require("../db/dbConfig.js");
+
+usersRouter.get("/", verifyToken, (req, res) => {
+  db("users")
+    .select("id", "username", "department")
+    .then(users => {
+      if (users) {
+        res.status(200).json(users);
+      } else {
+        res.status(500).json({ errorMessage: "Problems with your request" });
+      }
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    });
+});
