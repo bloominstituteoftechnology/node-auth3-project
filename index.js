91f8485fdbccfdcd5b95542cfe125b7fb8faae10
francistse23 <wtse.ucsd@gmail.com>
2018-09-12 12:05:56 -0700

    POST to /api/register, creates user and hashes password.

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const server = express();
 const cors = require('cors');
+const knex = require('knex');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+
+const dbConfig = require('./knexfile');
+const db = knex(dbConfig.development);
 
 server.use(express.json());
 server.use(cors());
 
 const secret = 'lambda-school-fsw12';
-
 
 generateToken = user => {
     const payload = {
@@ -22,7 +25,25 @@ generateToken = user => {
     return jwt.sign(payload, secret, options);
 }
 
-
+server.post('/api/register', async (req, res) => {
+    const creds = req.body;
+    const hash = bcrypt.hashSync(creds.password, 10);
+    creds.password = hash;
+    if ( !creds.username || !creds.password || !creds.department) {
+        res.status(400).json({
+            message: "All 3 fields are required."
+        })
+    } else {
+        try {
+            const newUser = await db('users').insert(creds);
+            const token = generateToken(newUser);
+            res.status(201).json({ id: newUser.id, token });
+        }
+        catch ( err ) {
+            res.status(500).json( err.message );
+        }
+    }
+});
 
 
 
