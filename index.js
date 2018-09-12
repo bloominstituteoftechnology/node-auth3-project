8ceee3058605445b91061130fb89a2daa38a7049
Charlie Winslow <cwinslow22@yahoo.com>
2018-09-12 16:34:31 -0500

    added POST api/login endpoint

--- index.js
+++ index.js
@@ -53,8 +53,21 @@ server.post('/api/register', (req, res) => {
 });
 
 server.post('/api/login', (req, res) => {
-    
-})
+    const creds = req.body;
+
+    db('users')
+        .where({ username: creds.username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(creds.password, user.password)) {
+                const token = conjureToken(user);
+                res.status(200).json({ token });
+            } else {
+                res.status(401).json({ message: 'You shall not pass' });
+            }
+        })
+        .catch(err => res.status(500).send(err));
+});
 
 
 server.listen(3900, () => console.log('\nrunning on port 3900\n'));
