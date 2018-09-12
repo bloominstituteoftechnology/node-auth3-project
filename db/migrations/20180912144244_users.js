af49bb0cb47a0ba521dda7ed7790351a27ea2b9e
Carey <careycbaldwin@gmail.com>
2018-09-12 14:49:33 -0400

    user table includes username, password, department

--- db/migrations/20180912144244_users.js
+++ db/migrations/20180912144244_users.js
@@ -1,6 +1,25 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', users => {
+        //ID
+        users.increments();
+
+        //USERNAME
+        users
+        .string('username', 128)
+        .notNullable()
+        .unique();
+
+        //PASSWORD
+        users
+        .string('password', 128)
+        .notNullable();
+
+        //DEPARTMENT
+        users
+        .string('department', 128)
+        .notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
