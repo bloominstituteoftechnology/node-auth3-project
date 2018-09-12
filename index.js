580885dc6b5f460f09b94caededbd07428314f22
Josue Peralta <jperal77@gmail.com>
2018-09-12 17:58:40 -0500

    added users migration & token function

--- index.js
+++ index.js
@@ -10,9 +10,37 @@ const server = express();
 
 server.use(express.json());
 
+function generateToken(user) {
+    const payload = {
+        username: user.username
+    };
+
+    const secret = 'The meaning of life is 42';
+
+    const options = {
+        expiresIn: '1h',
+        jwtid: '12345' 
+    };
+
+    return jwt.sign(payload, secret, options)
+}
 
 server.get('/', (req, res) => {
     res.send('API Running...');
+});
+
+server.post('/api/register', (req, res) => {
+    const credentials = req.body;
+
+    const hash = bcrypt.hashSync(credentials.password, 3);
+
+    credentials.password = hash;
+
+    db('users').insert(credentials).then(ids => {
+        const id = ids[0];
+
+        res.status(200).json(id);
+    }).catch(err => res.status(500).send(err));
 });
 
 server.listen(8000);
