ff8766dd97d5fea282fa7f8596f8a250cf7baaa3
Josue Peralta <jperal77@gmail.com>
2018-09-12 17:27:53 -0500

    added boilerplate

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const knex = require('knex');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const dbConfig = require('./knexfile');
+const db = knex(dbConfig.development);
+
+const server = express();
+
+server.use(express.json());
+
+
+server.get('/', (req, res) => {
+    res.send('API Running...');
+});
+
+server.listen(8000);
