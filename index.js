fab7e8f715c7c889dbf4a5713dada3d8b8ec9f84
szincone <sawyerzincone@gmail.com>
2018-09-12 14:03:45 -0600

    created generateToken func and protected middleware

--- index.js
+++ index.js
@@ -11,6 +11,37 @@ server.use(express.json());
 server.get("/", (req, res) => {
   res.status(201).send("Working");
 });
+// generate token function
+const generateToken = user => {
+  const payload = {
+    username: user.username,
+  };
+  const secret = "rosebud";
+  const options = {
+    expiresIn: "1h",
+    jwtid: "12345",
+  };
+  return jwt.sign(payload, secret, options);
+};
+// protected middleware
+const protected = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        // wrong token
+        res.status(401).json({ message: "Invalid Token" });
+      } else {
+        // good token
+        req.user = { username: decodedToken.username };
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "Missing Token" });
+  }
+};
+// end middleware
 
 server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
