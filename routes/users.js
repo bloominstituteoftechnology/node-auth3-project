25473f0a15f2181c5b44bf778fc055010a0cb797
kamalnrf <kamalnrf@hotmail.com>
2018-09-13 00:36:56 +0530

    Added GEt Users end point

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../database/dbConfig");
+const jwt = require("jsonwebtoken");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const secret = process.env.SECRET || "jwt-secret";
+
+function _protected (req, res, next) {
+  const token = req.headers.authorization
+  if (token) jwt.verify(token, secret, (err, decodeToken) => err ? next(err): next())
+  else next(new Error('No token in header'))
+}
+
+router.get('/', _protected, async (req, res, next) => {
+  try {
+    res.status(200).json(await db(`users`).select("id", "username"));
+  } catch (err) {
+    next(err);
+  }
+})
 
 module.exports = router;
