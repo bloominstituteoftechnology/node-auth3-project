65821fc635abff3b73aa43b6c439456afa241816
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-09-13 18:54:40 -0400

    SignUp feature added

--- pt-ii-client/src/App.js
+++ pt-ii-client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Route, withRouter } from "react-router-dom";
 import Signin from "./auth/Signin";
+import Signup from "./auth/Signup";
 import Users from "./users/Users";
 
 import logo from "./logo.svg";
@@ -12,12 +13,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
           <div>
             <button onClick={this.logout}>Logout</button>
           </div>
         </header>
 
+        <Route path="/signup" component={Signup} />
         <Route path="/signin" component={Signin} />
         <Route path="/users" component={Users} />
       </div>
