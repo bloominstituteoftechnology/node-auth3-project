58dfee5b31893522e0792f577c2f81dbc055275b
Alberto-Funk <laptopreneur@gmail.com>
2018-09-12 18:52:43 -0700

    MVP complete .. i think

--- server.js
+++ server.js
@@ -15,6 +15,41 @@ server.use(express.json());
 server.use(cors());
 server.use(helmet());
 
+const secret = 'seecreeettt';
+
+
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+    roleId: user.roleId
+  };
+  const options = {
+    expiresIn: '1h',
+    jwtid: '12345'
+  };
+  return jwt.sign(payload, secret, options);
+}
+
+
+function protected(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        // keep track if there is an error, a log of users trying to log in with invalid token
+        res.status(401).json({ message: 'Invalid Token' });
+      } else {
+        console.log(decodedToken);
+        req.user = { username: decodedToken.username, roleId: decodedToken.roleId };
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: 'no token provided' });
+  }
+}
+
 
 server.get('/', (req, res) => {
   res.send('hello');
@@ -27,6 +62,7 @@ server.get('/api/users', (req, res) => {
   .join('roles', 'users.roleId', 'roles.roleId')
   .join('departments', 'departments.departmentId', 'roles.departmentId')
   .then(users => {
+    console.log(users)
     res.status(200).json(users)
   })
   .catch(err => console.log(err));
@@ -60,6 +96,72 @@ server.get('/api/roles', (req, res) => {
 /* === === === */
 
 
+
+server.post('/api/register', (req, res) => {
+
+  !req.body.username || !req.body.password || !req.body.roleId ?
+  res.status(400).json({message: 'You need a username AND password AND role'})
+  :
+  null
+
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 10);
+  creds.password = hash;
+  creds.roleId = parseInt(creds.roleId, 10);
+  db('users')
+    .insert(creds)
+    .then(ids => {
+      const id = ids[0];
+
+      db('users')
+        .where({ userId: id })
+        .first()
+        .then(user => {
+          const token = generateToken(user);
+          res.status(201).json({ id: user.userId, token });
+        })
+        .catch(err => res.status(500).send('err 1'));
+    })
+    .catch(err => res.status(500).send('err 2'));
+});
+
+
+server.post('/api/login', (req, res) => {
+  const creds = req.body;
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({ token });
+      } else {
+        res.status(401).json({ message: 'You shall not pass!' });
+      }
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+
+server.get('/api/protected/users', protected, (req, res) => {
+  const { roleId } = req.user;
+  db('users')
+    .select('username as User', 'role_name as Role', 'department_name as Department', 'users.roleId')
+    .join('roles', 'users.roleId', 'roles.roleId')
+    .join('departments', 'departments.departmentId', 'roles.departmentId')  
+    //.where({roleId})
+    .then(users => {
+      console.log(users)
+      let matchedUsers = [];
+      matchedUsers = users.filter(users => {
+        return users.roleId === roleId;
+      });
+      res.json(matchedUsers);
+    })
+    .catch(err => res.send(err));
+});
 
 
 server.listen(port, () => console.log(`=== Listening on port ${port} ===`));
