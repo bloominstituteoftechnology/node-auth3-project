d6d0cf92e0c7eafdc6cc072e8de0d9ee44e2181e
kamalnrf <kamalnrf@hotmail.com>
2018-09-14 00:19:49 +0530

    Added cors

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
