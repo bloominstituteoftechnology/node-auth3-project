54163579d7e08fdde28e54265b62736b8cf6fa0a
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-09-13 18:41:00 -0400

    Users created

---
+++ pt-ii-client/src/users/Users.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class Users extends Component {
+  state = {
+    users: []
+  };
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.users.map(user => (
+            <li key={user.id}>{user.username}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    const token = localStorage.getItem("jwt");
+    const reqOptions = {
+      headers: {
+        Authorization: token
+      }
+    };
+
+    axios
+      .get("http://localhost:3300/api/users", reqOptions)
+      .then(res => {
+        console.log(res.data);
+        this.setState({ users: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+}
+
+export default Users;
