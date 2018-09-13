da49be22772161d15ed2c4ecd777e8aca3668c52
William <kwontaewoongah@gmail.com>
2018-09-12 22:11:40 -0700

    finished day 1 mvp

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ generateToken=(user)=>{
 protected=(req,res,next)=>{
     const token=req.headers.authorization;
     if (token) {
-        jwt.verify(token, secret,(err,decodedToken)=>{
+        jwt.verify(token, secret, (err,decodedToken)=>{
             if (err) {
                 res.status(401).json({message:'invalid token.'})
             } else {
