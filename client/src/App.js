a16cd8a74f189d17a732fcdf3b56c88858fde415
Charlie Winslow <cwinslow22@yahoo.com>
2018-09-12 23:15:59 -0500

    starting to work on the signup component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Link, Route } from "react-router-dom";
+
 import './App.css';
 
 class App extends Component {
@@ -7,7 +8,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
---
+++ client/src/signUp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import axios from "axios";
+
+class SignUp extends Component {
+    constructor() {
+        super();
+        this.state = {
+            username: '',
+            password: '',
+            department: ''
+        }
+    }
+}
