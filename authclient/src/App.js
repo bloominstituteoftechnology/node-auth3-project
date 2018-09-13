afa5818a04f6f3d70c05e5ef602a2a1bb96b9d69
Josue Peralta <jperal77@gmail.com>
2018-09-13 18:08:40 -0500

    added logout button

--- authclient/src/App.js
+++ authclient/src/App.js
@@ -6,12 +6,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  logout = e => {
+    localStorage.removeItem('jwt');
+  }
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <div>
+            <button onClick={this.logout}>Logout</button>
+          </div>
         </header>
         <Route path="/signin" component={Signin} />
         <Route path="/users" component={Users} />
