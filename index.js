def315ed334000054607a2d0dcf41ef0015e9c6b
Sania Parekh <sparekh@Sanias-MacBook.local>
2018-09-19 15:51:59 -0500

    added depencies and api information

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+const db = require('./database/dbConfig.js');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+
+
+
+const port = 6600;
+server.listen(port, function() {
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
