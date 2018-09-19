a2bc706e4af4c1885a366b8075aa2de59e16e313
Kairi <kairijb@gmail.com>
2018-09-19 17:43:45 -0500

    created user table

---
+++ migrations/20180919173733_user.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('user', function(tbl) {
+      tbl.increments();
+
+      tbl.string('username', 128).notNullable().unique();
+
+      tbl.string('password', 128).notNullable();
+
+      tbl.string('department', 128).notNullable();
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('user')
+};
