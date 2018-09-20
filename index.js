031bd56a8d9d3d8a6389b741f9ea955234e9f242
Kairi <kairijb@gmail.com>
2018-09-19 23:14:07 -0500

    created register route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const helmet = require('helmet');
 const knex = require('knex')
 const cors = require('cors')
 const server = express();
+const bcrypt = require('bcryptjs');
 const dbConfig = require('./db/knexfile')
 const jwt = require('jsonwebtoken')
 
@@ -34,7 +35,31 @@ server.get('/', (req, res) => {
     res.send('Api Online')
 })
 
+server.post('/api/register', (req, res) => {
+    const creds = req.body
 
+    const hash = bcrypt.hashSync(creds.password, 5)
+
+    creds.password = hash
+
+    db('user').insert(req.body)
+    .then(ids => {
+        const id = ids[0]
+        db('user').where({id}).first()
+        .then(user => {
+            const token =generateToken(user)
+            res.status(201).json({id: user.id, token})
+        })
+        .catch(err => {
+            console.log('/api/register err ', err)
+            res.status(500).json({'message': err })
+        })
+    })
+    .catch(err => {
+        console.log('post error', err)
+        res.status(500).json(err)
+    })
+})
 
 
 
