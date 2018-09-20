0f1e4fe8e5803c7d2c3cdaf4ee800bc44d03d8e2
cubsfan46350 <cubsfan.46350@msn.com>
2018-09-20 16:00:29 -0500

    created sign in form

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import SignIn from './auth/SignIn';
+import { Route } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route path="/signin" component={SignIn} />
       </div>
     );
   }
---
+++ auth/src/auth/SignIn.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+class SignIn extends Component {
+  render() {
+    return (
+        <form>
+            <div>
+                <label>Username:</label>
+                <input type="text" />
+            </div>
+            <div>
+                <label>Password:</label>
+                <input type="text" />
+            </div>
+            <div>
+                <button type="submit">Signin</button>
+            </div>
+        </form>
+    );
+  }
+}
+
+export default SignIn;
