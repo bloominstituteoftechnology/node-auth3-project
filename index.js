f3c66b6dd97393a1846757bdc92c6bab6720ed26
Kairi <kairijb@gmail.com>
2018-09-19 23:59:59 -0500

    created get user route

--- index.js
+++ index.js
@@ -91,7 +91,16 @@ server.post('/api/login', (req, res) => {
         .catch(err => res.status(500).json({message: err}))
 })
 
-
+server.get('/api/user', protected, (req,res) => {
+    db('user')
+    .select('id', 'username', 'password', 'department')
+    .then(user => {
+        res.status(200).json(user)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+})
 
 
 
