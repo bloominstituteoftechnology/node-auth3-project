430a37aacc4904f3314a03a3d533e251a3fccb3b
AlexanderCaves <ahcaves@gmail.com>
2018-09-20 15:54:20 -0700

    react app created, components boilerplated, plan hatched

---
+++ auth-auth/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
