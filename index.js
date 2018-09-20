451486602dcd0448b9e131739639fb6b336192b0
vanceleon <vanceleon44@gmail.com>
2018-09-20 14:07:50 -0400

    pasting endpoints

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require("express");
 const server = express();
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
-// const db = require()
-
+const jwt = require("jsonwebtoken");
 
 server.use(express.json());
 server.use(cors());
 
 
 
-server.get("/", (req, res) => {
-    res.send("Hello World");
-})
+const auth = () => {
+    //auth using token
+}
+
 
 
 server.post("/register", (req, res) => {
@@ -24,10 +24,70 @@ server.post("/register", (req, res) => {
         .insert(creds)
         .then(ids =>{
             const id = ids[0];
+            //generate token 
+
+            //attach that token to the response
             res.status(201).json(id);
         })
         .catch(err => res.status(500).send(err));
 })
+
+
+server.post("/api/login", (req, res) => {
+    const creds = req.body;
+    db("users")
+      .where({ username: creds.username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(creds.password, user.password)) {
+          req.session.username = user.username;
+          res.status(200).json(`Welcome ${creds.username}`);
+        } else {
+          res
+            .status(401)
+            .json({ message: "You shall not pass! Go Back to the Shadows!" });
+        }
+      })
+      .catch(err => res.status(500).send(err));
+  });
+
+
+  server.get("/api/users", auth, (req, res) => {
+    db("users")
+      .select("id", "username", "password")
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.json({ message: "Please login to access information" }));
+  });
+
+
+  
+  //router post
+  
+  server.get("/api/logout", (req, res) => {
+    if (req.session) {
+      req.session.destroy(err => {
+        if (err) {
+          res.send("error logging out");
+        } else {
+          res.send("good bye");
+        }
+      });
+    }
+  });
+  
+  
+  server.get("/api/users", auth, (req, res) => {
+    db("users")
+      .select("id", "username", "password")
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.json({ message: "Please login to access information" }));
+  });       
+
+
 
 
 server.listen(8000, () => console.log("======API running on Port 8000======"));
