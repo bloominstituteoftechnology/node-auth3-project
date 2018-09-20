71f0c9c1550a544c2a0e34b5ca6b485a17c9a26f
JoCatherine Griggs <jocafrin@gmail.com>
2018-09-20 13:47:21 -0400

    set up react App, index.js, and app.js

--- migrations/20180919133953_users.js
+++ migrations/20180919133953_users.js
@@ -1,8 +1,15 @@
-
-exports.up = function(knex, Promise) {
+exports.up = function(knex) {
+    return knex.schema.createTable('users', users => {
+      users.increments();
+      users
+        .string('username', 120)
+        .notNullable()
+        .unique();
+      users.string('password', 120).notNullable();
+      users.string('department', 120);
+    });
+  };
   
-};
-
-exports.down = function(knex, Promise) {
-  
-};
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('users');
+  };
