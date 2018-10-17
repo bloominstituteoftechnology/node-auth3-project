36482e9d7145aaa5f914033d81047a7fc3d0e881
alejandrok93 <alejandrok93@gmail.com>
2018-10-17 14:50:03 -0700

    Initial project set up for React app

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Login</h1>
+        <h1>Register</h1>
+        <h1>Users</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
