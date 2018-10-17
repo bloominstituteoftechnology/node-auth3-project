40769e2be9fe7df5f0a6305ad70b3f1c6ae6a5ba
PatrickTheCodeGuy <patrick.jthompson93@gmail.com>
2018-10-17 11:51:20 -0700

    server setup.

---
+++ data/migrations/20181017114428_users.js
@@ -0,0 +1,12 @@
+exports.up = function(knex, Promise) {
+	return knex.schema.createTable("users", function(tbl) {
+		tbl.increments();
+		tbl.string("username", 20).notNullable();
+		tbl.string("password", 255).notNullable();
+		tbl.string("department", 255).notNullable();
+	});
+};
+
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTableIfExists("users");
+};
---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const helmet = require("helmet");
+const knex = require("knex");
+const knexConfig = require("./knexfile.js");
+const server = express();
+
+const db = knex(knexConfig.development);
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get("/", (req, res) => {
+	res.send("Its Alive!");
+});
+
+server.post("/api/register", (req, res) => {
+	const credentials = req.body;
+	const hash = bcrypt.hashSync(credentials.password, 15);
+	credentials.password = hash;
+
+	db("users")
+		.insert(credentials)
+		.then(ids => {
+			res.status(201).json({ id: ids[0] });
+		})
+		.catch(err => {
+			res.status(500).json({ error: "could not create user" });
+		});
+});
+
+server.post("/api/login", (req, res) => {
+	const creds = req.body;
+	db("users")
+		.where({ username: creds.username })
+		.first()
+		.then(user => {
+			if (user && bcrypt.compareSync(creds.password, user.password)) {
+				res.status(201).json({ welcome: user.username });
+			} else {
+				res
+					.status(500)
+					.json({ error: "Wrong Username and/or Password, please try again" });
+			}
+		});
+});
+
+server.get("/api/users", (req, res) => {
+	db("users")
+		.select("id", "username", "password")
+		.then(users => {
+			res.json(users);
+		})
+		.catch(err => res.send(err));
+});
+
+server.listen(3300, () => console.log("\nrunning on port 3300\n"));
