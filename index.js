22b80a7acc414f70195b3fdd74079b5444ee04cd
Keiran <keirankozlowski@gmail.com>
2018-10-17 03:58:30 -0400

    Updated path for the db file, added in routes from auth-i project to be modified tomorrow.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const helmet = require('helmet');
 const bcrypt = require('bcryptjs');
+
 const session = require('express-session');
+const sessionConfig = {
+    name: 'session',
+    secret: "Hello I am a horse",
+    cookie: {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        secure: false
+    },
+    httpOnly: true,
+    resave: false,
+    saveUninitialized: false
+};
+
 const knex = require('knex');
 const knexConfig = require('./knexfile.js');
 
@@ -10,6 +23,7 @@ const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use(session(sessionConfig));
 
 const port = 8000;
 server.listen(port, () => console.log(`API running on port ${port}`));
@@ -17,4 +31,84 @@ server.listen(port, () => console.log(`API running on port ${port}`));
 // checker endpoint
 server.get('/', (request, response) => {
     response.send('Server initialized.');
+});
+
+// register/login POST requests
+server.post('/api/register', (request, response) => {
+    const credentials = request.body;
+
+    const hashedPW = bcrypt.hashSync(credentials.password, 14);
+    credentials.password = hashedPW;
+
+    db('users')
+        .insert(credentials)
+        .then(ids => {
+            const id = ids[0];
+            request.session.username = credentials.username;
+            return response
+                .status(201)
+                .json({ newUserId: id });
+        })
+        .catch(() => {
+            return response
+                .status(500)
+                .json({ Error: "There was an error while creating the user." })
+        });
+});
+
+server.post('/api/login', (request, response) => {
+    const credentials = request.body;
+
+    db('users')
+        .where({ username: credentials.username })
+        .first()
+        .then(user => {
+            if (!user || !bcrypt.compareSync(credentials.password, user.password)) {
+                return response
+                    .status(401)
+                    .json({ message: "You shall not pass!" });
+            } else {
+                request.session.username = user.username;
+                return response
+                    .status(200)
+                    .json({ message: `${credentials.username} logged in...` });
+            }
+        })
+        .catch(() => {
+            return response
+                .status(500)
+                .json({ Error: "There was an error while logging in." })
+        });
+});
+
+// user list and logout GET requests
+server.get('/api/users', protected, (request, response) => {
+    request.session.name = '12345';
+    const sessionName = request.session.name;
+
+    db('users')
+        .select('id', 'username', 'password')
+        .then(users => {
+            return response
+                .status(200)
+                .json(users);
+        })
+        .catch(() => {
+            return response
+                .status(500)
+                .json({ Error: "Could not find list of users." });
+        });
+});
+
+server.get('/api/logout', (request, response) => {
+    if (request.session) {
+        request.session.destroy();
+        return response
+            .status(200)
+            .json({ message: "User logged out." });
+    } else {
+        return response
+            .status(500)
+            .json({ message: "Unable to log out." });
+    }
 });
--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
-    connection: { filename: './dev.sqlite3' },
+    connection: { filename: './database/dev.sqlite3' },
     migrations: { directory: './database/migrations', },
     seeds: { directory: './database/seeds', },
     useNullAsDefault: true,
