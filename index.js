75b32a57293b7546b2687f4d619a84dbe026c7f9
iAmAdamReid <adam@adamreid.me>
2018-10-17 14:56:58 -0700

    Postman tests passing, added department checks to GET for userlist, code comments

--- index.js
+++ index.js
@@ -20,12 +20,29 @@ function logger(req, res, next){
 server.use(logger);
 
 server.get('/', (req, res) => {
-  res.send('Its Alive!');
+  res.send('Servin\' USA!');
 });
 
-// TODO
+/**************************************************/
+// JWT FUNCTION INITIALIZATION
+/**************************************************/
+const jwtSecret = 'now$this&is@podracing';
 
-// /api/register POST
+function generateToken(user) {
+  const jwtPayload = {
+      ...user,
+  };
+  const jwtOptions = {
+    expiresIn: '1h',
+  };
+
+  return jwt.sign(jwtPayload, jwtSecret, jwtOptions);
+}
+
+/**************************************************/
+// REGISTER
+/**************************************************/
+
 server.post('/api/register', async (req, res) => {
     try {
     const credentials = req.body;
@@ -39,30 +56,16 @@ server.post('/api/register', async (req, res) => {
         const token = generateToken(newUser);
         return res.status(201).json({ token });
     } catch(err) {
-        return res.status(404).json(err);
+        return res.status(404).json({err});
     }
-
     } catch(err) {
     return res.status(500).json({err});
     }
 })
 
-  // setup for jwt creation
-  const jwtSecret = 'now$this&is@podracing';
-
-  function generateToken(user) {
-    const jwtPayload = {
-        ...user
-    };
-    const jwtOptions = {
-      expiresIn: '1h',
-    };
-  
-    return jwt.sign(jwtPayload, jwtSecret, jwtOptions);
-  }
-
-
-// /api/login POST
+/**************************************************/
+// LOGIN
+/**************************************************/
 
 server.post('/api/login', (req, res) => {
     const creds = req.body;
@@ -75,7 +78,7 @@ server.post('/api/login', (req, res) => {
           const token = generateToken(user); // create new token for user on login
           res.status(200).json({ welcome: user.username, token }); // pass token to client
         } else {
-          res.status(401).json({ message: 'you shall not pass!' });
+          res.status(401).json({ message: 'YOU! SHALL NOT! PAAASS!' });
         }
       })
       .catch(err => {
@@ -84,7 +87,10 @@ server.post('/api/login', (req, res) => {
       });
   });
 
-  // setup protected middleware
+/**************************************************/
+// PROTECTED MIDDLEWARE
+/**************************************************/
+
   function protected(req, res, next) {
     // store auth token from req header
     const token = req.headers.authorization;
@@ -92,38 +98,48 @@ server.post('/api/login', (req, res) => {
       jwt.verify(token, jwtSecret, (err, decodedToken) => {
         if (err) {
           // token verification failed
-          res.status(401).json({ message: 'invalid token' });
+          res.status(401).json({ message: 'Invalid Token' });
         } else {
-          // token is valid
+          // token is valid, add decodedToken to req parameters
           req.decodedToken = decodedToken; 
           // any req after this ^ will have the decoded token *** HANDLE WITH CARE ***
-          console.log('\n** decoded token information **\n', req.decodedToken);
+          console.log('\n** Decoded Token **\n', req.decodedToken);
           next();
         }
       });
     } else {
-      res.status(401).json({ message: 'no token provided' });
+      res.status(401).json({ message: 'No Token Provided' });
     }
   }
 
-  // === TODO ===
-  /* *** STRETCH *** */
-  // setup department middleware
-//   function checkDepartment(dept) {
-//     return function(req, res, next) {
-//       if (req.decodedToken && req.decodedToken.department.includes(dept)) {
-//         next();
-//       } else {
-//         res.status(403).json({ message: 'you shall not pass! forbidden' });
-//       }
-//     };
-//   }
+/**************************************************/
+// CHECK DEPARTMENT MIDDLEWARE
+/**************************************************/
 
+  // checkDepartment middleware can be passed to restrict access to only certain department users
+  // this is done by passing the middleware checkDepartment('dept') where dept is the desired user type
+  // e.g. checkDepartment('admins') will restrict the response to only admin authorized users 
+  function checkDepartment(dept) {
+    return function(req, res, next) {
+      if (req.decodedToken && req.decodedToken.department.includes(dept)) {
+        next();
+      } else {
+        res.status(403).json({ message: 'FORBIDDEN! You are not authorized to view this!' });
+      }
+    };
+  }
+
+/**************************************************/
+// GET USERS
+/**************************************************/
 
 // /api/users GET with protected middleware
 server.get('/api/users', protected, (req, res) => {
+    /*** STRETCH ***/
+    // server will only send users that are of the same type as the logged in user
     db('users')
-      .select('id', 'username', 'password')
+    .where({department: req.decodedToken.department})
+      .select('id', 'username', 'department')
       .then(users => {
         res.json({ users });
       })
@@ -132,6 +148,22 @@ server.get('/api/users', protected, (req, res) => {
         res.status(500).json({err})
       })
 });
+
+// GET calls to /api/users/admin will show all information (including hash-passwords!) to admin users only
+server.get('/api/users/admin', protected, checkDepartment('admins'), (req, res) => {
+    db('users').select('id', 'username', 'password', 'department')
+    .then(users => {
+        res.json({users});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({err});
+    })
+})
+
+/**************************************************/
+// SERVER LISTEN
+/**************************************************/
 
 const port = 9000;
 server.listen(port, () => console.log(`\nrunning on port ${port}\n`));
