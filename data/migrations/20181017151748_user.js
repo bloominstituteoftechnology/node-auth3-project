2fc92cd951b3137cb289c3c03072cdf9d91cecf5
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-10-17 15:21:23 -0500

    Added migration user

---
+++ data/migrations/20181017151748_user.js
@@ -0,0 +1,12 @@
+exports.up = knex =>
+  knex.schema.createTable('users', table => {
+    table.increments();
+    table
+      .string('username', 128)
+      .notNullable()
+      .unique();
+    table.string('password', 128).notNullable();
+    table.string('department', 128);
+  });
+
+exports.down = knex => knex.schema.dropTableIfExists('users');
