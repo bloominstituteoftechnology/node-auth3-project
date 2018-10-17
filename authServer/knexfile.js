184c390ac7795ef1cc6a42f16777d42888818504
decagondev <decagondevelopment@gmail.com>
2018-10-17 20:36:17 +0100

    (MVP) : add migration for create users table

--- authServer/knexfile.js
+++ authServer/knexfile.js
@@ -6,6 +6,9 @@ module.exports = {
     connection: {
       filename: "./data/db.sqlite3"
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    }
   }
 };
