3304b798a28e66869fbe3c19d79e12320a4bee8e
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-10-17 18:01:03 -0400

    Register User is able to register through endpoint created

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+const jwtSecret= 'It is a party'
 
-
-generateToken=(user)=>{
+function generateToken(user) {
     const jwtPayload={
         username:user.username
     }
     
-    const jwtSecret= 'It is a party'
+    
 
     const jwtOptions={
         expiresIn:'1h',
@@ -29,5 +29,23 @@ generateToken=(user)=>{
 server.get('/', (req, res) => {
   res.send('Its Alive!');
 });
+
+server.post('/register', (req, res) => {
+    const credentials = req.body;
+
+    const hash = bcrypt.hashSync(credentials.password, 14);
+    credentials.password = hash;
+   
+    db('users')
+        .insert(credentials)
+        .then(ids => {
+        const id = ids[0];
+        res.status(201).json({ newUserId: id });
+   })
+   .catch(err => {
+       res.status(500).json({err});
+   })
+})
+
 
 server.listen(6000, () => console.log('\nrunning on port 6000\n'));
