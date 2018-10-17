616ba42941c44197dad6364e7b9ec109ad79a526
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-10-17 17:34:41 -0400

    Migration Created table to store users credentials Created knexfile to give us that ability to do so

---
+++ db/migrations/20181017173017_user.js
@@ -0,0 +1,22 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('users', users => {
+        users.increments();
+         users
+          .string('username', 128)
+          .notNullable()
+          .unique();
+         users
+          .string('password', 128)
+          .notNullable();
+         users
+          .string('department', 128)
+          
+    })
+  
+};
+
+exports.down = function(knex, Promise) {
+    knex.schema.dropTable('user')
+  
+};
