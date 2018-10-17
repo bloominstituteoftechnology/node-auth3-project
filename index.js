370001866db3210f48b55a248f66c390797225cf
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-17 16:59:09 -0400

    bcryptjs and jsonwebtoken dependencies added for authentication purposes

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const knex = require('knex');
 
+// security dependencies
+
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// server setup
+
 const server = express();
 const knexConfig = require('./knexfile.js');
 const db = knex(knexConfig.development);
@@ -12,6 +19,11 @@ server.get('/api', (req, res) => {
   res.send('Server test successful.');
 });
 
+server.post('/register', (req, res) => {
+  const credentials = req.body;
+
+  const
+})
 // server instantiation
 
 const port = 9000;
