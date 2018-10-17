a32797b79b20c9bce39e491098fd3e35a5e4b254
Benny Oseguera <oseguerabenny27@gmail.com>
2018-10-17 14:22:46 -0700

    Finished MVP - Benny Oswguera

--- data/migrations/20181017123904_users_table.js
+++ data/migrations/20181017123904_users_table.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("users", tbl => {
     // id
     tbl.increments();
-    tbl.string("users").notNullable();
+    tbl.string("username").notNullable();
     tbl.string("password").notNullable();
     tbl.string("department").notNullable();
   });
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // imports
 const express = require("express");
-const bcrpyt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // instantiate server
 const server = express();
@@ -9,25 +10,54 @@ server.use(express.json());
 // middleware
 // jwt
 // generate token
+const jwtSecret = "nobody tosses a dwarf";
 const generateToken = user => {
   const jwtPayload = {
     ...user,
     hello: "FSW13",
     role: "admin"
   };
-  const jwtSecret = "nobody tosses a dwarf";
   const jwtOptions = {
-    expiresIn: "1m"
+    expiresIn: "1h"
   };
-
   return jwt.sign(jwtPayload, jwtSecret, jwtOptions);
+};
+
+// protected
+const protected = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
+      if (err) {
+        // token verification failed
+        res.status(401).json({ message: "invalid token" });
+      } else {
+        // token is valid
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "no token provided" });
+  }
+};
+
+// check role
+const checkRole = role => {
+  return (req, res, next) => {
+    if (req.decodedToken && req.decodedToken.roles.includes(role)) {
+      next();
+    } else {
+      res.status(403).json({ message: "you shall not pass! forbidden" });
+    }
+  };
 };
 
 // endpoints
 server.post("/api/register", (req, res) => {
   const creds = req.body;
   //hash
-  bcrpyt.hsashSync(creds.password, 10);
+  const hash = bcrypt.hashSync(creds.password, 10);
   creds.password = hash;
   db("users")
     .insert(creds)
@@ -51,7 +81,8 @@ server.post("/api/login", (req, res) => {
     });
 });
 
-server.get("/api/users", protected, (req, res) => {
+server.get("/api/users", protected, checkRole("other"), (req, res) => {
+  console.log(req.decoded);
   db("users")
     .then(users => res.status(200).json(users))
     .catch(err => res.status(500).json(err));
