82603a9a937f13a2f095fbcc1ca60aa702cd7cb6
alejandrok93 <alejandrok93@gmail.com>
2018-10-17 14:47:14 -0700

    Completed API endpoints to register, login, get users with JSON Web Tokens

---
+++ data/migrations/20181017142533_add_users_table.js
@@ -0,0 +1,12 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("users", table => {
+    table.increments().primary();
+    table.string("username").notNullable();
+    table.string("password").notNullable();
+    table.string("department");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("users");
+};
---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const knex = require("knex");
+const jwt = require("jsonwebtoken");
+
+//set up db
+const knexConfig = require("./knexfile.js");
+const db = knex(knexConfig.development);
+
+const server = express();
+
+//apply middleware
+server.use(express.json());
+server.use(cors());
+
+server.listen(9000, () => {
+  console.log("API IS RUNNING...");
+});
+
+//token secret key
+const secret = "this.is%my^secret-key";
+
+//Routes
+server.post("/api/register", (req, res) => {
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 14);
+  creds.password = hash;
+
+  db("users")
+    .insert(creds)
+    .then(idObj => res.status(201).json(idObj))
+    .catch(err => res.status(500).json(err));
+});
+
+function generateToken(user) {
+  const payload = {
+    ...user
+  };
+  const options = { expiresIn: "1h" };
+
+  return jwt.sign(payload, secret, options);
+}
+server.post("/api/login", (req, res) => {
+  const creds = req.body;
+
+  db("users")
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      console.log(user);
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ hi: user.username, token });
+      } else res.status(500).json({ error: "You shall not pass" });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "You shall not pass", err });
+    });
+});
+
+function protected(req, res, next) {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ error: "Invalid token" });
+      } else {
+        console.log(token);
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ error: "Not authorized" });
+  }
+}
+
+server.get("/api/users", protected, (req, res) => {
+  db("users")
+    .then(users => {
+      console.log(users);
+      res.status(200).json(users);
+    })
+    .catch(err => res.status(500).json(err));
+});
