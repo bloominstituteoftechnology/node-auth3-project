9885f64dba85e6b3d779b3ab1bddb15d0bec9deb
Casey Baker <caseye.baker@gmail.com>
2018-10-17 14:10:28 -0700

    forgot nodemon! installed; server running; register route built

--- index.js
+++ index.js
@@ -4,3 +4,38 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const db = require('./data/dbConfig.js');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+  res.send('Welcome!');
+});
+
+server.post('/register', (req, res) => {
+  const creds = req.body;
+
+  const hash = bcrypt.hashSync(creds.password, 10);
+  creds.password = hash;
+
+  db('users')
+    .insert(creds)
+    .then(ids => {
+      const id = ids[0];
+      res
+        .status(201)
+        .json({ newUserId: id });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json(err);
+    });
+});
+
+
+
+port = 8675;
+server.listen(port, () => console.log(`\n= = Running on port ${port} = =\n`))
