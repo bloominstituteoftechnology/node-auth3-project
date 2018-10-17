ac819fec46e073e6fa4644dd241c65cef92a5443
m-j-hacker <mjhacker@gmail.com>
2018-10-17 15:45:46 -0500

    Added login and users endpoints

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ server.get('/', (req, res) => {
     res.send("It's allliiiiive!!");
 });
 
+server.get('/api/users', protected, (req, res) => {
+    db('users')
+      .select('id', 'username', 'department')
+      .then(users => {
+        res.status(200).json({ users });
+      })
+      .catch(err => res.status(500).send(err));
+  });
+
 server.post('/api/register', (req, res) => {
     const credentials = req.body;
 
@@ -33,13 +42,30 @@ server.post('/api/register', (req, res) => {
     })
 })
 
+server.post('/api/login', (req, res) => {
+    const creds = req.body;
+
+    db('users')
+    .where({username: creds.username})
+    .first()
+    .then(user => {
+        if(user && bcrypt.compareSync(creds.password, user.password)) {
+            const token = generateToken(user);
+            res.status(200).json({ message: `Logged in as ${user.username}`, token });
+        } else {
+            res.status(401).json({ message: "You shall not pass!"});
+        }
+    })
+    .catch(err => res.status(500).json({err}));
+})
+
+const jwtSecret = 'nobody tosses a dwarf!';
 function generateToken(user) {
 
     const jwtPayload = {
         ...user,
     };
 
-    const jwtSecret = 'nobody tosses a dwarf!';
 
     const jwtOptions = {
         expiresIn: '1h'
@@ -52,10 +78,18 @@ function protected(req, res, next) {
     const token = req.headers.authorization;
 
     if (token) {
-
+        jwt.verify(token, jwtSecret, (err, decodedToken) => {
+            if (err) {
+                res.status(401).json({ message: 'invalid token!' });
+            } else {
+                req.decodedToken = decodedToken;
+                next();
+            }
+        })
     } else {
-
+        res.status(401).json({ message: 'no token provided' })
     }
+    
 }
 
 const port = 8989;
