7babb59c4920d8a93654b39871fe4a58ddb845c8
lndubose <lndubose13@gmail.com>
2018-10-17 16:46:40 -0500

    Register users
    Made error handling within the post. Makes sure username is unique. It needs more error handling to make it more efficent.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const db = require('./data/dbCongfig');
+
 const server = express();
 const port = 5000;
 
@@ -20,5 +22,38 @@ function generateToken(user) {
   };
   return jwt.sign(jwtPayload, jwtSecret, jwtOptions);
 }
+
+server.post('/register', (req, res) => {
+  const credentials = req.body;
+
+  const hash = bcrypt.hashSync(credentials.password, 10);
+  credentials.password = hash;
+
+  db('users')
+    .insert(credentials)
+    .then(ids => {
+      res.status(201).json({ newUserId: ids[0] });
+    })
+    .catch(err => {
+      if (err.errno === 19) {
+        res.status(400).json({ error: 'Username is already used.' });
+      } else {
+        res.status(500).json(err);
+      }
+    });
+});
+
+//Get users
+server.get('/users', (req, res) => {
+  db('users')
+    .then(users => {
+      if (users.length < 1) {
+        res.status(401).json({ message: 'There are no users.' });
+      } else {
+        res.status(200).json(users);
+      }
+    })
+    .catch(err => console.log(err));
+});
 
 server.listen(port, console.log(`===Server running on ${port} port===\n`));
