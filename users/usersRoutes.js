89bfe578999fe00b5310d85cc10c5799e76678e0
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-10-17 07:52:11 -0400

    Login endpoint complete

--- users/usersRoutes.js
+++ users/usersRoutes.js
@@ -46,4 +46,23 @@ router.post("/register", (req, res) => {
     .catch(err => res.status(500).send(err));
 });
 
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  const creds = { username, password };
+
+  db("users")
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = makeToken(user);
+
+        res.status(200).json({ token });
+      } else {
+        res.status(401).json({ message: "You shall not pass!" });
+      }
+    })
+    .catch(err => res.status(500).send(err));
+});
+
 module.exports = router;
