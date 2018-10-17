093f66e7a9de645348b3deb0112483fcda5405d3
Eva Li <evalee904@gmail.com>
2018-10-17 17:52:17 -0500

    Users table and database.

---
+++ data/migrations/20181017160936_create_users_table.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(table) {
+    table.increments();
+    table
+      .string('username')
+      .notNullable()
+      .unique();
+    table
+      .string('password')
+      .notNullable();
+    table
+      .string('department')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users')
+};
