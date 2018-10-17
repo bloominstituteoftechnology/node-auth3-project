c143aee0c03958a8223d5e202b2a390fb90b2f34
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-17 17:08:19 -0400

    added jwt token generation and protected() function to check this token, created GET endpoint for users data protected by these features

--- index.js
+++ index.js
@@ -13,17 +13,90 @@ const server = express();
 const knexConfig = require('./knexfile.js');
 const db = knex(knexConfig.development);
 
+// create a secret key and generation user tokens via jwt
+
+const jwtSecret = 'SUPER SECRET KEY';
+
+function createToken(user) {
+
+  const jwtPayload = {
+    ...user,
+    greeting: 'Hello!'
+    // placeholder for testing with other attributes
+  };
+
+  const jwtOptions = {
+    expiresIn: '1h',
+  };
+
+  return jwt.sign(jwtPayload, jwtSecret, jwtOptions)
+} // createToken
+
 // basic GET for server testing
 
 server.get('/api', (req, res) => {
   res.send('Server test successful.');
 });
 
-server.post('/register', (req, res) => {
+// register a user
+
+server.post('/api/register', (req, res) => {
   const credentials = req.body;
 
-  const
-})
+  const hash = bcrypt.hashSync(credentials.password, 10);
+  credentials.password = hash;
+
+  db('users')
+    .insert('credentials')
+    .then(ids => {
+      const id = ids[0];
+      res.status(201).json({ newUserId generated: id });
+    })
+    .catch(err => {
+      res.status(500).json(err.message);
+    });
+});
+
+// GET all users in the database
+
+server.get('/api/users', protected, (req, res) => {
+  db('users')
+    .select('id', 'username', 'password', 'department')
+    .then(users => {
+      res.json({ users });
+    })
+    .catch(err => res.send(err.message));
+});
+
+// add protected() function for authentication
+
+function protected(req, res, next) {
+  const token = req.headers.authorization;
+
+  if(token) {
+    jwt.verify(token, jwtSecret, (err, plainToken) => {
+      if(err) {
+        // token verification failed
+        res.status(401).json({ error: 'The provided token is invalid. '});
+      } else {
+        // valid token, grant access
+        req.plainToken = plainToken;
+        next();
+      }
+    })
+  } else {
+    res.status(400).json({ error: 'Please provide a token. '}) // don't include this, generally speaking
+  }
+  next();
+}
+
+
+
+
+
+
+
+
 // server instantiation
 
 const port = 9000;
