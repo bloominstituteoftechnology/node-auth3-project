f740c7485f21c45dcce0ebff77d9d8d57004d39f
Max <maxkajiwara@gmail.com>
2018-10-17 16:15:23 -0700

    Fix file path

--- data/knexfile.js
+++ data/knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 	development: {
 		client: 'sqlite3',
 		connection: {
-			filename: './database.sqlite3'
+			filename: './data/database.sqlite3'
 		},
 		useNullAsDefault: true,
 		migrations: {
