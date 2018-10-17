aec9d253490078a91a5c4c43209e870dbef51742
Timothy Hoang <timh1203@gmail.com>
2018-10-17 16:58:23 -0400

    Add POST route to login with jwt

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken');
 const db = require('./db/dbConfig.js');
 const app = express();
 const port = 9000;
+const jwtSecret = 'Aho,Mitakuye.Oyasin/1017';
 
 app.use(express.json());
 app.use(helmet());
@@ -28,4 +29,31 @@ app.route('/api/register')
       .catch(err => res.status(500).json(err));
   })
 
+app.route('/api/login')
+  .post((req, res) => {
+    const credentials = req.body
+    db('users')
+      .where({ username: credentials.username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(credentials.password, user.password)) {
+          const token = generateToken(user);
+          return res.status(200).json({ Welcome: user.username, token });
+        }
+        return res.status(401).json({ Message: 'You shall not pass!' });
+      })
+      .catch(err => res.status(500).json(err));
+  })
+
+function generateToken(user) {
+  const jwtPayload = {
+    ...user,
+    role: 'admin',
+    pet: 'dog'
+  }
+  const jwtOptions = {
+    expiresIn: '5m'
+  }
+  return jwt.sign(jwtPayload, jwtSecret, jwtOptions);
+}
 app.listen(port, () => console.log(`\n===${port} is live!===\n`))
