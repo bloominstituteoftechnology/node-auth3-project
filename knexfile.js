736d9c397a91d00c0d906ed28761bab33de2bdab
Lucas Beemer <you@example.com>
2018-10-17 14:48:24 -0700

    useNullAsDefault

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,10 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
-    connection: {
-      filename: './database/auth.sqlite3'
-    }
+    connection: { filename: './database/auth.sqlite3' },
+    useNullAsDefault: true
   },
 
   staging: {
