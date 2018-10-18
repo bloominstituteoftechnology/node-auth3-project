f4579dc2afa8d1edd3838138b645278080178671
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-10-18 16:53:53 -0400

    Signin Setup signin component

---
+++ my-app/src/components/auth/signin.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import axios from "axios";
+class Signin extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: ""
+        };
+    }
+    handleInputChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+    signIn = event => {
+        event.preventDefault();
+        const { username, password } = this.state;
+        const info = { username, password };
+        axios
+            .post(`http://localhost:6000/api/login`, info)
+            .then(response => {
+                localStorage.setItem("jwt", response.data.token);
+            })
+            .catch(error => console.error("Error:", error));
+     
+        this.setState({
+            username: "",
+            password: ""
+        });
+    };
+    
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.signIn}>
+                <input
+                    onChange={this.handleInputChange}
+                    placeholder="username"
+                    value={this.state.username}
+                    name="username"
+                />
+                <input
+                    onChange={this.handleInputChange}
+                    placeholder="password"
+                    value={this.state.password}
+                    name="password"
+                />
+                <button type="submit">Sign in</button>
+            </form>
+        </div>
+        );
+    }
+}
+
+export default Signin;
--- my-app/src/components/auth/signup.js
+++ my-app/src/components/auth/signup.js
@@ -1,22 +1,25 @@
 import React, { Component } from "react";
 import axios from "axios";
 class Signup extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: "",
-      password: ""
-    };
-  }
-  handleInputChange = event => {
+    constructor() {
+        super();
+        this.state = {
+            username: "",
+            password: "",
+            department: 'HR'
+        };
+    }
+  
+    handleInputChange = event => {
     this.setState({ [event.target.name]: event.target.value });
-  };
-  signUp = event => {
+    };
+
+    signUp = event => {
     event.preventDefault();
-    const { username, password } = this.state;
-    const info = { username, password };
+    const { username, password, department } = this.state;
+    const info = { username, password, department };
     axios
-      .post(`http://localhost:8000/api/register`, info)
+      .post(`http://localhost:6000/api/register`, info)
       .then(response => {
         localStorage.setItem("jwt", response.data.token);
       })
@@ -25,28 +28,30 @@ class Signup extends Component {
       username: "",
       password: ""
     });
-  };
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.Signup}>
-          <input
-            onChange={this.handleInputChange}
-            placeholder="username"
-            value={this.state.username}
-            name="username"
-          />
-          <input
-            onChange={this.handleInputChange}
-            placeholder="password"
-            value={this.state.password}
-            name="password"
-          />
-          <button type="submit">Sign up</button>
-        </form>
-      </div>
-    );
-  }
+    this.props.history.push('/users')
+	 		
+    };
+    render() {
+        return (
+            <div>
+             <form onSubmit={this.Signup}>
+                <input
+                    onChange={this.handleInputChange}
+                    placeholder="username"
+                    value={this.state.username}
+                    name="username"
+                />
+                <input
+                    onChange={this.handleInputChange}
+                    placeholder="password"
+                    value={this.state.password}
+                    name="password"
+                />
+                <button type="submit">Sign up</button>
+             </form>
+            </div>
+        );
+    }
 }
 
 export default Signup;
