2be4b711948d7432d7000ce5d2c45d8f4e15b4d3
Andrew <ghobrial.andrew@gmail.com>
2018-10-18 18:40:45 -0400

    framework for app.js and login.js complete

---
+++ client/components/Login.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import {withRouter} from 'react-router-dom';
+ 
+class Login extends React.Component {
+    
+  constructor(props){
+    super(props);
+      this.state = {
+        username: '',
+        password: ''
+    }    
+  }
+     
+  handleInput = event => {
+      event.preventDefault();
+      this.setState({
+          [event.target.name]: event.target.value
+      })
+  }
+     
+  handleLogin = event => {
+      event.preventDefault();
+      let user = {
+          username: this.state.username,
+          password: this.state.password
+      };
+      let address = `http://localhost:5000/api/login`;
+      axios.post(address, user)
+      .then(res => {
+          localStorage.setItem('jwt', res.data.token);
+          this.props.history.replace('/users');
+      })
+      .catch(err => {
+          console.error(err)
+      })
+  }
+        
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleLogin}>
+            <input type = 'text' name = 'username' value = {this.state.username} placeholder='username' onChange={this.handleInput}>
+            </input>
+            <input type='password' name = 'password' value = {this.state.password} placeholder = 'password' onChange={this.handleInput}>
+            </input>
+            <button>Login</button>
+        </form>
+      </div>
+    )
+  }
+
+}
+ export default withRouter(Login); 
--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {withRouter, NavLink, Route} from 'react-router-dom';
+import UserList from './components/UserList';
+import Login from './components/Login';
+import Register from './components/Register';
 
-class App extends Component {
+const Home = (props) => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+ class App extends React.Component {
+   
+  handleLogout(){
+    localStorage.removeItem('jwt');
+    this.props.history.replace('/');
+  }
+   
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <nav>
+            <NavLink to = '/' exact>Home</NavLink>
+            <NavLink to = '/users'>Users</NavLink>
+            <NavLink to = '/login'>Login</NavLink>
+            <NavLink to ='/register'>Register</NavLink>
+            <NavLink to = '/' exact onClick={this.handleLogout}>Logout</NavLink>
+          </nav>
         </header>
+        <main>
+          <Route exact path = '/' component = {Home} />
+          <Route path = '/users' render = {(props) => {
+            return <UserList/>
+          }}/>
+          <Route path = '/login' component = {Login} />
+          <Route path = '/register' component = {Register} />
+        </main>
       </div>
     );
   }
 }
 
-export default App;
+ export default withRouter(App);
