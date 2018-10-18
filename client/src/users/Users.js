a29b06da961b1099722cffaae3da8ebe850cd9cb
Michael Trevino <preince560m@gmail.com>
2018-10-18 18:57:22 -0500

    part2

---
+++ client/src/users/Users.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Users extends Component {
+  state = {
+    users: [],
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Users</h2>
+        <ul>
+          {this.state.users.map(u => (
+            <li key={u.id}>{u.username}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    const token = localStorage.getItem('jwt');
+
+    const endpoint = 'http://localhost:3300/api/users';
+    const options = {
+      headers: {
+        Authorization: token,
+      },
+    };
+    axios
+      .get(endpoint, options)
+      .then(res => {
+        console.log(res.data);
+        this.setState({ users: res.data.users });
+      })
+      .catch(err => {
+        console.error('ERROR', err);
+      });
+  }
+}
+
+export default Users;
