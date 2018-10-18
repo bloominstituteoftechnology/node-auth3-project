8fe78c97be7c6dc31a01ff3bb31219d5ee0f77b6
Max <maxkajiwara@gmail.com>
2018-10-18 14:54:03 -0700

    Build HomePage

---
+++ react-auth-ii/src/components/HomePage.js
@@ -0,0 +1,11 @@
+// Dependencies
+import React, { Component } from 'react';
+// import styled from 'styled-components';
+
+class HomePage extends Component {
+	render() {
+		return <div className="HomePage">Hi. Log in or sign up I guess.</div>;
+	}
+}
+
+export default HomePage;
