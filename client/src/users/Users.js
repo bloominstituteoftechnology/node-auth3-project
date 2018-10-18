9953e77e54c58eacbbb0ab5624c5d246afae223d
michaelagard <mdagard@gmail.com>
2018-10-18 16:27:26 -0400

    create users page from lecture

---
+++ client/src/users/Users.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Users extends Component {
+  state = {
+    users: [],
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>List of Users</h2>
+        <ul>
+          {this.state.users.map(user => <li key={user.id}>user.username</li>)}
+        </ul>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    const token = localStorage.getItem('jwt');
+    const endpoint = 'http://localhost:3300/api/users';
+    const options = {
+      headers: {
+        Authorization: token,
+      }
+    };
+    axios
+      .get(endpoint, options)
+      .then(res => {
+        console.log(res.data);
+        this.setState({ users: res.data.users })
+      })
+      .catch(err => {
+        console.error('ERROR', err);
+      });
+  }
+}
+
+export default Users;
