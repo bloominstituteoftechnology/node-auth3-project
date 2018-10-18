2f49f7f070092aa9d12c8657edd4972e3c05d537
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-10-18 12:30:21 -0400

    Creted a Home component

--- authclient/src/App.js
+++ authclient/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { Route, withRouter } from "react-router-dom";
+import { Route, withRouter, NavLink } from "react-router-dom";
 import SignIn from "./components/SignIn";
 import Users from "./components/Users";
+import Home from "./components/Home";
 
 import logo from "./logo.svg";
 import "./App.css";
@@ -13,10 +14,19 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Welcome!</h1>
+          <nav>
+            <NavLink to="/" exact>
+              Home
+            </NavLink>
+            <NavLink to="/users" exact>
+              Users
+            </NavLink>
+          </nav>
           <div>
             <button onClick={this.logout}>Log Out</button>
           </div>
         </header>
+        <Route exact path="/" component={Home} />
         <Route exact path="/signin" component={SignIn} />
         <Route exact path="/users" component={Users} />
       </div>
---
+++ authclient/src/components/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const Home = props => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
