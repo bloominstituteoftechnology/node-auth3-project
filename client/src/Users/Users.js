7546e6bd07762d59f7b6c63e0b683defb685ddc4
Clint Kunz <clintkunz@gmail.com>
2018-10-18 14:07:27 -0600

    list of users

--- client/src/Users/Users.js
+++ client/src/Users/Users.js
@@ -2,19 +2,35 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class Users extends Component {
+  state = {
+    users: []
+  }
+  
   render() {
     return (
       <div>
         <h2>List of Users</h2>
+        <ul>
+          {this.state.users.map(user => 
+          <li key={user.id}>{user.username}, {user.department}</li>
+          )}
+        </ul>
       </div>
     );
   }
 
   componentDidMount() {
+      const token = localStorage.getItem('jwt');
       const endpoint = 'http://localhost:9000/api/users';
+      const options = {
+        headers: {
+          Authorization: token
+        }
+      }
 
-      axios.get(endpoint).then(res => {
+      axios.get(endpoint, options).then(res => {
           console.log(res.data);
+          this.setState({ users: res.data.users })
       }).catch(err => {
           console.error('Error', err);
       })
--- client/src/auth/Signin.js
+++ client/src/auth/Signin.js
@@ -36,6 +36,7 @@ class Login extends Component {
 
       axios.post(endpoint, this.state).then(res => {
           console.log(res.data);
+          localStorage.setItem('jwt', res.data.token);
       }).catch(err => {
           console.error('Error', err);
       })
