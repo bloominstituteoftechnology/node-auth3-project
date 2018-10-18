b23f883889136052b17f90076327ef11d688f471
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-10-18 16:06:20 -0400

    Index/Routes Setup index page Setup routes on App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { NavLink, Route } from 'react-router-dom';
 import './App.css';
+import Signup from "./components/auth/signup";
+import Signin from "./components/auth/signin";
+import Users from "./components/users/users";
 
+
+const Home = props => {
+  return (
+    <div>
+      <h1>Home </h1>
+    </div>
+  );
+};
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <nav>
+            <NavLink to="/" exact>
+              Home
+            </NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/users">Users</NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/signin">Signin</NavLink>
+            &nbsp;|&nbsp;
+            <button onClick={this.signout}>Signout</button>
+          </nav>
+          <main>
+            <Route path="/" component={Home} exact />
+            <Route path="/signup" component={Signup} />
+            <Route path="/signin" component={Signin} />
+            <Route path="/users" component={Users} />
+            
+          </main>
         </header>
       </div>
     );
   }
+  signout = () => {
+    localStorage.removeItem('jwt');
+  };
 }
 
 export default App;
