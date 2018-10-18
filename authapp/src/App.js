ca5e01e416cd6493657670a531dd07fab720d3e3
Alexandra <alexswartz9753@gmail.com>
2018-10-18 14:46:10 -0400

    created sign up component

--- authapp/src/App.js
+++ authapp/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {NavLink, Route} from 'react-router-dom';
 import Users from './users/Users';
 import LogIn from './authentication/LogIn';
+import SignUp from './authentication/SignUp';
 import './App.css';
 
 const Home = props => {
@@ -26,14 +27,17 @@ class App extends Component {
             <NavLink to="/users">Users</NavLink>
             &nbsp;|&nbsp;
             <NavLink to="/login">Log In</NavLink>
+            &nbsp; |&nbsp;
+            <NavLink to='/register'>Sign Up</NavLink>
             &nbsp;|&nbsp;
-            <button onClick={this.logOut}>Log Out</button>
           </nav>
           <main>
             <Route path="/" component={Home} exact />
             <Route path="/users" component={Users} />
             <Route path="/login" component={LogIn} />
+            <Route path='/register' component={SignUp} />
           </main>
+          <button onClick={this.logOut}>Log Out</button>
         </header>
       </div>
     );
