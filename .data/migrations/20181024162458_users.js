efc1098dde18712ee3b9a1ae74e3ed7f2a4275bf
lyang9 <lyang9@buffalo.edu>
2018-10-24 16:34:14 -0400

    Made changes to exports.down

--- .data/migrations/20181024162458_users.js
+++ .data/migrations/20181024162458_users.js
@@ -11,5 +11,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('users');  
+  return knex.schema.dropTableIfExists('users');  
 };
