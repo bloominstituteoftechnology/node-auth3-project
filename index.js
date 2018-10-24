73718864e5a7378003f41ad95543b021eb09fa9b
lyang9 <lyang9@buffalo.edu>
2018-10-24 18:51:27 -0400

    Completed all endpoints

--- index.js
+++ index.js
@@ -20,7 +20,88 @@ server.get('/', (req, res) => {
 // endpoints here
 
 // register
+server.post('/register', (req, res) => {
+  const credentials = req.body;
 
+  const hash = bcrypt.hashSync(credentials.password, 10);
+  credentials.password = hash;
+
+  db('users')
+    .insert(credentials)
+    .then(ids => {
+      const id = ids[0];
+      res.status(201).json({ newUserId: id });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+
+const jwtSecret = 'yabba dabba doo!';
+
+function generateToken(user) {
+  const jwtPayload = {
+    ...user,
+    hello: 'FSW13',
+    roles: ['admin', 'root'],
+  };
+  const jwtOptions = {
+    expiresIn: '1m', // expires in 1min
+  };
+
+  return jwt.sign(jwtPayload, jwtSecret, jwtOptions);
+}
+
+// login
+server.post('/login', (req, res) => {
+  const creds = req.body;
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user); // generate token for specific user
+        res.status(200).json({ welcome: user.username, token });
+      } else {
+        res.status(401).json({ message: 'username or password is incorrect' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ err });
+    });
+});
+
+// users
+server.get('/users', protected, (req, res) => {
+  db('users')
+    .select('id', 'username', 'password')
+    .then(users => {
+      res.json({ users });
+    })
+    .catch(err => res.send(err));
+});
+
+function protected(req, res, next) {
+  // authentication tokens are normally sent as a header instead of the body
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
+      if (err) {
+        // token verification failed
+        res.status(401).json({ message: 'invalid token' });
+      } else {
+        // token is valid
+        req.decodedToken = decodedToken; // any sub-sequent middleware of route handler have access to this
+        console.log('\n** decoded token information **\n', req.decodedToken);
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: 'no token provided' });
+  }
+}
 
 // listening port
 const port = 5000;
