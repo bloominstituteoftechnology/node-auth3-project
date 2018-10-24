8ed91f91baf3ef667b5955dab3b93c3c03ab405c
Ray Lomeli <raylom96@gmail.com>
2018-10-24 15:35:31 -0500

    added dependencies

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./db.sqlite3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+      tableName: "dbmigrations"
+    }
+  }
+};
