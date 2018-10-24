057e9758c7f10ae88bc9739fa188a5ed847e8238
verydecent <wnjhwng@gmail.com>
2018-10-24 15:06:55 -0700

    importing knex configuration to server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 // dependencies
 const express = require('express');
+// databse
+const db = require('./data/dbConfig.js');
 // express
 const server = express();
 // middleware
 server.use(express.json());
+
 
 // port
 const port = 7000;
