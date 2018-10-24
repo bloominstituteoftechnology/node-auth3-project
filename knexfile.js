84fee741e713e2ea464af8fc3bccdbe7eb4c03aa
mich-shrak <micah.shrak@gmail.com>
2018-10-24 16:22:20 -0700

    Installed dependencies

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+// Update with your config settings.
+
+module.exports = {
+	development: {
+		client: 'sqlite3',
+		connection: {
+			filename: './database/auth.sqlite3',
+		},
+		migrations: {
+			directory: './database/migrations',
+		},
+	},
+};
