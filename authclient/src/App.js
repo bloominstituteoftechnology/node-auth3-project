da81351669d2758eb4220fd5b1a090f76b8378ec
lyang9 <lyang9@buffalo.edu>
2018-10-25 13:19:54 -0400

    Imported NavLink and Route. Rendered NavLink and Route for Home and Users

--- authclient/src/App.js
+++ authclient/src/App.js
@@ -1,24 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { NavLink, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <nav>
+            <NavLink exact to='/'>Home</NavLink>
+            <NavLink to='/users'>Users</NavLink>
+          </nav>
+          <main>
+            <Route path='/' Component={Home}></Route>
+            <Route path='/users' Component={Users}></Route>
+          </main>
         </header>
       </div>
     );
