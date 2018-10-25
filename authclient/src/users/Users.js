f148e8f6bac3830c92b60cb9553c6bb3c32eb4c6
lyang9 <lyang9@buffalo.edu>
2018-10-25 13:01:51 -0400

    Class Users components and export Users

---
+++ authclient/src/users/Users.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Users extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Users List</h2>
+      </div>
+    );
+  }
+}
+
+export default Users;
