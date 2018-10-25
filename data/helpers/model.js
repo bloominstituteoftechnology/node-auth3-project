11b8e89445cf316a9e9c39e80bdab77eb5ece2e8
Abdulfatah Ahmad <abdulfatah.a.ahmad@gmx.com>
2018-10-25 10:28:14 -0400

    Restructured files

---
+++ data/helpers/model.js
@@ -0,0 +1,36 @@
+const knex = require('knex');
+const bcrypt = require('bcryptjs');
+
+const knexConfig = require('../knexfile');
+const db = knex(knexConfig.development);
+
+module.exports = {
+	getUsers,
+	addUser,
+	login
+};
+
+function getUsers() {
+	return db('users');
+}
+
+function addUser(user) {
+	return db('users')
+		.insert(user)
+		.then(id => {
+			console.log(id);
+			return { ...user, id };
+		});
+}
+
+function login(credentials) {
+	return db('users')
+		.where({ username: credentials.username })
+		.then(([user]) => {
+			if (user && bcrypt.compareSync(credentials.password, user.password)) {
+				return user;
+			} else {
+				return undefined;
+			}
+		});
+}
