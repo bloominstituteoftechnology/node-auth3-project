9f0de5fec8d7ea991bd72071636b39806077b6a4
verydecent <wnjhwng@gmail.com>
2018-10-25 15:53:04 -0700

    Working on a big bug

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
 // databse
 const db = require('./data/dbConfig.js');
 // express
 const server = express();
 // middleware
 server.use(express.json());
+server.use(cors());
 
 // endpoints
 server.post('/api/register', (req, res) => {
@@ -19,26 +21,21 @@ server.post('/api/register', (req, res) => {
         .insert(credentials)
             .then(ids => {
                 const id = ids[0];
-                console.log('from', id);
                 res.status(200).json({ newUserId: id });
             })
             .catch(err => {
-                console.log('register post error');
                 res.status(500).json(err);
             });
 });
 
 server.post('/api/login', (req, res) => {
     const credentials = req.body;
-    console.log(credentials)
     db('users')
         .where({ username: credentials.username })
         .first()
             .then(user => {
                 if(user && bcrypt.compareSync(credentials.password, user.password)) {
-                    console.log(user);
                     const token = generateToken(user);
-                    console.log(token);
                     res.status(200).json({ welcome: user.username, token });
                 } else {
                     res.status(400).json({ message: 'you shall not pass!' });
@@ -55,17 +52,17 @@ function generateToken(user) {
     const jwtPayload = {
         ...user,
         hello: 'FSW13',
-        role: 'admin'
+        role: ['admin', 'role']
     }
 
     const jwtOptions = {
-        expiresIn: '1m'
+        expiresIn: '1h'
     }
 
     return jwt.sign (jwtPayload, jwtSecret, jwtOptions)
 }
 
-server.get('/api/users', protected, (req, res) => {
+server.get('/api/users', protected, checkRole('admin'), (req, res) => {
     console.log('\n** decoded token information ** \n', req.codedToken);
     db('users')
         .select('id', 'username', 'password', 'department')
@@ -73,9 +70,19 @@ server.get('/api/users', protected, (req, res) => {
                 res.status(200).json(users);
             })
             .catch(err => {
-                res.status(200).json(err);
+                res.status(500).json(err);
             });
 });
+
+function checkRole(role) {
+    return function(req, res, next) {
+        if(req.decodedToken && req.decodedToken.roles.includes(role)) {
+            next();
+        } else {
+            res.status(403).json({ messsage: 'you shall not pass! forbidden '});
+        }
+    };
+}
 
 function protected(req, res, next) {
     const token = req.headers.authorization;
