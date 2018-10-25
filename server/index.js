fbdd61700f8101e74ea8d9356c9d948d800170a8
verydecent <wnjhwng@gmail.com>
2018-10-24 18:30:09 -0700

    adds protect middleware

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 // dependencies
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 // databse
 const db = require('./data/dbConfig.js');
 // express
 const server = express();
 // middleware
 server.use(express.json());
+
 // endpoints
 server.post('/api/register', (req, res) => {
     const credentials = req.body;
@@ -26,7 +28,45 @@ server.post('/api/register', (req, res) => {
             });
 });
 
-server.get('/api/users', (req, res) => {
+server.post('/api/login', (req, res) => {
+    const credentials = req.body;
+    console.log(credentials)
+    db('users')
+        .where({ username: credentials.username })
+        .first()
+            .then(user => {
+                if(user && bcrypt.compareSync(credentials.password, user.password)) {
+                    console.log(user);
+                    const token = generateToken(user);
+                    console.log(token);
+                    res.status(200).json({ welcome: user.username, token });
+                } else {
+                    res.status(400).json({ message: 'you shall not pass!' });
+                }
+            })
+            .catch(err => {
+                res.status(500).json(err);
+            });
+});
+
+const jwtSecret = 'nobody tosses a dwarf';
+
+function generateToken(user) {
+    const jwtPayload = {
+        ...user,
+        hello: 'FSW13',
+        role: 'admin'
+    }
+
+    const jwtOptions = {
+        expiresIn: '1m'
+    }
+
+    return jwt.sign (jwtPayload, jwtSecret, jwtOptions)
+}
+
+server.get('/api/users', protected, (req, res) => {
+    console.log('\n** decoded token information ** \n', req.codedToken);
     db('users')
         .select('id', 'username', 'password', 'department')
             .then(users => {
@@ -36,6 +76,24 @@ server.get('/api/users', (req, res) => {
                 res.status(200).json(err);
             });
 });
+
+function protected(req, res, next) {
+    const token = req.headers.authorization;
+
+    if(token) {
+        jwt.verify(token, jwtSecret, (err, decodedToken) => {
+            (err) ?
+            // token verification error
+            res.status(401).json({ message: 'invalid token' })
+            :
+            // token valid
+            req.decodedToken = decodedToken;
+            next();
+        })
+    } else {
+        res.status(401).json({ message: 'no token provided' });
+    }
+}
 
 // port
 const port = 7000;
