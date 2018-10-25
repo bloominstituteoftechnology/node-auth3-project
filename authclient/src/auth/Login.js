8db2b3b64581f0095dc3e8fabb583fadab80f860
lyang9 <lyang9@buffalo.edu>
2018-10-25 18:51:46 -0400

    Implemented localStorage for token called jwt

--- authclient/src/auth/Login.js
+++ authclient/src/auth/Login.js
@@ -11,11 +11,21 @@ class Login extends Component {
       <form onSubmit={this.handleSubmit}>
         <div>
           <label htmlFor='username'>Username</label>
-          <input type='text' />
+          <input 
+            name='username' 
+            value={this.state.username}
+            onChange={this.handleInputChange} 
+            type='text' 
+          />
         </div>
         <div>
           <label htmlFor='password'>Password</label>
-          <input type='password' />
+          <input 
+            name='password' 
+            value={this.state.password} 
+            onChange={this.handleInputChange} 
+            type='password' 
+          />
         </div>
         <div>
           <button type='submit'>Login</button>
@@ -23,6 +33,11 @@ class Login extends Component {
       </form>
     );
   }
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value })
+  };
 
   handleSubmit = event => {
     event.preventDefault();
@@ -34,6 +49,7 @@ class Login extends Component {
       .post(endpoint, this.state)
       .then(res => {
         console.log(res.data);
+        localStorage.setItem('jwt', res.data.token)
       })
       .catch(err => {
         console.error('ERROR', err);
