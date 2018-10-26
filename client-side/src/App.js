36543ab07bf6bcc87605b007c9f8f8ea604c85c8
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-10-26 08:42:53 -0500

    forgot to commit last night. error 404 on login cannot resolve.

---
+++ client-side/src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import {NavLink, Route} from 'react-router-dom';
+import Users from './Components/Users';
+import Signin from './Auth/Signin';
+
+import './App.css';
+
+
+
+const Home = props => {
+  return (
+    <div>
+      <h1>Home Component</h1>
+    </div>
+  )
+};
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <nav>
+          <NavLink to='/' exact>
+            Home
+          </NavLink>
+          &nbsp;|&nbsp;
+          <NavLink to='/users'>
+            Users
+          </NavLink>
+          &nbsp;|&nbsp;
+          <NavLink to="/login">Signin</NavLink>
+          &nbsp;|&nbsp;
+          <button onClick={this.logout}>Signout</button>
+        </nav>
+        <main>
+          <Route exact path = '/' component= {Home}></Route>
+          <Route path = '/users' component= {Users}></Route>
+          <Route path="/login" component={Signin} />
+        </main>
+        </header>
+      </div>
+    );
+  }
+  logout = () => {
+    localStorage.removeItem('jwt');
+  };
+}
+
+export default App;
---
+++ client-side/src/Components/Users.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Users extends Component {
+    state = {
+        users: []
+    };
+
+  render() {
+    return (
+      <div>
+        <h2>User List</h2>
+        <ul>
+            {this.state.users.map(user => (<li key={user.id}>{user.username}</li>))}
+        </ul>
+      </div>
+    );
+  }
+  componentDidMount() {
+      const token = localStorage.getItem('jwt');
+      const endpoint = 'http://localhost:8000/api/users';
+      const options = {
+          headers: {
+              Authorization: token,
+          },
+      };
+      axios
+        .get(endpoint, options)
+        .then(res => {
+            console.log(res.data);
+            this.setState({users:res.data.users});
+        })
+        .catch(err => {
+            console.log('ERROR', err)
+        });
+  }
+}
+
+export default Users;
