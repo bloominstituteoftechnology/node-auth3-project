801a89e1d8222e25ce4fcd4a1ece70b1b4106c42
Lee Len Formento <leebatef@gmail.com>
2018-11-27 17:19:20 -0500

    Redirect to /users when logged in

--- auth/src/authorize/Signin.js
+++ auth/src/authorize/Signin.js
@@ -51,6 +51,7 @@ class Signin extends Component {
       .then(res => {
           console.log(res.data);
           localStorage.setItem('jwt', res.data.token)
+          this.props.history.push('/users');
       })
       .catch(err => {
           console.error('ERROR!', err)
