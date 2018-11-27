c2945e09ef6b067748b8b44509c2e1ee6d8c8be4
Lee Len Formento <leebatef@gmail.com>
2018-11-27 10:47:58 -0500

    POST register

--- index.js
+++ index.js
@@ -11,4 +11,28 @@ server.get('/', (req, res) => {
     res.send('Whazzahh!');
   });
 
+server.post('/register', (req,res) => {
+    const credentials = req.body;
+    // hash!
+    const hash = bcrypt.hashSync(credentials.password, 2)
+    credentials.password = hash;
+    db('users')
+    .insert(credentials)
+    .then(ids => {
+        const id = ids[0];
+        res.status(201).json({newUserId: id})
+    })
+})
+
+// testing to see if a user has been created
+server.get('/users', (req, res) => {
+    db('users')
+      .select('id', 'username', 'password', 'department')
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err));
+  });
+  
+
 server.listen(4200, () => console.log('\nParty at port 4200\n'));
