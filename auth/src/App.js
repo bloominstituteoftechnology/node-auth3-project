64b05d2e8a8cc363d18248007fa1ca80dd883df1
Lee Len Formento <leebatef@gmail.com>
2018-11-27 17:08:56 -0500

    Implemented GET users - testing with the sign in

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import { NavLink, Route } from 'react-router-dom';
+import Users from './users/Users';
+
+const Home = props => {
+  return (
+    <div>
+      <h1> Home </h1>
+    </div>
+  )
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>Hola!</h2>
+        <header className="App-header">
+          <nav>
+            <NavLink to="/" exact>Home</NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/users">Users</NavLink>
+            &nbsp;|&nbsp;
+            </nav>
+            <main>
+              <Route path="/" component={Home} exact />
+              <Route path="/users" component={Users} />
+          </main>
+          </header>
       </div>
     );
   }
