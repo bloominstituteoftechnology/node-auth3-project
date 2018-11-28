3df5bdee10b00d5345c6d4fc79ee3590bebb26b2
sean-one <basement365@gmail.com>
2018-11-28 11:53:58 -0800

    created table and added root endpoint to check

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 require('dotenv').config();
 
 const express = require('express');
+const knex = require('knex');
+const knexConfig = require('./knexfile');
 
-
+const db = knex(knexConfig.development);
 const server = express();
+
+server.get('/', (req, res) => {
+    res.json('runnin!')
+})
 
 const port = 3600;
 server.listen(port, console.log(`\n ~~~ we can hear you on port ${port} ~~~\n`));
