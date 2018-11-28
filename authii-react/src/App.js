15d4acc2ceee844039bb95e9ecd5fd9bfbceb9a1
Brandon Vilaychith <blvilaychith@outlook.com>
2018-11-28 16:52:58 -0600

    Routes started

--- authii-react/src/App.js
+++ authii-react/src/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// Components import
+import Signin from './components/Signin.js';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <h1>Hello</h1>
+          <Switch>
+            <Route path="/signin" component={Signin} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
--- authii-react/src/index.js
+++ authii-react/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// Personal imports
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
