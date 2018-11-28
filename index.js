522c13d0cda2a3f8c29606eb725b9b9dd39f6723
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2018-11-28 16:08:04 -0700

    post requests

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
@@ -9,6 +11,51 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+server.post('/api/register', (req, res) => {
+    // grab username and password from body
+    const creds = req.body;
+    // generate the hash from the user's password
+    const hash = bcrypt.hashSync(creds.password, 4);
+    // override the user.password with the hash
+    creds.password = hash;
+    // save the user to the database
+    db('users')
+        .insert(creds)
+        .then(ids => res.status(201).json(ids))
+        .catch(err => res.status(401).json(err))
+});
 
+function generateToken(user) {
+    const payload = {
+        subject: user.id, 
+        username: user.username,
+        roles: []
+    };
+    const secret = process.env.JWT_SECRET;
+    const options = {
+        expiresIn: '2m'
+    };
+    return jwt.sign(payload, secret, options);
+};
+
+server.post('/api/login', (req, res) => {
+    // grab username and password from body
+    const creds = req.body;
+    db('users')
+        .where({ username: creds.username })
+        .first()
+        .then(user => {
+            if(user && bcrypt.compareSync(creds.password, user.password)) {
+            // passwords match and user exists by that username
+            // created a session > create a token
+            // library sent cookie automatically > we send the token manually
+            const token = generateToken(user);
+            res.status(200).json({ message: 'welcome', token})
+            } else {
+            res.status(401).json({ message: 'you shall not pass!' })
+            }
+        })
+        .catch(err => res.json(err));
+});
 
 server.listen(3300, () => console.log('\nrunning on port 3300\n'));
