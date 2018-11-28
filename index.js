db710d7beb49d2f15689073e0f43211240a80bbf
Unknown <zachary.arney@gmail.com>
2018-11-28 13:41:29 -0600

    index and dbConfig setup

---
+++ index.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+server.listen(3300, () => console.log('\nrunning on port 3300\n'));
