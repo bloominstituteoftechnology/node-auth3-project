7f1244e9edb09c72666499e76f7862fbf51ac89f
Katie Cleary <katiecleary@airmaster.lan>
2018-11-28 16:40:04 -0500

    added generateToken, and protected functions

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ const db = require('./database/dbConfig.js');
 const server = express();
 server.use(express.json());
 server.use(cors());
+
+function generateToken(user) {
+    const payload = {
+      subject: user.id,
+      username: user.username,
+      department: user.department,
+    };
+
+const secret = process.env.JWT_SECRET;
+const options = {
+    expiresIn: '1m',
+  };
+  return jwt.sign(payload, secret, options);
+}
