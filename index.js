ca82494f1355368ef6f97dc877d52a649944c668
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-11-28 14:31:44 -0800

    register endpoint

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const db = require("./database/dbConfig");
 
 const server = express();
 
@@ -28,4 +29,24 @@ function generateToken(user) {
   return jwt.sign(payload, secret, options);
 }
 
-server.listen(PORT, () => console.log("\nPORT is LIVE\n"));
+// local middleware
+
+// POST to /api/register
+server.post("/api/register", (req, res) => {
+  // grab username, password, and department
+  const creds = req.body;
+
+  // hash the password
+  const hash = bcrypt.hashSync(creds.password, 14);
+  creds.password = hash;
+
+  db("users")
+    .insert(creds)
+    .then(id => res.status(200).json(id))
+    .catch(err => res.status(500).json(err));
+});
+// POST to /api/login
+
+// GET from /api/users
+
+server.listen(PORT, () => console.log(`\n${PORT} is LIVE\n`));
