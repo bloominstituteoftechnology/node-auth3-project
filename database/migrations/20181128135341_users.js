80b9daced382d5e289f2021d7434396499fa0a9c
SavannahGreuel <smallidgesavannah@gmail.com>
2018-11-28 14:13:26 -0700

    added migration data and POST endpoint

--- database/migrations/20181128135341_users.js
+++ database/migrations/20181128135341_users.js
@@ -1,8 +1,23 @@
 
 exports.up = function(knex, Promise) {
-  
-};
+    return knex.schema.createTable('users', users => {
+        users.increments();
 
-exports.down = function(knex, Promise) {
-  
-};
+        users
+            .string('username', 128)
+            .notNullable()
+            .unique();
+
+        users
+            .string('password', 128)
+            .notNullable();
+
+        users
+            .string('department', 128)   
+            .notNullable(); 
+        });
+    };
+
+    exports.down = function(knex, Promise) {
+        return knex.schema.dropTableIfExists('users');
+    };
--- index.js
+++ index.js
@@ -13,4 +13,31 @@ server.use(express.json());
 server.use(cors());
 
 
+// POST /api/register
+server.post('/api/register', (req, res) => {
+    const creds = req.body;
+    const hash = bcrypt.hashSync(creds.password, 14);
+
+    creds.password = hash;
+
+    db('users')
+    .insert(creds)
+    .then(ids=> {
+        res.status(201).json({message:'user added', ids});
+    })
+    .catch(err => json(err));
+})
+
+// GET /api/login
+
+// GET /api/users
+
+
+//GET /
+server.get('/', (req, res) => {
+    res.send('Its Alive!');
+  });
+
+
+
 server.listen(5000, () => console.log('====================\nrunning on port 5000\n===================='));
