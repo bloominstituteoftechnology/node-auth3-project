b5ebaa98fd088a10c21c56b51930abb2e5c31d20
ssmodish <smodish@hotmail.com>
2018-11-28 18:48:48 -0500

    Wednesday MVP

--- index.js
+++ index.js
@@ -27,6 +27,22 @@ function generateToken(user) {
   return jwt.sign(payload, secret, options);
 }
 
+function protected(req, res, next) {
+  const token = req.headers.authorization;
+  if(token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if(err) {
+        res.status(401).json({ message: 'broken token'});
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    })
+  } else {
+    res.status(401).json({ message: 'no token'});
+  }
+}
+
 server.post('/api/login', (req, res) => {
   const creds = req.body;
 
@@ -63,4 +79,17 @@ server.get('/', (req, res) => {
   res.send('yep');
 });
 
+
+server.get('/api/users', protected, (req, res) => {
+  db('users')
+    .select('id', 'username', 'password')
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+})
+
+
+
+// this is the last line
 server.listen(8800, () => console.log('\nRunning on Port 8800\n'));
