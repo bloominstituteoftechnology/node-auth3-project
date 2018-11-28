eb0b774dd4be0c9cf22a487b9a10e964a0008e58
theryanclausen <theryanclausen@gmail.com>
2018-11-28 16:47:33 -0700

    mvp

--- config/middleware.js
+++ config/middleware.js
@@ -1,23 +1,37 @@
+//require("dotenv").config();
+
+//const jwt = require("jsonwebtoken");
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
-
 module.exports = server => {
-  const protected = (req, res, next) => {
-    const {path} = req
-    let reg = /(?:api\/)(\w+)\/*/g
-    let destination = reg.exec(path)[1]
-    if (destination === 'users'){
-        if (0) {
-            return res.status(401).json({ message: "kick rocks, snoopy!" });
-          } 
-    }
-    next();
-  };
+  // const protected = async (req, res, next) => {
+  //   const token = await req.headers.authorization;
+  //   console.log(token);
+  //   const { path } = req;
+  //   let reg = /(?:api\/)(\w+)\/*/g;
+  //   let destination = reg.exec(path)[1];
+  //   if (destination === "users") {
+  //     if (token) {
+  //       jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+  //         console.log(decodedToken)
+  //         if (err) {
+  //           return res.status(401).json({ message: "Broken Token" });
+  //         } else {
+  //           req.decodedToken = decodedToken;
+  //           next();
+  //         }
+  //       });
+  //     } else {
+  //       return res.status(401).json({ message: "No Token, No Dice" });
+  //     }
+  //   }
+  //   next();
+  // };
   server.use(express.json());
   server.use(cors());
-  server.use(protected);
+  //server.use(protected);
   server.use(helmet());
 
   return server;
--- index.js
+++ index.js
@@ -1,12 +1,36 @@
+
+
+require("dotenv").config();
+
+const jwt = require("jsonwebtoken");
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const port = 3334;
 
 const db = require("./config/dbConfig");
 const middleware = require("./config/middleware");
+const generateToken = require('./config/tokenConfig');
 const server = express();
 
 middleware(server);
+
+const protected =(req, res, next) => {
+    const token = req.headers.authorization;
+  
+    if (token) {
+      jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+        if (err) {
+          res.status(401).json({ message: 'broken token' });
+        } else {
+          req.decodedToken = decodedToken;
+          next();
+        }
+      });
+    } else {
+      // bounced
+      res.status(401).json({ message: 'no token, no dice' });
+    }
+  }
 
 const sendError = (res, message = "Server Error", code = 500) => {
   return res.status(code).json({ error: message });
@@ -16,7 +40,7 @@ server.get("/", (req, res) => {
   res.send("<h1>Built by Ryan Clausen</h1>");
 });
 
-server.get("/api/users", async (req, res) => {
+server.get("/api/users", protected, async (req, res) => {
   const users = await db("users");
   return res.status(200).json(users);
 });
@@ -46,7 +70,8 @@ server.post("/api/login", async (req, res) => {
       .where({ username: credentials.username })
       .first();
     if (user && bcrypt.compareSync(credentials.password, user.password)) {
-      return res.status(200).json({ message: "You did good" });
+        const token = generateToken(user)
+      return res.status(200).json({ message: "You did good", token });
     } else {
       return sendError(res, "get outta here", 401);
     }
