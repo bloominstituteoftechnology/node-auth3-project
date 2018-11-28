6eaf766daea32952c5500632fe1eae21f700e686
Katie Cleary <katiecleary@airmaster.lan>
2018-11-28 16:35:07 -0500

    connected dependecies in index.js, created env file

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const db = require('./database/dbConfig.js');
+
+const server = express();
+server.use(express.json());
+server.use(cors());
