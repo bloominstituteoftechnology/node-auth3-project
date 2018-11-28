a4480e0c11240025c380f090def808b0a49de2ad
lajawanti <myemail>
2018-11-28 16:42:41 -0500

    Login endpoint token added..

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
+
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const db = require('./database/dbConfig.js');
 
@@ -23,6 +26,22 @@ server.post('/api/register', (req, res) => {
                .catch(err => res.send(err));
 });
 
+//FUNCTION TO GENERATE TOKEN WHICH WILL BE SAVED AT CLIENT END..
+function generateToken(user) {
+    const payload = {
+          subject : user.id,
+          username : user.username,
+          roles : ['sales', 'admin'], //from database..
+    };
+    const secret = process.env.JWT_SECRET;
+
+    const options = {
+           expiresIn : '1h',
+    };
+
+    return jwt.sign(payload, secret, options);
+}
+
 //USER LOGIN...To check authenticated users
 server.get('/api/login', (req, res) => {
         const credentials = req.body;
@@ -31,29 +50,13 @@ server.get('/api/login', (req, res) => {
                 .first()
                 .then(user => {
                      if(user && bcrypt.compareSync(credentials.password, user.password)) {
-                            res.status(200).json({message : "Logged In"});
+                            const token = generateToken(user);
+                            res.status(200).json({message : "Logged In", token});
                      } else {
                             res.status(401).json({message : "Invalid username or password.."})
                      }
                  })
                 .catch(err => res.send({Message : "Error in Logging In..."}));
 })
-//USER LOGIN...To check uthenticated users
-/*server.get('/api/login', (req, res) => {
-    const credentials = req.body;
-    db('users')
-        .where({username : credentials.username})
-        .first()
-        .then(user => {
-             if(user && bcrypt.compareSync(credentials.password, user.password)) {
-                //req.session.userId = user.id; 
-                res.status(200).json({message : "Logged In"})
-             } else {
-                 res.status(401).json({message : "Invalid username or password.."})
-             }
-         })
-        .catch(err => res.send(err));
-});*/
-
 
 server.listen(3300, () => console.log('\nrunning on port 3300\n'));
