ad2a973e1d1e350953ed3e2fa7ed319163408d49
rytwalker <rytwalker@gmail.com>
2018-11-28 16:38:35 -0500

    Added department column

---
+++ data/migrations/20181128163358_departments.js
@@ -0,0 +1,11 @@
+exports.up = function(knex) {
+  return knex.schema.table('users', table => {
+    table.string('department', 255);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('users', table => {
+    table.dropColumn('department');
+  });
+};
