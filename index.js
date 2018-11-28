7b54f711d84258e6717e82a82dcb7aa1a449bdbd
Brandon Strellis <bstrellis@gmail.com>
2018-11-28 16:46:54 -0500

    Added GET request to /api/users

--- index.js
+++ index.js
@@ -14,5 +14,33 @@ server.get('/', (req, res) => {
     res.status(200).json({ message: 'At /'});
 });
 
+// database endpoints
+// register a new user
+server.post('/api/register', async (req, res) => {
+    const userCreds = req.body;
+    try {
+        const returned = await db('users').insert(userCreds);
+        // need to hash pw
+        res.status(200).json(returned);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+// get the list of users
+server.get('/api/users', async (req, res) => {
+    try {
+        const users = await db('users');
+        res.status(200).json(users);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+
 const port = 5000;
 server.listen(port, () => console.log(`\nServer running on port ${port}\n`));
