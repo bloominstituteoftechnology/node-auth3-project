61d4d43205af0134571e48661a24afc997e30085
jsmnglng <jsmn.glng@gmail.com>
2018-11-28 12:13:09 -0800

    API test: success

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+
 const db = require('./database/dbConfig');
+
+const server = express();
+const port = 8080;
+
+// test API
+server.get('/', (_, res) => {
+  res.send('API is live!')
+});
+
+server.listen(port, () => console.log(`Listening to port: ${port}`));
