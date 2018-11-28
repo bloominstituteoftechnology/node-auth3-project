a21a5bb555f5e2500762e46319c4e30c4bfc49d8
adamhinckley <adamhinckley@mac.com>
2018-11-28 14:46:51 -0700

    Day one MVP done

--- index.js
+++ index.js
@@ -13,42 +13,75 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-// server.post('api/register', (req, res) => {
-//   //get username and password from body
-//   const creds = req.body;
-//   //generate hash
-//   const hash = bcrypt.hashSync(creds.password, 10);
-//   //override the password with hash
-//   creds.password = hash;
-//   db('users')
-//   .insert(creds)
-//   .then(ids => {
-//     res.status(201).json(ids)
-//   })
-//   .catch( err => json({ message: 'error adding user to the DB', err}))
-// })
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    roles: ["admin", "grunts", "big boss"]
+  };
 
-server.post("/api/register", (req, res) => {
-  // grab username and password from body
+  const secret = process.env.JWT_SECRET;
+  const options = {
+    expiresIn: "1m"
+  };
+  return jwt.sign(payload, secret, options);
+}
+
+server.post("api/register", (req, res) => {
+  //get username and password from body
   const creds = req.body;
-
-  // generate the hash from the user's password
-  const hash = bcrypt.hashSync(creds.password, 4); // rounds is 2^X
-
-  // override the user.password with the hash
+  //generate hash
+  const hash = bcrypt.hashSync(creds.password, 10);
+  //override the password with hash
   creds.password = hash;
-
-  // save the user to the database
   db("users")
     .insert(creds)
     .then(ids => {
       res.status(201).json(ids);
     })
-    .catch(err => json(err));
+    .catch(err => json({ message: "error adding user to the DB", err }));
 });
 
-server.get("/", (req, res) => {
-  res.send("Its Alive!");
+server.post("/api/login", (req, res) => {
+  const creds = req.body;
+
+  db("users")
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ message: "welcome", token });
+      } else {
+        res.status(401).json({ message: "not authorized" });
+      }
+    })
+    .catch(err => res.json({ message: "error", err }));
+});
+
+function protected(req, res, next) {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodeToken) => {
+      if (err) {
+        res.status(401).json({ message: "invalid token", err });
+      } else {
+        req.decodeToken = decodeToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "no token provided" });
+  }
+}
+
+server.get("/api/users", protected, (req, res) => {
+  db("users")
+    .select("id", "username", "password")
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
 });
 
 server.listen(4200, () => console.log("running on port 4200"));
