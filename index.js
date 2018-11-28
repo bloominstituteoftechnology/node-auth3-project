e62dcd46fc39cfbae46588192c23b73633c7b7d0
unknown <quan.nguyen199712@gmail.com>
2018-11-28 17:23:42 -0500

    rewrite and fix errors

--- index.js
+++ index.js
@@ -1,97 +1,125 @@
-require('dotenv').config(); 
+require('dotenv').config();
 
-
-const db = require("./dbConfig");
 const express = require('express');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const bcrypt = require('bcryptjs'); 
+const db = require('./dbConfig');
+
 const server = express();
+
+server.use(express.json());
+server.use(cors());
 
 function generateToken(user) {
   const payload = {
     subject: user.id,
-    username: user.username,
-    roles: ['sales', 'marketing'], // this will come from the database
+    username: user.username
   };
 
   const secret = process.env.JWT_SECRET;
   const options = {
-    expiresIn: '1m',
+    expiresIn: '1h',
   };
 
   return jwt.sign(payload, secret, options);
 }
 
-server.use(express.json());
-server.use(cors());
-
 server.post('/api/login', (req, res) => {
-    // grab username and password from body
-    const creds = req.body;
-  
-    db('users')
-      .where({ username: creds.username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(creds.password, user.password)) {
-          // passwords match and user exists by that username
-          const token= generateToken(user);
-          res.status(200).json({ message: 'Come on in!' });
-        } else {
-          // either username is invalid or password is wrong
-          res.status(401).json({ message: 'invalid username or password' });
-        }
-      })
-      .catch(err => res.json(err));
-  });
-server.post("/api/register", (req, res) => {
+  // grab username and password from body
   const creds = req.body;
-  const hash = bcrypt.hashSync(creds.password, 4);
-  creds.password = hash;
-  db("users")
-    .insert(creds)
-    .then(ids => {
-      res.status(201).json(ids);
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+
+        const token = generateToken(user);
+        res.status(200).json({ message: 'welcome!', token });
+      } else {
+
+        res.status(401).json({ message: 'you shall not pass!!' });
+      }
     })
-    .catch(err => {
-      res.send(err);
-    });
+    .catch(err => res.json(err));
 });
+
 function protected(req, res, next) {
-  // token is normally sent in the the Authorization header
+
   const token = req.headers.authorization;
 
   if (token) {
-    // is it valid
+
     jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
       if (err) {
-        // token is invalid
+
         res.status(401).json({ message: 'invalid token' });
       } else {
-        // token is gooooooooooood
+
         req.decodedToken = decodedToken;
         next();
       }
     });
   } else {
-    // bounced
+
     res.status(401).json({ message: 'not token provided' });
   }
 }
-server.get("/api/users",protected, (req, res) => {
-  db("users")
-    .select("id", "username", "password")
+
+
+server.get('/api/me', protected, (req, res) => {
+  db('users')
+    .select('id', 'username', 'password')
+    .where({ id: req.session.user })
+    .first()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
 
+server.get('/api/users', protected, checkRole('sales'), (req, res) => {
+  db('users')
+    .select('id', 'username', 'password')
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+function checkRole(role) {
+  return function(req, res, next) {
+    if (req.decodedToken && req.decodedToken.roles.includes(role)) {
+      next();
+    } else {
+      res.status(403).json({ message: 'you have no access to this resource' });
+    }
+  };
+}
+
+server.post('/api/register', (req, res) => {
+  // grab username and password from body
+  const creds = req.body;
+
+  // generate the hash from the user's password
+  const hash = bcrypt.hashSync(creds.password, 4); // rounds is 2^X
+
+  // override the user.password with the hash
+  creds.password = hash;
+
+  // save the user to the database
+  db('users')
+    .insert(creds)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => json(err));
+});
+
 server.get('/', (req, res) => {
-    res.send('Its Alive!');
-  });
+  res.send('Its Alive!');
+});
 
-server.listen(3300, () => console.log("alive at 3300"));
-
+server.listen(3300, () => console.log('\nrunning on port 3300\n'));
