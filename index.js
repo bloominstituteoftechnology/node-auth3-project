ac57c3c310227d113199de1308517b2452e2730e
Unknown <zachary.arney@gmail.com>
2018-11-28 16:07:14 -0600

    login POST endpoint and generateToken function

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    role: user.department,
+  };
+
+  const secret = process.env.JWT_SECRET;
+  const options = {
+    expiresIn: '1h',
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+
 server.post('/api/register', (req, res) => {
   const creds = req.body;
   const hash = bcrypt.hashSync(creds.password, 11);
@@ -25,6 +40,24 @@ server.post('/api/register', (req, res) => {
     .catch((err) =>
       res.status(500).json({ message: 'could not register user', err })
     );
+});
+
+server.post('/api/login', (req, res) => {
+  const creds = req.body;
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then((user) => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+        // response sends token manually
+        res.status(200).json({ message: 'welcome!', token });
+      } else {
+        res.status(401).json({ message: 'you shall not pass!' });
+      }
+    })
+    .catch((err) => res.json(err));
 });
 
 server.get('/', (req, res) => {
