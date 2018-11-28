c9026b1ba6b923b1ab80babbc9c41bd4450a1090
sean-one <basement365@gmail.com>
2018-11-28 11:50:44 -0800

    server running

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // https://github.com/LambdaSchool/auth-ii/pull/273
 require('dotenv').config();
+
+const express = require('express');
+
+
+const server = express();
+
+const port = 3600;
+server.listen(port, console.log(`\n ~~~ we can hear you on port ${port} ~~~\n`));
