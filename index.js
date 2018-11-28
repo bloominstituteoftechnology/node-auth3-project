b2109f99ed563b11923ac80d0643156344d3c99e
David247365 <ds051128@gmail.com>
2018-11-28 17:18:31 -0600

    Created env file and generating a token during login

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const db = require("./data/dbConfig.js");
 
@@ -8,6 +11,20 @@ const server = express();
 
 server.use(express.json());
 server.use(cors());
+
+const generateToken = user => {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    department: user.department
+  };
+
+  const secret = process.env.JWT_SECRET;
+  const options = {
+    expiresIn: "1h"
+  };
+  return jwt.sign(payload, secret, options);
+};
 
 // POST - register
 
@@ -31,9 +48,34 @@ server.post("/api/register", (req, res) => {
 
 // POST - login
 
+server.post("/api/login", (req, res) => {
+  const creds = req.body;
+
+  db("users")
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ message: "Welcome!", token });
+      } else {
+        res.status(401).json({ message: "You shall not pass!!" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ err: "There was an error, please try again." });
+    });
+});
+
+// Protection
+
+function protected(req, res, next) {
+  next();
+}
+
 // POST - users
 
-server.get("/api/users", (req, res) => {
+server.get("/api/users", protected, (req, res) => {
   db("users")
     .select("username")
     .then(users => {
