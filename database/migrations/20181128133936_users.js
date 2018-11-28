c10e22123ef8fbeddc31992c2c6c39edfd089a25
samsisle <samko9522@gmail.com>
2018-11-28 15:21:28 -0800

    Configure database migrations

---
+++ database/migrations/20181128133936_users.js
@@ -0,0 +1,13 @@
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('users', users => {
+    users.increments();
+    users
+      .string('username', 128)
+      .notNullable()
+      .unique();
+    users.string('password', 128).notNullable();
+    users.string('department', 128).notNullable();
+  });
+};
+
+exports.down = (knex, Promise) => knex.schema.dropTableIfExists('users');
---
+++ knexfile.js
@@ -0,0 +1,8 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: { filename: './database/users.sqlite3' },
+    useNullAsDefault: true,
+    migrations: { directory: './database/migrations' }
+  }
+};
