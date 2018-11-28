b24044fc789b73d08771fa297686c9cb9c8fa82e
Stublink <chrisrey95@gmail.com>
2018-11-28 16:00:34 -0500

    :zap: added protected middleware that verifies and decodes jwt

--- index.js
+++ index.js
@@ -2,11 +2,43 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const db = require('./database/dbConfig');
 const server = express();
 
+const generateToken = (user) => {
 
+    const payload = {
+        subject: user.userId,
+        username: user.username,
+        roles: ['admin']
+    }
+
+    const secret = process.env.JWT_SECRET;
+    const options = {
+        expiresIn: '1h'
+    }
+
+    return jwt.sign(payload, secret, options)
+}
+
+const protected = (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if(token){
+        jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+            if(err) {
+                res.status(401).json({message: 'invalid token'})
+            } else {
+                req.decodedToken = decodedToken;
+                next();
+            }
+        })
+    } else {
+        res.status(401).json({message: 'no token provided'})
+    }
+}
 
 server.use(express.json());
 server.use(cors());
@@ -32,10 +64,27 @@ server.post('/api/register', (req, res) => {
 });
 
 server.post('/api/login', (req, res) => {
+    const creds = req.body;
+
+    if(!creds.username || !creds.password) {
+        res.status(422).json({message: 'username and password both required'});
+        return;
+    }
+
+    db('users').where({username: creds.username}).first()
+    .then(user => {
+        if (user && bcrypt.compareSync(creds.password, user.password)){
+            const token = generateToken(user);
+            res.status(200).json({message: 'success', token})
+        } else {
+            res.status(401).json({ message: 'error loggin in' });
+        }
+    })
+    .catch(err => res.json(err))
 
 });
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', protected, (req, res) => {
     db('users')
         .select('id', 'username', 'password')
         .then(users => {
