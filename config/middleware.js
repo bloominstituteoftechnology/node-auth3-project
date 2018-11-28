0d2c34010c6f539012db5366bc5c34c47501985a
theryanclausen <theryanclausen@gmail.com>
2018-11-28 15:19:53 -0700

    endpoints

---
+++ config/middleware.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+
+module.exports = server => {
+  const protected = (req, res, next) => {
+    const {path} = req
+    let reg = /(?:api\/)(\w+)\/*/g
+    let destination = reg.exec(path)[1]
+    if (destination === 'users'){
+        if (true) {
+            return res.status(401).json({ message: "kick rocks, snoopy!" });
+          } 
+    }
+    next();
+  };
+  server.use(express.json());
+  server.use(cors());
+  server.use(protected);
+  server.use(helmet());
+
+  return server;
+};
