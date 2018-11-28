4a6483ed3cdafa1b86ff77a316f3c1acc6a09d36
KHollobaugh <khollobaugh@gmail.com>
2018-11-28 15:53:12 -0500

    server is running

--- index.js
+++ index.js
@@ -10,3 +10,37 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+function generateToken(user) {
+    const payload = {
+        subject: user.id,
+        username: user.username,
+    }
+    const secret = process.env.JWT_SECRET;
+    const options = {
+        expiresIn: '1h'
+    }
+    return jwt.sign(payload, secret, options)
+}
+
+function protected(req, res, next) {
+    const token = req.headers.authorization;
+
+    if (token) {
+        jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+            if (err) {
+                res.status(401).json({ message: 'invalid token' })
+            } else {
+                req.decodedToken = decodedToken;
+                next();
+            }
+        })
+    } else {
+        res.status(401).json({ message: 'no token provided' })
+    }
+}
+
+server.get('/', (req, res) => {
+    res.send('server is running');
+})
+
+server.listen(3300, () => console.log('server is running on port 3300'));
