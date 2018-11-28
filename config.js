092dcfd1d46941dc52f1b26a78948d16ed639026
JacobABrennan <jacobabrennan@gmail.com>
2018-11-28 17:05:38 -0500

    Configure Database (table users)

---
+++ config.js
@@ -0,0 +1,15 @@
+
+
+//== Project Constants =========================================================
+
+module.exports = {
+
+    //-- Database ------------------------------------
+    TABLE_USERS: 'users',
+    FIELD_USERNAME  : 'username'  ,
+    FIELD_PASSWORD  : 'password'  ,
+    FIELD_DEPARTMENT: 'department',
+    LIMIT_USERNAME  : 128,
+    LIMIT_PASSWORD  : 128,
+    LIMIT_DEPARTMENT: 128,
+}
---
+++ database/seeds/users.js
@@ -0,0 +1,21 @@
+
+
+//== Users Table ===============================================================
+
+//-- Dependencies --------------------------------
+const config = require('../../config.js');
+
+//-- Test Data -----------------------------------
+const testData = [
+    {
+        [config.FIELD_USERNAME  ]: 'test',
+        [config.FIELD_PASSWORD  ]: '$2a$04$V2NVQ0uvMkLwIUrha9rfteMR.HS.mvDJVEsoH19lVNoYa5CDjyXu6',
+        [config.FIELD_DEPARTMENT]: 'Silly Walks',
+    },
+];
+
+//-- Seed Table with Example Data ----------------
+exports.seed = async function(knex, Promise) {
+    await knex(config.TABLE_USERS).del();
+    return await knex(config.TABLE_USERS).insert(testData);
+};
