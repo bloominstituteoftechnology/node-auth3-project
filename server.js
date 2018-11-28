539599464550ef589db6086dd5817cd79dc71dd7
Hargobind Atwal <ethaipath@gmail.com>
2018-11-28 12:30:24 -0800

    added api routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const server = express();
 
+server.use('/api', require('./api/apiRoutes'))
 server.get('/', (req, res) => {
   res.status(200).json({
     message: 'server up!'
