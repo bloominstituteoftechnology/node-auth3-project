c8064da6b85ef22673810431143761752e59942e
unknown <quan.nguyen199712@gmail.com>
2018-11-28 17:17:12 -0500

    finished mvp

--- index.js
+++ index.js
@@ -1,31 +1,51 @@
+require('dotenv').config(); 
+
+
 const db = require("./dbConfig");
-const express = require("express");
-const cors = require("cors");
-const bcrypt = require("bcryptjs");
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
+const bcrypt = require('bcryptjs'); 
 const server = express();
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    roles: ['sales', 'marketing'], // this will come from the database
+  };
+
+  const secret = process.env.JWT_SECRET;
+  const options = {
+    expiresIn: '1m',
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 server.use(express.json());
 server.use(cors());
 
-server.post("/api/login", (req, res) => {
-  // grab username and password from body
-  const creds = req.body;
-
-  db("users")
-    .where({ username: creds.username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(creds.password, user.password)) {
-        // passwords match and user exists by that username
-        req.session.user = user.id;
-        res.status(200).json({ message: "Come on in!" });
-      } else {
-        // either username is invalid or password is wrong
-        res.status(401).json({ message: "invalid username or password" });
-      }
-    })
-    .catch(err => res.json(err));
-});
+server.post('/api/login', (req, res) => {
+    // grab username and password from body
+    const creds = req.body;
+  
+    db('users')
+      .where({ username: creds.username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(creds.password, user.password)) {
+          // passwords match and user exists by that username
+          const token= generateToken(user);
+          res.status(200).json({ message: 'Come on in!' });
+        } else {
+          // either username is invalid or password is wrong
+          res.status(401).json({ message: 'invalid username or password' });
+        }
+      })
+      .catch(err => res.json(err));
+  });
 server.post("/api/register", (req, res) => {
   const creds = req.body;
   const hash = bcrypt.hashSync(creds.password, 4);
@@ -39,7 +59,28 @@ server.post("/api/register", (req, res) => {
       res.send(err);
     });
 });
-server.get("/api/users", (req, res) => {
+function protected(req, res, next) {
+  // token is normally sent in the the Authorization header
+  const token = req.headers.authorization;
+
+  if (token) {
+    // is it valid
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        // token is invalid
+        res.status(401).json({ message: 'invalid token' });
+      } else {
+        // token is gooooooooooood
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    // bounced
+    res.status(401).json({ message: 'not token provided' });
+  }
+}
+server.get("/api/users",protected, (req, res) => {
   db("users")
     .select("id", "username", "password")
     .then(users => {
@@ -47,3 +88,10 @@ server.get("/api/users", (req, res) => {
     })
     .catch(err => res.send(err));
 });
+
+server.get('/', (req, res) => {
+    res.send('Its Alive!');
+  });
+
+server.listen(3300, () => console.log("alive at 3300"));
+
