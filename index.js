bb355553c216314031a7c66a414839aa95189a10
sean-one <basement365@gmail.com>
2018-11-28 13:05:46 -0800

    unhashed register route

--- index.js
+++ index.js
@@ -8,8 +8,26 @@ const knexConfig = require('./knexfile');
 const db = knex(knexConfig.development);
 const server = express();
 
+server.use(express.json());
+
 server.get('/', (req, res) => {
     res.json('runnin!')
+})
+
+server.get('/api/users', async (req, res) => {
+    const users = await db('users');
+    res.status(200).json(users);
+})
+
+server.post('/api/register', async (req, res) => {
+    const creds = req.body;
+    if (!creds.username || !creds.password || !creds.department) {
+        res.status(400).json({ message: 'please fill all required inputs' });
+    } else {
+        const user = await db('users').insert(creds);
+        res.status(200).json(user);
+    }
+
 })
 
 const port = 3600;
