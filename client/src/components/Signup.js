76613c6dc0f7f7554e5f28a0d44ccca36354aeb6
Rahul Desai <rd825@georgetown.edu>
2018-11-28 16:48:33 -0500

    Created react app

---
+++ client/src/components/Signup.js
@@ -0,0 +1,52 @@
+
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Signup extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: null,
+            password: null
+        }
+    }
+
+    changeHandler = event => {
+        event.preventDefault();
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    signUp = event => {
+        event.preventDefault();
+        const {username, password} = this.state;
+        if (!username || !password) {
+            alert("Please enter a username and password");
+        } else {
+            axios.post('http://localhost:9000/api/register', {username, password})
+                .then(res => alert('Signup Complete!'))
+                .catch(err => console.dir(err));
+        }
+        this.setState({
+            username: null,
+            password: null,
+        })
+        event.target.reset();
+    }
+
+    render() { 
+        return (
+            <div>
+                <h3>Sign up here!</h3>
+                <form onSubmit={this.signUp}>
+                    <input type='text' placeholder='username' name='username' value={this.value} onChange={this.changeHandler}/>
+                    <input type='password' placeholder='password' name='password' value={this.value} onChange={this.changeHandler}/>
+                    <input type='submit' />
+                </form>
+            </div>
+        );
+    }
+}
+ 
+export default Signup;
