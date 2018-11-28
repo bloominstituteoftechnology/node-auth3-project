24d989e50349c39532f8f9a63dd1da0812be7a3c
Yanrong Xu <xuyanrong@icloud.com>
2018-11-28 15:34:58 -0500

    edit the knefile.js

--- knexfile.js
+++ knexfile.js
@@ -4,41 +4,14 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+    connection: { filename: './database/auth.sqlite3'},
+    useNullAsDefault: true,
+    migration: {
+      directory: './database/migrations',
+      tableName: 'dbmigrations',
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+    seeds: {
+      directory: './database/seeds'
     }
   }
-
 };
