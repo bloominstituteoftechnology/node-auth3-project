3586674b20e84fe6846d0aff60e010c736e69d64
Unknown <zachary.arney@gmail.com>
2018-11-28 16:04:31 -0600

    test GET endpoint

--- index.js
+++ index.js
@@ -4,4 +4,15 @@ const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const db = require('./data/dbConfig');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+  res.json({ message: 'Its Alive!' });
+});
+
 server.listen(3300, () => console.log('\nrunning on port 3300\n'));
