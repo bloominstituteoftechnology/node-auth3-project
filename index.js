c72f45665f3c1e1b54072f6474e20c623adfaf7f
Brandon Strellis <bstrellis@gmail.com>
2018-11-28 17:11:16 -0500

    Added hashing of pw to registration.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const knex = require('knex');
 const knexConfig = require('./knexfile');
+const bcryptjs = require('bcryptjs');
 
 // create server and define usages
 const server = express();
@@ -15,19 +16,36 @@ server.get('/', (req, res) => {
 });
 
 // database endpoints
-// register a new user
+// securely register a new user
 server.post('/api/register', async (req, res) => {
     const userCreds = req.body;
+    const hash = bcryptjs.hashSync(userCreds.password, 2); // should be 13 or higher in production
+    userCreds.password = hash;
     try {
-        const returned = await db('users').insert(userCreds);
-        // need to hash pw
-        res.status(200).json(returned);
+        const insertCount = await db('users').insert(userCreds);
+        res.status(200).json(insertCount);
     } catch(err) {
         res.status(500).json(err);
     }
 });
 
-
+// securely log a user in
+server.post('/api/login', async (req, res) => {
+    const loginCreds = req.body;
+    try {
+        // find the user by the username in the db
+        // compare the submitted pw to the pw in the db
+        const user = db('users').where({ username: loginCreds.username });
+        if (user && bcryptjs.compareSync(loginCreds.password, user.password)) {
+            // create a token
+            res.status(200).json({ message: '*extends the key to the castle*'});
+        } else {
+            res.status(401).json({ message: 'Username or password incorrect.'});
+        }
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
 
 // get the list of users
 server.get('/api/users', async (req, res) => {
