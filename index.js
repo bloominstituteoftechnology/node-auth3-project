b4e8701f558b138500324a2325b974b21456f0ab
samsisle <samko9522@gmail.com>
2018-11-28 15:19:41 -0800

    Configure root index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// WEB API ARCHITECTURE
+// ==============================================
+require('dotenv').config();
+
+const app = require('express')();
+
+require('./API/middleware')(app);
+require('./API/routes')(app);
+
+const port = process.env.PORT || 3300;
+
+// START THE WEB API
+// ==============================================
+app.listen(port, () => console.log(`\n=== Web API listening on http://localhost:${port} ===\n`));
