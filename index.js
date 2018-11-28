53805b36f648195e2e59ded9448db06191f891af
pedro <pedroluismontesinosrivera@gmail.com>
2018-11-28 17:20:05 -0500

    implemented web token

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+
+const jwt = require('jsonwebtoken');
 
 const db = require('./data/dbConfig');
 
@@ -8,6 +12,19 @@ const server = express();
 
 server.use(express.json());
 server.use(cors());
+
+const generateToken = user => {
+    const payload = {
+        subject: user.id,
+        username: user.username,
+        department: user.department,
+    };
+    const secret = process.env.JWT_SECRET;
+    const options = {
+        expiresIn: '1h'
+    };
+    return jwt.sign(payload, secret, options)
+}
 
 server.post('/api/register', (req, res) => {
     const creds = req.body;
@@ -29,7 +46,8 @@ server.post('/api/login', (req, res) => {
         .where({ username: creds.username }).first()
         .then(user => {
             if(user && bcrypt.compareSync(creds.password, user.password)){
-                res.status(200).json({message: 'WELCOME!'});
+                const token = generateToken(user);
+                res.status(200).json({message: 'WELCOME!', token});
             }else{
                 res.status(401).json({message: 'You are not allowed here'});
             }
