6b3c68ef2e011157437b7d73f2b5d6b0902ff70b
MEBonham <programmer@mckaybonham.com>
2018-11-28 15:07:53 -0700

    POST register endpoint implemented

--- index.js
+++ index.js
@@ -16,8 +16,33 @@ const port = 9000;
 const rounds = 14;
 
 server.post("/api/register", (req, res) => {
-    
+    const creds = req.body;
+    const hash = bcrypt.hashSync(creds.password, rounds);
+    creds.password = hash;
+
+    db("users")
+        .where({ username: creds.username })
+        .first()
+        .then(user => {
+            if (user) {
+                res.status(409).json({ message: "Username in use." });
+            } else {
+                db("users")
+                    .insert(creds)
+                    .then(ids => {
+                        res.status(201).json(ids);
+                    })
+                    .catch(err => {
+                        res.status(500).json({ error: err });
+                    });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: err });
+        });
 });
+
+
 
 server.get("/", (req, res) => {
     res.send("We are live!");
