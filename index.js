653cafe858a6f9c4ce232f9d028c6c0028ec79f8
David247365 <ds051128@gmail.com>
2018-11-28 15:58:27 -0600

    Set up basic endpoints for GET/Users and POST/Register

--- index.js
+++ index.js
@@ -9,6 +9,43 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+// POST - register
+
+server.post("/api/register", (req, res) => {
+  // Grab username and password from body
+  const creds = req.body;
+  //  Hash the password
+  const hash = bcrypt.hashSync(creds.password, 14);
+  // Override the password with the hash
+  creds.password = hash;
+  // Svae the user to the database
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json({ err: "There was an error, please try again" });
+    });
+});
+
+// POST - login
+
+// POST - users
+
+server.get("/api/users", (req, res) => {
+  db("users")
+    .select("username")
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ err: "There was an error, please try again" });
+    });
+});
+
+// Server
+
 server.get("/", (req, res) => {
   res.send("Server Up And Running");
 });
