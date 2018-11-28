46ea950cb39e0d963af62a7b07fce522b23582ca
Gannon <gkleblanc@gmail.com>
2018-11-28 17:47:47 -0500

    added middleware and token generator in index.js, added .env too

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bcrypt = require('bcryptjs')
 const morgan = require('morgan')
 const db = require('./database/dbConfig')
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 //call the dependencies
 const server = express();
@@ -18,6 +19,49 @@ server.use(helmet());
 server.get("/api", (req, res) => {
     res.send("Welcome To The Black Parade!");
 });
+
+//Token Generator for JWT 
+const generateToken = user => {
+    const payload = {
+        subject: user.id,
+        username: user.username,
+        department: user.department,
+    };
+    const secret = process.env.JWT_SECRET;
+    const options = {
+        expiresIn: '1m',
+    };
+    return jwt.sign(payload, secret, options);
+};
+
+//middleware
+const protected = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (token) {
+        jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+            if (err) {
+                res.status(401).json({ message: 'invalid token' });
+            } else {
+                req.decodedToken = decodedToken;
+                next();
+            }
+        })
+    } else {
+        res.status(401).json({ message: 'Where is your token?' })
+    }
+}
+const checkRole = role => {
+    return function (req, res, next) {
+        if (req.decodedToken && req.decodedToken.roles.includes(role)) {
+            next();
+        } else {
+            res.status(403).json({ message: 'You Don\'t Have Any Power Here!' })
+        }
+    }
+}
+
+//endpoints
+
 
 
 //port
