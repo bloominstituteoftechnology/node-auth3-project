9f8c871bee755e6072eb49d2c684d861f6a0b43d
Katie Cleary <katiecleary@airmaster.lan>
2018-11-28 16:40:47 -0500

    ooops, think I didn't save before pushing?

--- index.js
+++ index.js
@@ -24,3 +24,25 @@ const options = {
   };
   return jwt.sign(payload, secret, options);
 }
+
+function protected(req, res, next) {
+    // token is normally sent in the the Authorization header
+    const token = req.headers.authorization;
+  
+    if (token) {
+      // is it valid
+      jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+        if (err) {
+          // token is invalid
+          res.status(401).json({ message: 'invalid token' });
+        } else {
+          // token is gooooooooooood
+          req.decodedToken = decodedToken;
+          next();
+        }
+      });
+    } else {
+      // bounced
+      res.status(401).json({ message: 'not token provided' });
+    }
+  }
