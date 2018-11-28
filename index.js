927000e03287d551d3147113a45fbf5e977edea4
Drewsup123c <drewsup123c@gmail.com>
2018-11-28 13:56:42 -0700

    modified knexfile to save everything in database folder

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ const session = require('express-session');
 
 const server = express();
 
+/*
+POST to /api/login
+
+GET to /api/users
+    -returns an array of all users if authenticated
+
+POST to /api/register
+    -Creates a user in the database (hash password (bcrypt))
+*/
+
 server.listen(9000, console.log("Server Running on Port 9000"))
