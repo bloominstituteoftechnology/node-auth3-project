78e9904b4f1d0a8da75e21dd51f1cacc030c89db
bummings <eabummings@gmail.com>
2018-11-28 16:50:36 -0500

    configured token generation, initialized login route

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const db = require('./database/dbConfig.js');
+const server = express();
 
 server.use(express.json());
 server.use(cors());
 
-const secret = process.env.JWT_SECRET;
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+
+  const secret = process.env.JWT_SECRET;
+  const options = {
+    expiresIn: '1m'
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+
+server.get('/', (req, res) => {
+  res.send('yr server is fine, relax');
+});
+
+server.post('/login', (req, res) => {
+  const creds = req.body;
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({ message: 'Welcome!', token });
+      } else {
+        res.status(200).json({ message: 'Invalid login!' });
+      }
+    })
+    .catch(err => res.json(err));
+});
+
+server.listen(8000, () => console.log('\nS E R V I N G   O N   8 0 0 0'));
--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,8 @@
 module.exports = {
   development: {
     client: 'sqlite3',
-    connection: { filename: './database/auth-ii.sqlite3' }, // change this if you want a different name for the database
-    useNullAsDefault: true, // used to avoid warning on console
+    connection: { filename: './database/auth-ii.sqlite3' },
+    useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
       tableName: 'dbmigrations'
