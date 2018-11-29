193946040b957288433b67234b71370805a0ee83
Mindysueasaurus <mymindyyeager@gmail.com>
2018-11-28 18:22:18 -0600

    login end point done

--- index.js
+++ index.js
@@ -17,6 +17,24 @@ server.use(cors());
 
 
 
-// | POST   | /api/register | Creates a `user` using the information sent inside the `body` of the request. **Hash the password** before saving the user to the database.                                                                                                                                                         |
-// | POST   | /api/login    | Use the credentials sent inside the `body` to authenticate the user. On successful login, create a new JWT with the user id as the subject and send it back to the client. If login fails, respond with the correct status code and the message: 'You shall not pass!' |
+// | POST   | /api/register | Creates a `user` using the information sent inside the `body` of the request. **Hash the password** before saving the user to the database.    
+                                                                     
 // | GET    | /api/users    | If the user is logged in, respond with an array of all the users contained in the database. If the user is not logged in repond with the correct status code and the message: 'You shall not pass!'. Use this endpoint to verify that the password is hashed before it is saved.       
+
+
+server.post('/api/login', (req,res) => {
+  const creds = req.body;
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if(user && bcrypt.compareSync(creds.password, user.password)){
+        const token = generateToken(user)
+        res.status(200).json({ message: 'Welcome!', token})
+      } else  {
+        res.status(401.json({ message: 'you shall not pass' }))
+      }
+    })
+    .catch( error => res.json(error))
+})
