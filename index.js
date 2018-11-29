87c74069c944087d75222c0cf81c24a1d1d2857f
Mindysueasaurus <mymindyyeager@gmail.com>
2018-11-28 18:25:47 -0600

    register end point with password hashing done

--- index.js
+++ index.js
@@ -15,10 +15,8 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+  
 
-
-// | POST   | /api/register | Creates a `user` using the information sent inside the `body` of the request. **Hash the password** before saving the user to the database.    
-                                                                     
 // | GET    | /api/users    | If the user is logged in, respond with an array of all the users contained in the database. If the user is not logged in repond with the correct status code and the message: 'You shall not pass!'. Use this endpoint to verify that the password is hashed before it is saved.       
 
 
@@ -37,4 +35,19 @@ server.post('/api/login', (req,res) => {
       }
     })
     .catch( error => res.json(error))
+})
+
+server.post('/api/register', (req, res) => {
+  const creds = req.body;
+
+  const hash = bcrypt.hashSync(creds.password, 4);
+
+  creds.password = hash;
+
+  db('users')
+    .insert(creds)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(error => json(error))
 })
