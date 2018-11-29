69c7f15ea2fc3b4b4f1164024484ec538d397f3e
Katie Cleary <katiecleary@airmaster.lan>
2018-11-29 15:58:46 -0500

    okay, really this time updated the app.js return

--- client/src/App.js
+++ client/src/App.js
@@ -11,5 +11,39 @@ class App extends Component {
     users: [],
   }
 }
+}
 
-export default App;
+return (
+  //<SWITCH> Only the first child to match the current location will be rendered.
+  //<REACT.FRAGMENT> Fragments let you group a list of children without adding extra nodes to the DOM.
+  //... using fragments results in a correct <Table /> output 
+  
+  <div className="App">
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/login">Login</NavLink>
+      <NavLink to="/register">Register</NavLink>
+    </nav>
+    <section> 
+      <Switch>
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route path="/" render={() => {
+          return (
+            <React.Fragment>
+            <h2>Users</h2>
+              <ol>
+                {this.state.users.map(user => <li key={user.id}>{user.username}</li>)}
+              </ol>
+            </React.Fragment>
+          );
+        }} />
+      </Switch>
+      
+    </section>
+  </div>
+);
+}
+}
+
+export default withRouter(App);
