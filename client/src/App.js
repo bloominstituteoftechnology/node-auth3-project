c10d2e3dba00aff92f17d450f71ebc4fc683a5a2
AM_Myrick <amyrick.820@gmail.com>
2018-11-29 15:14:44 -0500

    creates react client

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import axios from "axios";
+import {Route, NavLink} from "react-router-dom"
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
