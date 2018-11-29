757b12828a60522b187d6d761a625ba7f5816295
sarahtennis <sarahtennis@Sarahs-Air.fios-router.home>
2018-11-29 14:01:23 -0500

    user registration

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import axios from 'axios';
+
+const url = process.env.REACT_APP_API_URL;
 
 const initialUser = {
     username: '',
-    password: ''
+    password: '',
+    department: ''
 }
 
 class Register extends React.Component {
@@ -14,8 +18,38 @@ class Register extends React.Component {
         }
     }
 
-    submitHandler = () => {
-        
+    inputHandler = (event) => {
+        const { name, value } = event.target;
+
+        this.setState({
+            user: { ...this.state.user, [name]: value }
+        });
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        axios.post(`${url}/api/register`, this.state.user)
+            .then(res => {
+                if (res.status === 201) {
+                    this.setState({
+                        message: 'Registration successful',
+                        user: { ...initialUser }
+                    });
+                } else {
+                    throw new Error();
+                }
+            })
+            .catch(err => {
+                if (err.errno === 19) {
+                    this.setState({
+                        message: 'Username already exists'
+                    });
+                } else {
+                    this.setState({
+                        message: 'Registration failure'
+                    });
+                }
+            });
     }
 
     render() {
@@ -29,6 +63,7 @@ class Register extends React.Component {
                         name="username"
                         value={this.state.user.username}
                         placeholder="Username"
+                        onChange={this.inputHandler}
                     />
                     <label htmlFor="password">Password:</label>
                     <input
@@ -37,6 +72,16 @@ class Register extends React.Component {
                         name="password"
                         value={this.state.user.password}
                         placeholder="Password"
+                        onChange={this.inputHandler}
+                    />
+                    <label htmlFor="department">Department:</label>
+                    <input
+                        type="text"
+                        id="department"
+                        name="department"
+                        value={this.state.user.department}
+                        placeholder="Department"
+                        onChange={this.inputHandler}
                     />
                     {this.state.message ? <h4>{this.state.message}</h4> : null}
                     <button type="submit">Submit</button>
