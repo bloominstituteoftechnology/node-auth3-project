34332028525c905f8701c02961ce662cd3b6fc87
bummings <eabummings@gmail.com>
2018-11-29 14:55:53 -0500

    event handlers working, tokens being rendered

--- client/src/App.js
+++ client/src/App.js
@@ -6,41 +6,77 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      users: []
+      username: 'bummings',
+      password: 'password'
     };
   }
 
   componentDidMount() {
-    this.getData();
+    // this.getData();
+    console.log(this.state);
   }
 
-  getData = () => {
-    axios
-      .get('http://localhost:8000/api/users')
-      .then(response => {
-        console.log(response.data);
-        this.setState({ users: response.data });
-      })
-      .catch(err => console.log(err));
-  };
+  // getData = () => {
+  //   axios
+  //     .get('http://localhost:8000/api/users')
+  //     .then(response => {
+  //       console.log(response.data);
+  //       this.setState({ users: response.data });
+  //     })
+  //     .catch(err => console.log(err));
+  // };
 
   render() {
     return (
       <div className='App'>
         <h1>test</h1>
-
-        {this.state.users.map(user => {
-          return (
-            <div className='users'>
-              <h3>{user.username}</h3>
-              <h3>{user.password}</h3>
-              <h3>{user.roles}</h3>
+        {/*                 */}
+        <form onSubmit={this.handleSubmit}>
+          <div className='form'>
+            <div className='form__input'>
+              <label htmlFor='username'>Username</label>
+              <input
+                name='username'
+                value={this.state.username}
+                onChange={this.handleInputChange}
+                type='text'
+              />
             </div>
-          );
-        })}
+            <div className='form__input'>
+              <label htmlFor='password'>Password</label>
+              <input
+                name='password'
+                value={this.state.password}
+                onChange={this.handleInputChange}
+                type='password'
+              />
+            </div>
+            <div className='form__btn'>
+              <button type='submit'>Sign In</button>
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const endpoint = 'http://localhost:8000/api/login';
+    console.log(this.state);
+    axios
+      .post(endpoint, this.state)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log('big time error bruh', err);
+      });
+  };
 }
 
 export default App;
