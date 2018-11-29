9f17f2de9c24b0fca620a652a81079fcae3b1694
Elvis Ibarra <ielvisd@gmail.com>
2018-11-29 15:47:31 -0800

    Registration works

---
+++ auth_app/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Register from './components/Register';
+import { Switch, Route } from 'react-router-dom';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <section>
+          <Switch>
+            <Route path="/register" component={Register} />
+          </Switch>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
