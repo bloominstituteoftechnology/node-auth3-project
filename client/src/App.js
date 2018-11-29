67a6071266b72dc5638ecdb686d5cdf098dba506
MEBonham <programmer@mckaybonham.com>
2018-11-29 11:34:12 -0700

    Added login; messages still not showing

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Register from "./components/Register";
+import Login from "./components/Login";
 
 import './App.css';
 
@@ -15,6 +16,7 @@ class App extends Component {
         <section>
           <Switch>
             <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
           </Switch>
         </section>
       </div>
---
+++ client/src/components/Login.js
@@ -0,0 +1,74 @@
+import React from "react";
+import axios from "axios";
+
+const url = process.env.REACT_APP_API_URL;
+
+const initialUser = {
+    username: "",
+    password: ""
+};
+
+export default class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: { ...initialUser },
+            message: ""
+        }
+    }
+
+    inputHandler = ev => {
+        const { name, value } = ev.target;
+        this.setState({ user: { ...this.state.user, [name]: value } });
+    }
+
+    submitHandler = ev => {
+        ev.preventDefault();
+        axios.post(`${url}/api/login`, this.state.user)
+            .then(res => {
+                if (res.status === 200 && res.data) {
+                    localStorage.setItem("secretBitcoinToken", res.data);
+                    this.setState({
+                        message: "Login successful",
+                        user: { ...initialUser }
+                    });
+                } else {
+                    throw new Error();
+                }
+            })
+            .catch(err => {
+                this.setState({
+                    message: "Authentication failed",
+                    user: { ...initialUser }
+                });
+            });
+    }
+
+    render() {
+        return (
+            <section>
+                <form onSubmit={this.submitHandler}>
+                    <label htmlFor="username">Username</label>
+                    <input 
+                        type="text" 
+                        id="username" 
+                        name="username" 
+                        value={this.state.user.username}
+                        onChange={this.inputHandler}
+                    />
+                    <label htmlFor="password">Password</label>
+                    <input 
+                        type="text" 
+                        id="password" 
+                        name="password" 
+                        value={this.state.user.password}
+                        onChange={this.inputHandler} 
+                    />
+                </form>
+                { this.state.message ? 
+                    (<h4>{this.state.message}</h4>) : 
+                    undefined }
+            </section>
+        );
+    }
+}
