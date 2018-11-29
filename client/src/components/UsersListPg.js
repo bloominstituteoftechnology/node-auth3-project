f0c5bcbeea321891f85f7af61c3789022b1cb0ca
Brandon Strellis <bstrellis@gmail.com>
2018-11-29 18:38:44 -0500

    users display if token is present

--- client/src/components/UsersListPg.js
+++ client/src/components/UsersListPg.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class UsersListPg extends Component {
     constructor(props) {
@@ -8,15 +9,36 @@ export default class UsersListPg extends Component {
         }
     }
 
-    componentDidMount() {
+    authenticate = () => {
         
+    }
+    
+    componentDidMount() {
+        const token = localStorage.getItem('token');
+        const options = {
+            headers: {
+                authorization: token
+            }
+        }
+        if (token) {
+            axios.get('http://localhost:5000/api/users', options)
+                .then(response => {
+                    console.log(response);
+                    this.setState({ users: response.data });
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        } else {
+
+        }
     }
 
     render() {
         return (
             <div className='usersListContainer'>
                 <ul>
-                    {this.state.usersList.map(user => {
+                    {this.state.users.map(user => {
                      return <li key={user.id}>{user.username}</li>
                     })}
                 </ul>
