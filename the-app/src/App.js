81f656e621026fcb454fa44205e5d36b40a96582
unknown <quan.nguyen199712@gmail.com>
2018-11-29 18:21:40 -0500

    register route set up

--- the-app/src/App.js
+++ the-app/src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
+import Register from './Components/Register';
+import Login from './Components/Login';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <section>
+            <Switch>
+              <Route path= "/register" component={Register}/>
+              <Route path="/login" component={Login}/>
+            </Switch>
+        </section>
       </div>
     );
   }
---
+++ the-app/src/Components/Login.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class Login extends Component{
+    constructor(props){
+        super(props);
+        
+    }
+}
