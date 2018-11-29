f85456a7a58ef19c259ef90845f7a8e8f9b6a1c0
DaftBeowulf <ajmboosh@gmail.com>
2018-11-29 16:33:36 -0600

    reinitialized stretch line of code so users will only be able to see users in their own department based on department listed at registration

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ server.get("/api/users", restricted, (req, res) => {
   db("users")
     //following line completes stretch goal but can be taken out for testing MVP
     //will only return a list of users with the same department as the logged-in user's department(read on decoded token)
-    // .where({ department: req.decodedToken.department })
+    .where({ department: req.decodedToken.department })
     .select("id", "username") //never pull password in production, but this is just to verify it was saved as hash
     .then(users => {
       res.status(200).json(users);
