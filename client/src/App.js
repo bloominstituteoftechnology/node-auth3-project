83b6b4ab151c772503e1e4e601355f053bc9a56b
jsmnglng <jsmn.glng@gmail.com>
2018-11-29 13:04:14 -0800

    Show list of users done

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,69 @@
 import React, { Component } from "react";
-import { NavLink, Route, Switch } from "react-router-dom";
+import { NavLink, Route, Switch, withRouter } from "react-router-dom";
+import axios from "axios";
 
 import Login from "./components/Login";
 
 import "./App.css";
 
-const Home = _ => {
-  return <h1>Home</h1>;
+const Home = props => {
+  if (props.loggedIn) {
+    return (
+      <ul>
+        {props.users.map(user => (
+          <li key={user.id}>{user.username}</li>
+        ))}
+      </ul>
+    );
+  } else {
+    return <h1>Home</h1>;
+  }
 };
 
 class App extends Component {
+  state = {
+    loggedIn: false,
+    users: []
+  };
+
+  authenticate = () => {
+    const url = "http://localhost:8080/api/users";
+    const token = localStorage.getItem("jwtToken");
+    const options = {
+      headers: {
+        Authorization: token
+      }
+    };
+
+    if (token) {
+      axios
+        .get(url, options)
+        .then(res => {
+          if (res.status === 200 && res.data) {
+            this.setState({ loggedIn: true, users: res.data });
+          } else {
+            throw new Error();
+          }
+        })
+        .catch(err => this.props.history.push("/login"));
+    } else {
+      this.props.history.push("/login");
+    }
+  };
+
+  componentDidMount() {
+    this.authenticate();
+  }
+
+  componentDidUpdate(prevProps) {
+    const { pathname } = this.props.location;
+    console.log(this.props);
+    console.log(prevProps);
+    if (pathname === "/" && pathname !== prevProps.location.pathname) {
+      this.authenticate();
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -33,7 +87,17 @@ class App extends Component {
         </header>
         <section>
           <Switch>
-            <Route exact path="/" component={Home} />
+            <Route
+              exact
+              path="/"
+              render={ownProps => (
+                <Home
+                  {...ownProps}
+                  loggedIn={this.state.loggedIn}
+                  users={this.state.users}
+                />
+              )}
+            />
             <Route path="/login" component={Login} />
           </Switch>
         </section>
@@ -42,5 +106,5 @@ class App extends Component {
   }
 }
 
-// export default withRouter(App);
-export default App;
+export default withRouter(App);
+// export default App;
