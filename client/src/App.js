7c08a83bf832a74897935014a0d9d2bd90cbe0d7
Carlos Lantigua <clantigua954@gmail.com>
2018-11-29 16:34:25 -0500

    adding front end to project

---
+++ client/src/App.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Signup from './components/Signup';
+import Signin from './components/Signin';
+import { withRouter, Switch, Route, NavLink } from 'react-router-dom';
+import './App.css';
+
+const url = process.env.REACT_APP_API_URL;
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			loggedIn: false,
+			users: []
+		};
+	}
+
+	authenticate = () => {
+		const token = localStorage.getItem('secret_bitcoin_token');
+		const options = {
+			headers: {
+				authentication: token
+			}
+		};
+
+		if (token) {
+			axios
+				.get(`${url}/api/restricted/users`, options)
+				.then((res) => {
+					if (res.status === 200 && res.data) {
+						this.setState({ loggedIn: true, users: res.data });
+					} else {
+						throw new Error();
+					}
+				})
+				.catch((err) => {
+					this.props.history.push('/login');
+				});
+		} else {
+			this.props.history.push('/login');
+		}
+	};
+
+	componentDidMount() {
+		this.authenticate();
+	}
+
+	componentDidUpdate(prevProps) {
+		const { pathname } = this.props.location;
+		console.log(this.props);
+		console.log(prevProps);
+		if (pathname === '/' && pathname !== prevProps.location.pathname) {
+			this.authenticate();
+		}
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<nav>
+					<NavLink to="/">Home</NavLink>
+					<NavLink to="/login">Login</NavLink>
+					<NavLink to="/register">Register</NavLink>
+				</nav>
+				<section>
+					<Switch>
+						<Route path="/register" component={Signup} />
+						<Route path="/login" component={Signin} />
+						<Route
+							path="/"
+							render={() => {
+								return (
+									<React.Fragment>
+										<h2>Users</h2>
+										<ol>
+											{this.state.users.map((user) => <li key={user.id}>{user.username}</li>)}
+										</ol>
+									</React.Fragment>
+								);
+							}}
+						/>
+					</Switch>
+				</section>
+			</div>
+		);
+	}
+}
+
+export default withRouter(App);
---
+++ client/src/components/Signin.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const url = process.env.REACT_APP_API_URL;
+
+const initialUser = {
+	username: '',
+	password: '',
+	department: ''
+};
+
+export default class Signin extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			user: { ...initialUser },
+			message: ''
+		};
+	}
+
+	changeHandler = (e) => {
+		const { name, value } = e.target;
+		this.setState({ user: { ...this.state.user, [name]: value } });
+	};
+
+	submitHandler = (e) => {
+		e.preventDefault();
+		axios
+			.post(`${url}/api/login`, this.state.user)
+			.then((res) => {
+				if (res.status === 201 && res.data) {
+					localStorage.setItem('this_is_weird_secret_thingy', res.data);
+					this.props.history.push('/');
+				} else {
+					throw new Error();
+				}
+			})
+			.catch((err) => {
+				this.setState({
+					message: 'Registration failed.',
+					user: { ...initialUser }
+				});
+			});
+	};
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.submitHandler}>
+					<label htmlFor="username">Username</label>
+					<input
+						type="text"
+						id="username"
+						name="username"
+						value={this.state.user.username}
+						onChange={this.changeHandler}
+					/>
+					<label htmlFor="department">Department</label>
+					<input
+						type="text"
+						id="department"
+						name="department"
+						value={this.state.user.department}
+						onChange={this.changeHandler}
+					/>
+					<label htmlFor="password">Password</label>
+					<input
+						type="text"
+						id="password"
+						name="password"
+						value={this.state.user.password}
+						onChange={this.changeHandler}
+					/>
+					<button type="submit">Submit</button>
+				</form>
+				{this.state.message ? <h4>{this.state.message}</h4> : undefined}
+			</div>
+		);
+	}
+}
