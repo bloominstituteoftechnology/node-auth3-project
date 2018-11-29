4513633147e86401c6e502cc081e3e67b49c88c7
Unknown <zachary.arney@gmail.com>
2018-11-29 16:30:26 -0600

    Added Register component

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const url = process.env.REACT_APP_API_URL;
+
+const initialUser = {
+  username: '',
+  password: '',
+};
+
+export default class Register extends Component {
+  state = {
+    user: { ...initialUser },
+    message: '',
+  };
+
+  inputHandler = (event) => {
+    const { name, value } = event.target;
+    this.setState({ user: { ...this.state.user, [name]: value } });
+  };
+
+  submitHandler = (event) => {
+    event.preventDefault();
+    axios
+      .post(`${url}/api/register`, this.state.user)
+      .then((res) => {
+        if (res.status === 201) {
+          this.setState({
+            message: 'Registration successful',
+            user: { ...initialUser },
+          });
+        } else {
+          // Error() just passes to the .catch()
+          throw new Error();
+        }
+      })
+      .catch((err) => {
+        this.setState({
+          message: 'Registration failed',
+          user: { ...initialUser },
+        });
+      });
+  };
+
+  render() {
+    return (
+      <section>
+        <form onSubmit={this.submitHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={this.state.user.username}
+            onChange={this.inputHandler}
+          />
+          <label htmlFor="username">Password</label>
+          <input
+            type="text"
+            id="password"
+            name="password"
+            value={this.state.user.password}
+            onChange={this.inputHandler}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        {this.state.message ? <h3>{this.state.message}</h3> : undefined}
+      </section>
+    );
+  }
+}
