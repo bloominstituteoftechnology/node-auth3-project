d67f08865d4ab4ffabd0b4bafd0988ab4b382fc6
jsmnglng <jsmn.glng@gmail.com>
2018-11-29 12:37:13 -0800

    Login component done

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,46 @@
 import React, { Component } from "react";
+import { NavLink, Route, Switch } from "react-router-dom";
+
+import Login from "./components/Login";
+
 import "./App.css";
+
+const Home = _ => {
+  return <h1>Home</h1>;
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Hi</h1>
+          <nav>
+            <NavLink exact to="/" activeStyle={{ textDecoration: "underline" }}>
+              Home
+            </NavLink>
+            &nbsp;|&nbsp;
+            <NavLink
+              to="/register"
+              activeStyle={{ textDecoration: "underline" }}
+            >
+              Register
+            </NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/login" activeStyle={{ textDecoration: "underline" }}>
+              Login
+            </NavLink>
+          </nav>
         </header>
+        <section>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </section>
       </div>
     );
   }
 }
 
+// export default withRouter(App);
 export default App;
---
+++ client/src/components/Login.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class Login extends Component {
+  state = {
+    username: "",
+    password: ""
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const url = "http://localhost:8080/api/login";
+    console.log("state", this.state);
+    axios
+      .post(url, this.state)
+      .then(res => {
+        console.log("res data", res.data);
+        localStorage.setItem("jwtToken", res.data.token);
+      })
+      .catch(err => {
+        console.log("ERROR", err);
+      });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            name="username"
+            value={this.state.username}
+            onChange={this.handleInputChange}
+            type="text"
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            name="password"
+            value={this.state.password}
+            onChange={this.handleInputChange}
+            type="password"
+          />
+          <button type="submit">Log in</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default Login;
