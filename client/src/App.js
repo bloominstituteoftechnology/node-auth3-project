55a856cf1dc53b5b79e59f291739a91551f585eb
ssmodish <smodish@hotmail.com>
2018-11-29 16:01:22 -0500

    front end in progress

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import axios from 'axios';
+// import Register from ''
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Steve's Front End</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
