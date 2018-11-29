742c479fb2e3c7a5ccde4a2e1500a70444fdab98
jsmnglng <jsmn.glng@gmail.com>
2018-11-29 11:39:29 -0800

    CRA setup

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const db = require('./database/dbConfig');
+const db = require("./database/dbConfig");
 
 const server = express();
 const port = 8080;
@@ -19,10 +19,10 @@ const generateToken = user => {
 
   const secret = process.env.JWT_SECRET;
   const options = {
-    expiresIn: '1h'
+    expiresIn: "1h"
   };
 
-  return jwt.sign(payload, secret, options)
+  return jwt.sign(payload, secret, options);
 };
 
 // middleware
@@ -35,57 +35,65 @@ const protected = (req, res, next) => {
   if (token) {
     jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
       if (err) {
-        res.status(401).json({ message: 'Invalid token' });
+        res.status(401).json({ message: "Invalid token" });
       } else {
         req.decodedToken = decodedToken;
         next();
       }
     });
   } else {
-    res.status(401).json({ message: 'No token provided' });
+    res.status(401).json({ message: "No token provided" });
   }
 };
 
 const checkRole = department => {
   return (req, res, next) => {
-    if (req.decodedToken && req.decodedToken.department === department || req.decodedToken.department === 'admin') {
+    if (
+      (req.decodedToken && req.decodedToken.department === department) ||
+      req.decodedToken.department === "admin"
+    ) {
       next();
     } else {
-      res.status(403).json({ message: 'You have no access to this resource.' });
+      res.status(403).json({ message: "You have no access to this resource." });
     }
   };
-}
+};
 
 server.listen(port, () => console.log(`Listening to port: ${port}`));
 
 // ====================== ENDPOINTS ======================
 // retrieve users
-server.get('/api/users', protected, checkRole('ministry of magic'), (req, res) => {
-  const { department } = req.decodedToken;
+server.get(
+  "/api/users",
+  protected,
+  checkRole("ministry of magic"),
+  (req, res) => {
+    const { department } = req.decodedToken;
 
-  if (department === 'admin') {
-    db('users')
-      .then(users => {
-        res.status(200).json(users);
-      })
-      .catch(err => res.status(500).json(err));
-  } else {
-    db('users')
-      .where({ department })
-      .then(users => {
-        res.status(200).json(users);
-      })
-      .catch(err => res.status(500).json(err));
+    if (department === "admin") {
+      db("users")
+        .then(users => {
+          res.status(200).json(users);
+        })
+        .catch(err => res.status(500).json(err));
+    } else {
+      db("users")
+        .where({ department })
+        .then(users => {
+          res.status(200).json(users);
+        })
+        .catch(err => res.status(500).json(err));
+    }
   }
-});
+);
 
 // register a new user
-server.post('/api/register', (req, res) => {
+server.post("/api/register", (req, res) => {
   const creds = req.body;
   const hash = bcrypt.hashSync(creds.password, 8);
   creds.password = hash;
 
-  db('users')
+  db("users")
     .insert(creds)
     .then(id => {
       res.status(201).json({ message: `ID ${id} created` });
@@ -94,24 +102,24 @@ server.post('/api/register', (req, res) => {
 });
 
 // login a user
-server.post('/api/login', (req, res) => {
+server.post("/api/login", (req, res) => {
   const creds = req.body;
 
-  db('users')
+  db("users")
     .where({ username: creds.username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(creds.password, user.password)) {
         const token = generateToken(user);
-        res.status(200).json({ message: 'Logged in', token });
+        res.status(200).json({ message: "Logged in", token });
       } else {
-        res.status(401).json({ message: 'You shall not pass!' });
+        res.status(401).json({ message: "You shall not pass!" });
       }
     })
     .catch(err => res.status(500).json(err));
-})
+});
 
 // test API
-server.get('/', (_, res) => {
-  res.send('API is live!');
+server.get("/", (_, res) => {
+  res.send("API is live!");
 });
