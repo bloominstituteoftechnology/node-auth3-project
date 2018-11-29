4cb71d51c251dac9c437bad954ed5af17bdc910c
Matthew Wright <matthewbwright@gmail.com>
2018-11-29 13:57:42 -0700

    sets up App with Routes and render

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,91 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import Signup from '../components/Signup';
+import Signin from '../components/Signin';
+import { withRouter, Switch, Route, NavLink } from 'react-router-dom';
 import './App.css';
 
+const url = process.env.REACT_APP_API_URL;
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false,
+      users: []
+    };
+  }
+
+  authenticate = () => {
+    const token = localStorage.getItem('secret_bitcoin_token');
+    const options = {
+      headers: {
+        authentication: token
+      }
+    };
+    if (token) {
+      axios
+        .get(`${url}/api/restricted/users`, options)
+        .then(res => {
+          if (res.status === 201 && res.data) {
+            this.setState({ loggedIn: true, users: res.data });
+          } else {
+            throw new Error();
+          }
+        })
+        .catch(err => {
+          this.props.history.push('login');
+        });
+    } else {
+      this.props.history.push('login');
+    }
+  };
+
+  componentDidMount() {
+    this.authenticate();
+  }
+
+  componentDidUpdate(prevProps) {
+    const { pathname } = this.props.location;
+    console.log(this.props);
+    console.log(prevProps);
+    if (pathname === '/' && pathname !== prevProps.location.pathname) {
+      this.authenticate();
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <nav>
+          <NavLink to="/">HOME</NavLink>
+          <NavLink to="/signin">SIGNIN</NavLink>
+          <NavLink to="/signup">SIGNUP</NavLink>
+        </nav>
+        <section>
+          <Switch>
+            <Route path="signup" component={Signup} />
+            <Route path="signin" component={Signin} />
+            <Route
+              path="/"
+              render={() => {
+                return (
+                  <React.Fragment>
+                    <h2>USERS</h2>
+                    <ol>
+                      {this.state.users.map(user => (
+                        <li key={user.id}>{user.username}</li>
+                      ))}
+                    </ol>
+                  </React.Fragment>
+                );
+              }}
+            />
+          </Switch>
+        </section>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
