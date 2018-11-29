2a13af918a91113eb446cf163397e5e8680c5d5e
Yanrong Xu <xuyanrong@icloud.com>
2018-11-29 16:28:43 -0500

    create a NavLink for client

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        </header>
+        <nav>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/signup'>SignUp</NavLink>
+          <NavLink to='/signin'>Signin</NavLink>
+          <NavLink to='/users'>Users</NavLink>
+        </nav>
       </div>
     );
   }
