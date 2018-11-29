167447ed5a90838fc810383ce3e92db99341b3ed
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-11-29 13:10:54 -0800

    installed CRA

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      loggedIn: false,
+      users: [],
+    };
+  }
+  
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
