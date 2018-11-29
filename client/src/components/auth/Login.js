6989942d44fb71193b8def1099f38355d53587af
Unknown <zachary.arney@gmail.com>
2018-11-29 16:30:39 -0600

    Added Login component

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class Login extends Component {
+  state = {
+    username: '',
+    password: '',
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Login
