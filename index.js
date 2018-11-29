dd7ee28c21b2599321725a07091278ef89d80538
adamhinckley <adamhinckley@mac.com>
2018-11-29 09:36:30 -0700

    this one is just for fun

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const UserRouter = require("./Routers/UserRouter");
 const LoginRouter = require("./Routers/LoginRouter");
 const RegisterRouter = require("./Routers/RegisterRouter");
 
-server.use(express.json()); //make sure this is first or stuff breaks
-server.use("/api/users", UserRouter);
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+server.use("/api/users", UserRouter); //make sure the routes are always last in the list
 server.use("/api/login", LoginRouter);
 server.use("/api/register", RegisterRouter);
-server.use(cors());
-server.use(helmet());
 
 server.listen(4200, () => console.log("running on port 4200"));
