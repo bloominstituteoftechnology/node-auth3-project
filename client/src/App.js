c72bef4bda6a6e7bd5136b97206bd8a1ba931fab
Jordan Stoddard <jjsincorporated@gmail.com>
2018-11-29 16:05:12 -0800

    fixed registration method bugs

--- client/src/App.js
+++ client/src/App.js
@@ -75,13 +75,16 @@ class App extends Component {
       .post("http://localhost:9000/api/register", this.state.credentials)
       .then(res => {
         this.setState({
-          username: '',
-          password: '',
-          department: ''
+          credentials: {
+            username: "",
+            password: "",
+            department: ""
+          }
         })
         if(res.status === 201) {
           alert('New user has been successfully registered.')
         }
+        this.props.history.push('/')
       })
       .catch(err => console.log(`There was an error: ${err}`));
   };
