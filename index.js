2cba08eabf3b117db5a9026001738de3cb1b7333
Angelo Deleon <rudy209@comcast.net>
2018-12-05 13:26:09 -0800

    added Database

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+
+const server = express();
+
+// POST register
+
+// POST login
+
+// GET users
+
+server.listen(3300, () => console.log('Server stared on port 3300'));
