cf921ae97a3824ba342fefc63a23297c2250faae
Troy Amlee <troyamlee19@gmail.com>
2018-12-05 17:28:51 -0600

    [mvp] added login and user routes to react-app

--- authclient/src/App.js
+++ authclient/src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { NavLink, Route } from 'react-router-dom';
 import './App.css';
+import Users from './users/Users';
+
+const Home = props => {
+  return (
+    <div>
+      <h1>Home Component</h1>
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
@@ -8,17 +18,15 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <nav className = 'nav-links'>
+              <NavLink to= "/" exact>Home</NavLink>
+              &nbsp; | &nbsp;
+              <NavLink to= "/users" >Users</NavLink>
+            </nav>
+            <main>
+              <Route path = "/" component={ Home } exact/>
+              <Route path = "/users" component={ Users }/>
+            </main>
         </header>
       </div>
     );
