93d49ff207b35048228f713b7bcd93062d9c90a1
jaklundun <govi.escudero@gmail.com>
2018-12-06 07:47:50 -0800

    Set up JWTs token gernerator.

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const helmet = require('helmet');
 const knexConfig = require('./knexfile');
 const knex = require('knex');
 const bcrypt = require('bcryptjs');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config(); 
 
 const server = express();
 
@@ -11,6 +15,21 @@ const db = knex(knexConfig.development);
 
 //Connect Middleware to Server 
 server.use(helmet(), express.json());
+
+erver.use(cors());
+
+function generateToken(user) {
+    const payload = {
+      subject: user.id,
+      username: user.username,
+      department: user.department,
+    };
+    const secret = process.env.JWT_SECRET;
+    const options = {
+        expiresIn: '1m',
+    };
+        return jwt.sign(payload, secret, options);
+}
 
 // SANITY CHECK
 server.get('/', (request, response) => {
