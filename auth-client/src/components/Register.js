9bd3850a62d63b3ab958e0d6eba5de2d89f457c6
Wanda <doubleuevans@gmail.com>
2018-12-06 12:19:49 -0600

    commit at first break; not functional

--- auth-client/src/components/Register.js
+++ auth-client/src/components/Register.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const url = process.env.REACT_APP_API_URL;
 
 const initialUser = {
   username: '',
@@ -14,7 +17,32 @@ export default class Register extends Component {
     }
   }
 
-  submitHandler = () => {}
+  inputHandler =  (event) => {
+    const { name, value } = event.target;
+    this.setState({user: {...this.state.user, [name]: value}});
+  }
+
+  submitHandler = (event) => {
+    event.preventDefault();
+    axios.post(`${url}/api/register`, this.state.user)
+      .then(res => {
+        if (res.status === 200) {
+          this.setState({
+            message: 'Registration Successful!',
+            user: {...initialUser},
+          })
+        } else {
+          throw new Error();
+        }
+      })
+      .catch(err => {
+        this.setState({
+          message: 'Registration Failed!',
+          user: {...initialUser},
+        })
+      });
+
+  }
 
   render() {
     return (
@@ -25,14 +53,18 @@ export default class Register extends Component {
         type='text' 
         id='username' 
         name='username' 
-        value={this.state.user.username}></input>
+        value={this.state.user.username}
+        onChange={this.inputHandler}></input>
 
+      <label htmlFor="password">Password</label>
       <input 
       type='text' 
       id='password' 
       name='password' 
-      value={this.state.user.password}></input>
+      value={this.state.user.password}
+      onChange={this.inputHandler}></input>
     </form>
+    <button type="submit">Submit</button>
 
     { this.state.message 
         ? (<h3>{this.state.message}</h3>)
