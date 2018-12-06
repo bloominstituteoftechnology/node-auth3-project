ba0670b71a7542d1437c2169c6db8dbc4e8fa5d9
Phil Hart <phil@Phils-iMac.local>
2018-12-06 10:06:41 -0700

    Created react app

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Switch,  Route } from 'react-router-dom'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        </header>
+          <section>
+            <Switch>
+              <Route path="/register" component={Register} />
+            </Switch>
+          </section>
+      </div>
+    );
+  }
+}
+
+export default App;
