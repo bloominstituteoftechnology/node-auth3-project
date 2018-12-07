6a55af36311357c5f1f1efdb9192cb9456cffc52
Scott Bren <smbren@ncsu.edu>
2018-12-07 17:03:44 -0500

    Sets up React App

---
+++ lambda-client/src/components/Register.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+const initialUser = {
+    username: '',
+    password: ','
+};
+
+export default class Register extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: { ...initialUser },
+            message: '',
+        }
+    }
+
+    submitHandler = () => {}
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.submitHandler}>
+                    <label htmlFor="username">Username</label>
+                        <input 
+                            type="text" 
+                            id="username"
+                            name="username" 
+                            value={this.state.user.username}>
+                        
+                        </input>
+                    <label htmlFor="password">Password</label>
+                        <input 
+                            type="text" 
+                            id="password"
+                            name="password" 
+                            value={this.state.user.password}>
+                        
+                        </input>
+                </form>
+            { this.state.message
+            
+            
+            }
+            </div>
+        );
+    }
+}
