450244b58e4d7454a4505bef8496d376a5034483
Colin Dismuke <colin.dismuke@gmail.com>
2019-01-21 12:23:41 -0600

    Added users table with usernames, password, and department

--- data/migrations/20190121121759_authentication-ii-init.js
+++ data/migrations/20190121121759_authentication-ii-init.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("users", table => {
+    table.increments();
+    table
+      .string("username", 255)
+      .unique()
+      .notNullable();
+    table.string("password", 255).notNullable();
+    table.string("department", 255).notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("users");
 };
