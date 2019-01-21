65a5be6277d5314abb36cc6a326a0a98d9cc41bc
Robert Billington <robertbillington@Roberts-MacBook-Pro.local>
2019-01-21 11:36:27 -0600

    Finished setting up package.json. knexfile, and wrote migration for user table

---
+++ data/migrations/20190121113158_user.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('user', table =>{
+      table.increments();
+      table
+        .string('username')
+        .notNullable()
+        .unique();
+      table
+        .string('password')
+        .notNullable();
+      table
+        .string('department')
+        .notNullable();        
+  })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('user');
+};
