994da97aa058b2673e81ac46ecb392ca6ab7ecae
Eric Fleming <efleming111@yahoo.com>
2019-01-21 06:24:51 -0700

    Setup initial project

---
+++ data/migrations/20190121061726_add_users_table.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', table=>{
+      table.increments();
+      table.string('username').notNullable().unique();
+      table.string('password').notNullable();
+      table.string('department').notNullable();
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropIfTableExists('users');
+};
