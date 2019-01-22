4b20bd665b6c279e94f98240f615527883903311
Jennifer King <jenbellpgh@gmail.com>
2019-01-22 00:53:50 -0500

    Added token sending/receiving /api/users will get user list based on dept

--- index.js
+++ index.js
@@ -2,15 +2,31 @@
 const express = require('express');
 const knex = require('knex');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // Server requires
 const server = express();
 const dbConfig = require('./knexfile');
 const db = knex(dbConfig.development);
 const PORT = 1234;
+const secret = 'something hidden and not really here in this file';
 
 /* ---------- Middleware ---------- */
 server.use(express.json());
+
+const generateToken = (user) => {
+  console.log(user.user);
+  const payload = {
+    user: user.user,
+    dept: user.dept
+  };
+  
+  const options = {
+    expiresIn: '1h',
+    jwtid: '21234'
+  };
+  return jwt.sign(payload, secret, options);
+};
 
 /* ---------- Endpoints ---------- */
 
@@ -27,7 +43,14 @@ server.post('/api/register', (req,res) => {
 
     db(`users`).insert(newUser)
       .then( (newId) => {
-        res.status(201).json(newId);
+
+        // Get user from db:
+        db(`users`).where('id', newId[0])
+          .then( (usr) => {
+            const token = generateToken(usr);
+            res.status(201).json({id: newId[0], token});
+          });
+        // end-user-db
       })
       .catch( (err) => {
         res.status(500).json({ error: `Could not register new user: ${err}` });
@@ -44,7 +67,22 @@ server.post('/api/register', (req,res) => {
 // - Use the credentials sent inside the body to authenticate the user. 
 // - On successful login, create a new JWT with the user id as the subject and send it back to the client. 
 // - If login fails, respond with the correct status code and the message: 'You shall not pass!'
-server.post('/api/login', (req,res) => {});
+server.post('/api/login', (req,res) => {
+  const login = req.body;
+
+  db('users').where('user', login.user).limit(1)
+    .then( (user) => {
+      if( user.length && bcrypt.compareSync(login.pass, user[0].pass)) {
+        const token = generateToken(user[0]);
+        res.status(201).json({ info: "Logged in", token});
+      } else {
+        res.status(201).json({ error: "You shall not pass!" });
+      }
+    })
+    .catch( (err) => {
+      res.status(500).json({ error: err });
+    });
+});
 
 
 // GET:
@@ -53,14 +91,29 @@ server.post('/api/login', (req,res) => {});
 // - If the user is not logged in repond with the correct status code and the message: 'You shall not pass!'. 
 // - Use this endpoint to verify that the password is hashed before it is saved.
 server.get('/api/users', (req, res) => {
-  db('users').select('id', 'user', 'dept')
-    .then( (users) => {
-      res.json(users);
-    })
-    .catch( (err) => {
-      res.status(500).json({ error: err });
-    });
-  // end-db
+  const token = req.headers.authorization;
+
+  if( !token ){
+    console.log( "Invalid login!" );
+    res.status(401).json({ message: "No token provided."})
+  }
+
+  jwt.verify(token, secret, (err, decodedToken) => {
+    if(err) {
+      // invalid token
+      res.status(401).json({ message: "You shall not pass!" });
+    } else {
+      // valid token
+      db('users').select('id', 'user').where('dept', decodedToken.dept )
+      .then( (users) => {
+        res.json(users);
+      })
+      .catch( (err) => {
+        res.status(500).json({ error: err });
+      });
+    // end-db  
+    }
+  })
 });
 
 
