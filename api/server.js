bb18ab877a2d90adb0f93d0c5ec8cf2dba1e4ce1
Lidii <lidiiany92@gmail.com>
2019-01-23 15:58:28 -0500

    Created register endpoint

--- api/server.js
+++ api/server.js
@@ -16,4 +16,17 @@ server.get("/", (req, res) => {
   res.send("Test test 1 2 3");
 });
 
+server.post("/register", async (req, res) => {
+  try {
+    const userCredentials = req.body;
+    const hash = bcrypt.hashSync(userCredentials.password, 12);
+    userCredentials.password = hash;
+
+    const newUser = await db("users").insert(userCredentials);
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(500).json({ message: "REgister failed. Please try again" });
+  }
+});
+
 module.exports = server;
