78c39e66f8a1d205da0ebd72815b2f61aa0bb678
Freekeyboy <jjhutton81@gmail.com>
2019-01-22 20:18:05 -0800

    Added migration for users table.

--- data/migrations/20190122182404_01_users.js
+++ data/migrations/20190122182404_01_users.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
-};
+    return knex.schema.createTable('users', users => {
+        users.increments();
+        users.string('username', 25).notNullable().unique();
+        users.string('password', 16).notNullable();
+        users.string('department', 25);
+    })
+  };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('users');
 };
