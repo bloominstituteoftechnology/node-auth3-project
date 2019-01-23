4187e4e210ae64eeb626d20c79e91287038d640e
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2019-01-22 20:49:28 -0700

    Added endpoint to register new user and return token

--- index.js
+++ index.js
@@ -18,11 +18,45 @@ function generateToken(user) {
     };
     const options = {
         expiresIn: '1h',
-        // jwtid??
+        jwtid: 'jwtid'
     };
     return jwt.sign(payload, secret, options);
 };
 
-
+server.post('/api/register', (req, res) => {
+    const creds = req.body;
+    const hash = bcrypt.hashSync(creds.password, 12);
+    creds.password = hash;
+    if (req.body.username && req.body.password && req.body.department) {
+        db('users_table')
+        .insert(creds)
+        .then(ids => {
+            const id = ids[0];
+            db('users_table')
+                .where('id', id)
+                .then(user => {
+                    const token = generateToken(user);
+                    res
+                        .status(201)
+                        .json({subject: id, token});
+                })
+                .catch(err => {
+                    res
+                        .status(500)
+                        .json({message: 'The user could not be registered at this time.'})
+                })
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({message: 'The user could not be registered at this time.'})
+        });
+    }
+    else {
+        res
+            .status(400)
+            .json({message: 'Please provide a username, password, and department to register.'})
+    }
+});
 
 server.listen(PORT, () => console.log(`Running on ${PORT}`));
