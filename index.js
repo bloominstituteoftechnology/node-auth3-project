e7f194d78f2e56ae4da62bb2d46db9f5781fcd8c
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2019-01-22 20:34:27 -0700

    Added method to generate token

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ const PORT = 4040;
 
 server.use(express.json(), cors());
 
+const secret = `terces`;
+
+function generateToken(user) {
+    const payload = {
+        username: user.username
+    };
+    const options = {
+        expiresIn: '1h',
+        // jwtid??
+    };
+    return jwt.sign(payload, secret, options);
+};
+
+
+
 server.listen(PORT, () => console.log(`Running on ${PORT}`));
