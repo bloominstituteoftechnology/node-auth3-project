92ba95c967273feb5fdf58ee7338501db2f66e8e
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2019-01-22 21:07:49 -0700

    Added method to protect endpoint

--- index.js
+++ index.js
@@ -23,6 +23,27 @@ function generateToken(user) {
     return jwt.sign(payload, secret, options);
 };
 
+function protected(req, res, next) {
+    const token = req.headers.authorization;
+    if (token) {
+        jwt.verify(token, secret, (err, decodedToken) => {
+            if (err) {
+                res
+                    .status(401)
+                    .json({message: 'You shall not pass!'})
+            }
+            else {
+                next();
+            }
+        });
+    }
+    else {
+        res
+            .status(401)
+            .json({message: 'No token provided.'})
+    }
+};
+
 server.post('/api/register', (req, res) => {
     const creds = req.body;
     const hash = bcrypt.hashSync(creds.password, 12);
