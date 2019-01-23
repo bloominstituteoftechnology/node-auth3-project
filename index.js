5aac432c5cfd01b4c7a23cd232cf45213f1130e8
alasalle <angela.lasalle91@gmail.com>
2019-01-22 20:47:52 -0500

    set up index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const server = express();
+const jwt = require("jsonwebtoken");
+
+const cors = require("cors")
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const usersRouter = require("./routers/usersRouter");
+
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+server.use(morgan("dev"));
+
+server.use("/api", usersRouter);
+
+const PORT = 4000;
+server.listen(PORT, () => {
+  console.log(`server is now running on port ${PORT}`);
+});
