461892ef3e3806a719e8845bb9519c481a087192
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2019-01-23 03:00:40 -0500

    added register route

---
+++ database/dbHelpers.js
@@ -0,0 +1,18 @@
+const knex = require('knex');
+const knexConfig = require('../knexfile.js');
+const db = knex(knexConfig.development)
+
+module.exports = {
+  insertUser: (user) => {
+    return db('users').insert(user);
+  },
+  findByUsername: (username) => {
+    return db('users').where('username', username);
+  },
+  findByID: (id) => {
+    return db('users').where('id', id).first();
+  },
+  getUsers: (users) => {
+    return db('users').select('id', 'username')
+  }
+}
--- index.js
+++ index.js
@@ -3,9 +3,65 @@ const server = express()
 const cors = require('cors')
 const helmet = require('helmet');
 const jwt = require('jsonwebtoken');
+const db = require('./database/dbHelpers');
+const uuid = require('uuid/v1');
+const bcrypt = require('bcryptjs');
 
-server.use(cors(),helmet(),express.json());
+server.use(cors(), helmet(), express.json());
 
+const secret = ("easySecret");
+//never do this in production 
+
+function newToken(user) {
+  const payload = {
+    username: user.username
+  };
+  const options = {
+    expiresIn: "1h",
+    jwtid: uuid()
+  };
+  return jwt.sign(payload, secret, options);
+}
+
+function protected(req, res, next) {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ error: 'invalid token' })
+      } else {
+        console.log('decoded token', decodedToken);
+        if (req.username = decodedToken.username) {
+          next()
+        } else {
+          res.status(401).json({ error: 'bad token' });
+        };
+      }
+    })
+  }
+};
+
+server.post('/api/register', (req, res) => {
+  const loginInfo = req.body;
+  const hashedPass = bcrypt.hashSync(loginInfo.password, 12);
+  loginInfo.password = hashedPass;
+  db.insertUser(loginInfo)
+    .then(ids => {
+      const id = ids[0];
+      db.findByID(id)
+        .then(user => {
+          const token = (newToken(user));
+          res.status(200).json({ id: user.id, token });
+        })
+        .catch(err => {
+          console.log("error", err);
+          res.status(500).json({ error: 'Something went wrong' })
+        })
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
 
 
 
