20bf511f5a161cbde8ca13e3a9442974daf55fef
iepoch <staclem@live.com>
2019-01-22 17:28:22 -0700

    Added:> Login with Hashcompare. > Register with hash > And updated Helper function

--- data/helpers/dataHelper.js
+++ data/helpers/dataHelper.js
@@ -1,18 +1,19 @@
+const knex = require('knex')
 const knexConfig = require('../../knexfile')
 
-const db = knex(knexConfig.development)
+const db = knex(knexConfig.development);
 
 
 module.exports = {
     getUser: () => {
         return db('users')
             .select('id', 'username')
-            .orderby('department')
+            .orderBy('department')
     },
     insert: (user) => {
         return db('users').insert(user)
     },
     findByUser: (username) => {
-        return db('users').where('username', username)
+        return db('users').where('username', username).first()
     }
 }
--- index.js
+++ index.js
@@ -11,10 +11,36 @@ server.use(express.json())
 // May add cors orgin to make sure it is allowed.
 
 server.post('/api/register', (req, res) => {
+    const user = req.body
+    user.password = bcrypt.hashSync(user.password, 12)
+
+    db.insert(user)
+        .then(ids => {
+            res.status(201).json({id: ids[0]})
+        })
+        .catch(err => {
+            res.status(500).send(err)
+        }
+        )
 
 })
 
 server.post('/api/login', (req, res) => {
+    const creds = req.body
+
+    db.findByUser(creds.username)
+        .then(user => {
+            if (user && bcrypt.compareSync(creds.password, user.password)) {
+
+                res.status(200).json({info: 'Login Successfull'})
+            } else {
+                res.status(404).json({err: 'Not a valid login'})
+            }
+        })
+        .catch(err => {
+            res.status(500).send(err)
+        }
+        )
 
 })
 
