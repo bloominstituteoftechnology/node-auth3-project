2f7af7e6cab812e95b94b2fc96e3c7cfcb0713ae
jbrock1981 <jbrock1981@gmail.com>
2019-01-23 09:11:02 -0600

    token stuffs done with test to see if server launches

--- index.js
+++ index.js
@@ -7,4 +7,42 @@ const db = require('./data/dbConfig');
 const server = express();
 const PORT = 4200;
 
+const secret = "SuperCaliFragilisticExpialadocious8675309"; //heh, two fun old references here! :-)
 
+function protect(req, res, next) {
+    const token = req.headers.authorization;
+    if (token) {
+        jwt.verify(token, secret, (err, decodedToken) => {
+            if (err) {
+                res
+                    .status(401)
+                    .json({ message: "nope" });
+            } else {
+                next();
+            }
+        });
+    } else {
+        res
+            .status(401)
+            .json({ message: "no token?" });
+    }
+}
+
+function makeToken(user) {
+    const payload = {
+        username: user.username
+    };
+    const options = {
+        expiresIn: "42mins",
+        jwtid: "8675309"
+    };
+    return jwt.sign(payload, secret, options);
+}
+
+server.get('/', (req, res) => {
+    res.send("it works!");
+});
+
+server.listen(PORT, () => {
+    console.log(`working so far, on port ${PORT}`);
+});
