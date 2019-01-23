a24864045109b874fb9b4d9f116192c1fff95e64
shannonhealy <sahealy525@gmail.com>
2019-01-23 12:59:18 -0500

    "register" API endpoint

--- index.js
+++ index.js
@@ -10,7 +10,19 @@ const server = express();
 server.use(express.json());
 
 server.post("/api/register", (req, res) => {
-    
+    if (req.body.username && req.body.password && typeof req.body.username === "string" && typeof req.body.password === "string") {
+        let user = req.body;
+        user.password = bcrypt.hashSync(user.password);
+        db("users")
+            .insert(user)
+            .then(ids => {
+                res.status(201).json(ids[0]);
+            }).catch(error => {
+                res.status(500).json({message: "Error registering user", error: error});
+            });
+    } else {
+        res.status(400).json({ error: "Incorrectly formatted user data" });
+    }
 });
 
 server.post("/api/login", (req, res) => {
