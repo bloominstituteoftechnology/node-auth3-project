2e1343b4c65cedac8c5a081364333c2b119f4f6c
Dewayne87 <dewaynelindsay87@gmail.com>
2019-01-22 20:20:08 -0800

    finshed migrations and reigster and login endpoints

---
+++ data/migrations/20190122182259_users_table.js
@@ -0,0 +1,20 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable('users', table => {
+        table
+            .increments();
+        table
+            .string('username', 128)
+            .notNullable()
+            .unique();
+        table
+            .string('password', 128)
+            .notNullable();
+        table
+            .string('department', 128);
+    });
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('users')
+};
