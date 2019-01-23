8f3312f7231bb9e7c1c4b30850e1c827ee5f3e47
FreitagDavid <freitagdavid@outlook.com>
2019-01-23 17:43:50 -0500

    migrations/20190123162344_create_users_table: create

---
+++ migrations/20190123162344_create_users_table.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('users', tbl => {
+        tbl.increments();
+
+        tbl.string('username', 255)
+            .notNullable()
+            .unique();
+        tbl.string('password', 255).notNullable();
+        tbl.string('department', 255).notNullable();
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('users');
+};
