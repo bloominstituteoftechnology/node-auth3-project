dfee132c9353b771ed1d273a8391a7b5aacd0ee9
jbrock1981 <jbrock1981@gmail.com>
2019-01-23 08:40:53 -0600

    filled out users migration

--- data/migrations/20190123080759_users.js
+++ data/migrations/20190123080759_users.js
@@ -1,8 +1,11 @@
-
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable("users", table => {
+        table.increments();
+        table.string("username", 42).unique().notNullable();
+        table.string("password", 42).unique().notNullable();
+    });
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists("users");
 };
