a9f4e1b32326aca85bd3ed3f59f8ed1fd0494db4
Pascale Pierre <pascaleee@gmail.com>
2019-01-22 20:49:22 -0600

    added functionality to login and register

---
+++ database/dbHelpers.js
@@ -0,0 +1,26 @@
+const knex = require('knex');
+
+const knexConfig = require('../knexfile.js');
+
+const db = knex(knexConfig.development);
+
+function insertUser(user){
+  return db('users').insert(user)
+}
+
+function findUsers(){
+  return db('users').select('id', 'username');
+}
+
+function findByUsername(username){
+  return db('users').where('username', username);
+}
+
+function findByUserId(id){
+  return db('users').where({id}).first()
+}
+
+
+module.exports = {
+   insertUser, findByUsername, findUsers
+}
--- index.js
+++ index.js
@@ -1,79 +1,116 @@
+
+const jwt = require('jsonwebtoken');
 const express = require('express');
 const cors = require('cors');;
-const knex = require('knex');
-const jwt = require('jsonwebtoken');
-const knexConfig = require('./knexfile.js');
-
-const db = knex(knexConfig.development);
-
+const db = require('./database/dbHelpers');
 const server = express();
 const bcrypt = require('bcryptjs');
 const session = require('express-session');
 
-server.use(session({
-  name: 'notsession', // default is connect.sid
-  secret: 'nobody tosses a dwarf!', //this allows us to encrypt or unencrypt. we wouldn't want this to be hard coded or a string.
-  cookie: {
-    maxAge: 1 * 24 * 60 * 60 * 1000, //the age of our cookie
-  }, // this is 1 day in milliseconds
-  httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
-  resave: false, //forces the session to be saved back to the session store, even if the session wasn't modified during the request
-  saveUninitialized: false, //do we want to make people give their consent for cookies? thing EU's GDPR laws
- }))
+server.use(express.json());
+server.use(cors())
 
- 
+const secret = "ssecretsecretsecret";
+
+ //restricts access based on login
+function protect(req, res, next) {
+   const token = req.headers.authorization;
+   if(token){
+      jwt.verify(token, secret, (err, decodedToken) => {
+         if(err) {
+            res.status(400).send("invalid login")
+         } else {
+//not sure what this does need to ask about this tuesday**
+            req.username = decodedToken.username
+            next();
+         }
+      })
+   } else {
+      res.status(401).json({err: "token missing"});
+   }
+}
+
+
+function generateToken(user) {
+    const payload = {
+       jwtid: user.id,
+       username: user.username
+    }
+    const options = {
+       expiresIn: "1hr",
+    }
+    return jwt.sign(payload, secret, options)
+ }
+
+
+function protected(req, res, next) {
+  const token = req.headers.authorization;
+
+   if (token) {
+      jwt.verify(token, secret, (err, decodedToken) => {
+          if (err) {
+              //token will be invalid
+              res.status(401).json({message: "Invalid Token"})
+          } else {
+              req.username = decodedToken.username
+              next();
+          }
+      })
+
+   } else {
+      res.status(401).json({ message: 'no token provided' })
+  }
+}
 
 server.get('/', (req, res) => {
   res.send('Its Alive!');
 });
 
+
 server.post('/api/register', (req, res) => {
-  const user =  req.body;
-  console.log('session', req.session)
-  user.password = bcrypt.hashSync(user.password);
-  db.insertUser(user)
-  .then(ids => {
-    res.status(201).json({id: ids[0]})
-  })
-  .catch(err => {
-    res.status(500).send(err)
-  })
-});
+    const user = req.body
+    user.password = bcrypt.hashSync(user.password, 12)
 
+     db. insertUser(user)
+        .then(ids => {
+            const id = ids[0]
+            db.findUsers(id)
+                .then(user => {
+                    const token = generateToken(user);
+                    res.status(201).json({id: user.id, token})
+                })
+                .catch(err => {
+                    res.status(404).send(err)
+                })
+        })
+        .catch(err => {
+            res.status(500).send(err)
+        }
+        )
 
-
-server.post('/api/login', (req, res) => {
-  //check that username AND passwords match
-  const bodyUser = req.body;
-  db.findByUsername(bodyUser.email)
-    .then(users => {
-      if(users.length && bcrypt.compareSync(bodyUser.password, users[0].password)){
-        res.json({ info: "Logged In!" })
-      } else {
-        res.status(404).json({ error: 'You shall not pass' })
-      }
-    })
-    .catch(err => {
-      res.status(500).send(err)
-    })
-    
  });
+ server.post("/api/login", (req, res) => {
+    const login = req.body;
+    if (login.username && login.password) {
+       db.findByUsername(login.username)
+          .then(users => {
+             if(users.length && bcrypt.compareSync(login.password, users[0].password)) {
+             const token = generateToken(users[0])
+             res.send(`Welcome ${login.username}`);
+              } else { res.status(404).send("You shall not pass!");}
+          })
+          .catch(err => {
+             res.status(500).send(err);
+          });
+    } else  res.status(400).json({err: "please provide a username and password"});
+ })
 
 
- server.get('/api/users', (req, res) => {
-  if(req.session && req.session.id){
+ server.get("/api/users", protect, (req, res) => {
     db.findUsers()
-    .select('id', 'firstname', 'lastname')
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => res.send(err));
-  } else {
-    res.status(400).send('You shall not pass!');
-  }
+       .then(users => {res.json(users)})
+       .catch(err => {res.json(err)});
  });
-
- 
 
 
 server.listen(8000, () => console.log('Running on port 8000'));
