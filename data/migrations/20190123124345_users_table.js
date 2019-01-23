3c365ce9d952da4f42f39442decd3f5ad2b327eb
shannonhealy <sahealy525@gmail.com>
2019-01-23 12:46:21 -0500

    created and ran migration for creating users table

---
+++ data/migrations/20190123124345_users_table.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable("users", table => {
+        table.increments("id").primary();
+        table.string("username").notNullable();
+        table.string("password").notNullable();
+        table.string("department");
+    });  
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists("users");  
+};
