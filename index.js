1702b925fc066820fcd206fa7775135c916ccb87
vy3191 <venkyafhs@gmail.com>
2019-01-23 22:48:00 -0500

    Added migrations

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const cors = require('cors');
 const bcryptjs = require('bcryptjs');
+const logger = require('morgan');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
 const server = express();
 const PORT = 5500;
 
 server.use(express.json());
+server.use(logger('dev'));
 server.use(cors());
 server.get('/', (req,res) => {
    res.json(`Server is and running`);
