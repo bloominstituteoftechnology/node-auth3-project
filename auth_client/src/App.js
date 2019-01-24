7c75badf384eda29bb55b93f5f61ad484084bf9c
iepoch <staclem@live.com>
2019-01-23 21:47:18 -0700

    Added Navigation

--- auth_client/src/App.js
+++ auth_client/src/App.js
@@ -1,24 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { NavLink, Route } from "react-router-dom";
+import "./App.css";
+
+const Home = props => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <nav>
+            <NavLink to="/" exact>
+              Home
+            </NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/signin" exact>
+              Signin
+            </NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/users" exact>
+              Users
+            </NavLink>
+          </nav>
         </header>
       </div>
     );
