9761118d26f1b450b61875ed13c6b8225f137c1d
Levi Appenfelder <levi.appenfelder@gmail.com>
2019-01-23 18:58:07 -0700

    Data added through seeding for testing purposed...Bug fixed

--- data/seeds/01_users.js
+++ data/seeds/01_users.js
@@ -3,7 +3,10 @@ const bcrypt = require('../../node_modules/bcrypt')
 
 let USERS = [{username: "Carson", password: "1", department: "sheriff"}, {username: "Chester", password: "2", department: "deputy"}, {username: "Caleb", password: "3", department: "citizen"}]
 
-let HASHEDU = USERS.map( (user) => {return user.password = bcrypt.hashSync(user.password, 16)})
+let HASHEDU = USERS.map( (user) => {
+  user.password = bcrypt.hashSync(user.password, 16); return user})
+
+console.log (HASHEDU)
 
 exports.seed = function(knex, Promise) {
 
