8f6fdca76c479a8befa50de28ab8a743f308d1bd
rlmclaughlin <rlmclaughlin@yahoo.com>
2019-01-23 23:38:38 -0600

    populated the signin.js file

--- users/src/auth/signin.js
+++ users/src/auth/signin.js
@@ -4,13 +4,42 @@ import axios from 'axios'
 export default class Signin extends React.Component {
     constructor(){
         super();
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
 
+    inputHandler = event => {
+        event.preventDefault(); 
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    
+    submitHandler = event => {
+        event.preventDefault(); 
+        axios.post('', this.state)
+          .then(res => {
+            localStorage.setItem('jwt', res.data.token)
+          })
+          .catch(error => {
+              console.log("we've encountered an error")
+          })
     }
 
     render(){
         return(
             <div>
-                Sign In 
+                <p>username</p>
+                  <input name='username' 
+                         value={this.state.username} 
+                         onChange={this.inputHandler}/>
+                <p>password</p>
+                  <input name='password' 
+                         value={this.state.password}
+                         onChange={this.inputHandler}/> 
+                <button onClick={this.submitHandler}>Submit</button>
             </div> 
         )
     }
