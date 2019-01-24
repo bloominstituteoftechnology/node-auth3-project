fc4a779e47bc518132769f8ddac08cc102b77527
FreitagDavid <freitagdavid@outlook.com>
2019-01-24 18:08:11 -0500

    client/src/components/Users: Got it working

--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Users = () => {
-    return <div />;
-};
+const endPoint = 'http://localhost:5000/api/';
+
+class Users extends Component {
+    constructor() {
+        super();
+        this.state = {
+            users: [],
+        };
+    }
+
+    componentDidMount() {
+        const endpoint = `${endPoint}users`;
+
+        const token = localStorage.getItem('jwt');
+        const requestOptions = {
+            headers: {
+                authorization: token,
+            },
+        };
+
+        axios
+            .get(endpoint, requestOptions)
+            .then(res => this.setState({ users: res.data.users }))
+            .catch(err => console.log(err));
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Users</h2>
+                <ul>
+                    {this.state.users.map(u => (
+                        <li key={u.id}>{u.username}</li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+}
 
 export default Users;
