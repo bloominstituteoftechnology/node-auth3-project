3d63b1a3286ac0ea4de7fa5660c7aca7d2a1667f
rlmclaughlin <rlmclaughlin@yahoo.com>
2019-01-23 23:19:21 -0600

    added react dependencies and set up boilerplate for files

---
+++ users/src/auth/signin.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import axios from 'axios'
+
+export default class Signin extends React.Component {
+    constructor(){
+        super();
+
+    }
+
+    render(){
+        return(
+            <div>
+                Sign In 
+            </div> 
+        )
+    }
+}
