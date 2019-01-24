c7f2e5e50761cb3530ceb378a0c8e78b2784aae5
FreitagDavid <freitagdavid@outlook.com>
2019-01-24 16:20:48 -0500

    client: Basic skeleton work

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Users from './components/Users';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            authenticated: false,
+        };
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Route exact path="/" component={Users} />
+                <Login />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ client/src/components/Signup.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Signup = () => {
+    return <div />;
+};
+
+export default Signup;
