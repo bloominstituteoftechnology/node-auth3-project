fd6cdc346b6a6f44d34a74fe7dce9bab2fb1d999
FreitagDavid <freitagdavid@outlook.com>
2019-01-24 18:05:56 -0500

    client/src/components/Login: Got it working

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Login = () => {
-    return <div />;
-};
+const endPoint = 'http://localhost:5000/api/';
+
+class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            username: 'testing2',
+            password: '123445',
+            department: 'accounting',
+        };
+    }
+
+    handleChange = event => {
+        const { name, value } = event.target;
+
+        this.setState({ [name]: value });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        axios
+            .post(`${endPoint}login`, this.state)
+            .then(res => {
+                localStorage.setItem('jwt', res.data.token);
+            })
+            .catch(err => console.log(err));
+    };
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="username">Username: </label>
+                    <input
+                        type="text"
+                        name="username"
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <label htmlFor="password">Password: </label>
+                    <input
+                        type="password"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <label htmlFor="department">Department: </label>
+                    <button type="submit">Log in</button>
+                </form>
+            </div>
+        );
+    }
+}
 
 export default Login;
