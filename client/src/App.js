013c2541627080855255bffb126113738ed56b96
Rob Salzberg <robsalzberg@gmail.com>
2019-01-24 19:24:16 -0500

    Added register route to React App

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import { NavLink, Route } from "react-router-dom";
 
 import Home from "./components/Home";
+import Register from "./components/Register";
 
 import "./App.css";
-
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -18,8 +18,12 @@ class App extends Component {
         <NavLink Component={NavLink} to={"/"}>
           Home
         </NavLink>
+        <NavLink Component={NavLink} to={"/register"}>
+          Register
+        </NavLink>
         <main>
           <Route exact path="/" component={Home} />
+          <Route path="/register" component={Register} />
         </main>
       </Container>
     );
---
+++ client/src/components/Register.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import axios from "axios";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  width: 400px;
+  margin: 0 100px;
+  flex-direction: column;
+`;
+
+class Register extends Component {
+  state = {
+    username: "",
+    password: "",
+    department: ""
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const endpoint = "http://localhost:3300/api/register";
+    axios
+      .post(endpoint, this.state)
+      .then(res => {
+        console.log(res.data);
+        localStorage.setItem("jwt", res.data.token);
+      })
+      .catch(err => {
+        console.log("ERROR");
+      });
+
+    //    this.props.history.push("/users");
+  };
+
+  handleInput = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <Container>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <input
+              label="Username"
+              name="username"
+              placeholder="your@email.com"
+              value={this.state.username}
+              onChange={this.handleInput}
+              type="text"
+            />
+          </div>
+          <div>
+            <input
+              label="Password"
+              name="password"
+              placeholder="·····"
+              value={this.state.password}
+              onChange={this.handleInput}
+              type="password"
+            />
+          </div>
+          <div>
+            <input
+              label="Department"
+              name="department"
+              placeholder="Computer Science"
+              value={this.state.department}
+              onChange={this.handleInput}
+              type="text"
+            />
+          </div>
+          <div>
+            <button primary lg type="submit">
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </Container>
+    );
+  }
+}
+
+export default Register;
