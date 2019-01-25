ab0af5e4704598e5aa012e7aef3861a77dfe0df8
shannonhealy <sahealy525@gmail.com>
2019-01-24 22:29:16 -0500

    set up routing between pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import {Route, BrowserRouter as Router} from "react-router-dom";
+
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Users from "./pages/Users";
 
 class App extends Component {
   render() {
     return (
-      ""
+      <Router>
+        <div>
+          <Route exact path="/" component={Register} />
+          <Route exact path="/signup" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/users" component={Users} />
+        </div>
+      </Router>
     );
   }
 }
