df73359c7167e3f90a29cb77eef6fb1d5865b7df
BryceEvans <evans.bryce@gmail.com>
2019-01-24 20:50:22 -0700

    Getting started on this React app.

--- auth-ii_client/src/App.js
+++ auth-ii_client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { NavLink, Route } from 'react-router-dom';
+
 import './App.css';
 
 class App extends Component {
@@ -7,18 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
         </header>
       </div>
     );
