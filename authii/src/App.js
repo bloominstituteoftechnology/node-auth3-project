b541ea7548bf555bdd43441fb5fd882fb2f3cad9
Freekeyboy <jjhutton81@gmail.com>
2019-01-24 19:21:20 -0800

    Tested progress. App loads. Home and User links function.

--- authii/src/App.js
+++ authii/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { NavLink, Route } from 'react-router-dom';
-import './App.css';	import './App.css';
-import Users from './users/Users';
+import './App.css';	
+import Users from './Components/users/Users';
 
 const Home = props => {
   return (
@@ -22,8 +22,8 @@ class App extends Component {
             <NavLink to = "/users">Users</NavLink>
           </nav>
           <main>
-            <Rout path = "/" component = {Home} exact/>
-            <Rout path = "/users" component = {Users}/>
+            <Route path = "/" component = {Home} exact/>
+            <Route path = "/users" component = {Users}/>
           </main>
         </header>
       </div>
--- authii/src/Components/users/Users.js
+++ authii/src/Components/users/Users.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import '../App.css';
 
 class Users extends Component {
   render() {
