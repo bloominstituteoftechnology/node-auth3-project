3a90778ed6f6fc4bec03cdd6824fb283b1da279c
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-01-24 22:29:42 -0500

    added GET endpoint for /api/users

--- index.js
+++ index.js
@@ -10,6 +10,29 @@ const PORT = 8080;
 
 const secret = 'secretstuff';
 
+function protect(req, res, next) {
+  const token = req.headers.authorization;
+  jwt.verify(token, secret, (err, decodedToken) => {
+    if (err) {
+      res.status(401).json({message: 'Invalid token'});
+    } else {
+      next();
+    }
+  });
+}
+
+function genToken(user) {
+  const payload = {
+    username: user.username,
+  };
+
+  const options = {
+    expiresIn: '1h'
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -35,11 +58,9 @@ app.post('/api/login', (req, res) => {
   const creds = req.body;
   db('users').where('username', creds.username)
     .then(user => {
-      console.log(user[0].password);
-      console.log(bcrypt.compareSync(creds.password, user[0].password));
       if (user && bcrypt.compareSync(creds.password, user[0].password)) {
         // return jwt
-        const token = jwt.sign({subject: user.id}, secret);
+        const token = genToken(user)
         res.json({id: user.id, token});
       } else {
         res.status(400).json({message: 'You shall not pass!'});
@@ -51,7 +72,15 @@ app.post('/api/login', (req, res) => {
 });
 
 // GET /api/users
-
+app.get('/api/users', protect, (req, res) => {
+  db('users').select('id', 'username')
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`app is running on PORT: ${PORT}`);
