0b1875ea06376120cd11752b350a9d6dbd83ae83
Jennifer King <jenbellpgh@gmail.com>
2019-01-24 22:45:56 -0500

    Started signin route

--- test/src/App.js
+++ test/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { NavLink, Route } from 'react-router-dom';
 
+// Includes for Routes
+import Signin from './auth/Signin';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <nav>
+            <NavLink to='/' exact>Home</NavLink> | 
+            <NavLink to='/signin'>Log In</NavLink> | 
+            <NavLink to='/users'>Users</NavLink> |
+            <NavLink to='/signout'>Log Out</NavLink>
+          </nav>
+          <main>
+            <Route path='/' exact></Route>
+            <Route path='/signin' component={Signin} exact></Route>
+          </main>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
---
+++ test/src/auth/Signin.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Signin extends Component {};
+
+export default Signin;
