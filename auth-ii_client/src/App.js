694e99e9660b47b41d9ac289952cd418777e8363
BryceEvans <evans.bryce@gmail.com>
2019-01-24 20:57:09 -0700

    More progress made.

--- auth-ii_client/src/App.js
+++ auth-ii_client/src/App.js
@@ -3,12 +3,26 @@ import { NavLink, Route } from 'react-router-dom';
 
 import './App.css';
 
+const Home = props => {
+  return (
+    <div>
+      <h2>Welcome to</h2>
+      <h1>Auth-II!!</h1>
+    </div>
+  )
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          
+          <nav>
+            <NavLink exact to='/'>Home</NavLink>
+          </nav>
+          <div>
+            <Route exact path='/' component={Home} />
+          </div>
         </header>
       </div>
     );
