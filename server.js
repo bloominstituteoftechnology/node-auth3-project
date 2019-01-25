950ce05b4eeeb3bc8c5edacc12fc975bcaa80771
Levi Appenfelder <levi.appenfelder@gmail.com>
2019-01-24 23:05:12 -0700

    Backend Authentication with Web Tokens Complete

--- server.js
+++ server.js
@@ -2,7 +2,47 @@ const express = require('express');
 const knex = require('knex');
 const bcrypt = require('bcrypt')
 const dbConfig = require('./knexfile.js')
+const jwt = require('jsonwebtoken')
+
+const secret = "gold";
+
 require('dotenv/config')
+
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+    department: user.department
+  };
+
+  const options = {
+    expiresIn: '10min',
+    jwtid: '4040'
+  }
+  return jwt.sign(payload,secret,options)
+}
+
+function protected(req,res, next) {
+
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken ) => {
+        if (err) {
+          res.status(401).json({message: `Invalid Token`})
+        }
+
+        else {
+          req.username = decodedToken.username;
+          next();
+        }
+  } )
+}
+
+else { res.status(401).json({message: `No Token`})
+}
+
+}
+
 
 const PORT = process.env.PORT;
 
@@ -13,8 +53,9 @@ const server = express();
 
 server.use(express.json())
 
-server.get('/api/users', (req, res) => {
-      db('users')
+server.get('/api/users', protected, (req, res) => {
+
+      if (req.username) {db('users')
       .then(info => {
         res.json(info);
       })
@@ -22,7 +63,9 @@ server.get('/api/users', (req, res) => {
         res.status(500).json({err: `Failed to get users! ${err}`})
 
       }
-)})
+)}
+      else { res.status(500).json({err: `Failed to get users!`})
+    }})
 
 
 server.post('/api/register', (req,res) => {
@@ -33,6 +76,16 @@ server.post('/api/register', (req,res) => {
   if(missing.length===0)
   {db('users').insert(user)
   .then(ids => {
+    const id = ids[0];
+      db('users')
+      .where({id})
+      .first()
+      .then(user => {
+        const token = generateToken(user);
+
+        res.status(201).json({id: user.id, token})
+
+      })
       res.status(201).json({message: `user ${user.username} added`})})
   .catch(err => {
       res.status(500).json({err: `Could not add user! ${err}`})
@@ -42,6 +95,26 @@ server.post('/api/register', (req,res) => {
   else {res.status(500).json({message: `missing info: ${missing}`})}
   })
 
+
+  server.post('/api/login', (req,res) => {
+    const creds = req.body;
+  
+    db('users')
+    .where({username: creds.username})
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+        res.json({message: `${user.username}, You are logged in!`, department: user.department, token});
+
+      }
+
+      else {res.status(401).json( {message: `Failed to login!`})}
+    })
+    .catch(err => {
+      res.status(500).json({message: `${err}`})
+  })
+    })
 
 
 
