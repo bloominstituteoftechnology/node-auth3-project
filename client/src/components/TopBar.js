cf6d87a56fa1ad222e37ea36dcad94482243c0a3
FreitagDavid <freitagdavid@outlook.com>
2019-01-24 19:12:57 -0500

    client/src/components/TopBar: Implemented signout button

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const TopBar = () => {
+    const logout = () => {
+        localStorage.clear('jwt');
+    };
+
     return (
         <nav>
             <Link to="/login">Login</Link>
             <Link to="/signup">Signup</Link>
             <Link to="/users">Users</Link>
+            <button type="button" onClick={logout}>
+                Sign out
+            </button>
         </nav>
     );
 };
