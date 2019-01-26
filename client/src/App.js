4ef7c3efb03f6cb4c0b3528d78cd92350dec560f
Alex Martin <atonymartin20@yahoo.com>
2019-01-26 10:02:18 -0600

    Added NavLinks and Routes to index.js.  Added SignUp and Users pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { NavLink, Route } from 'react-router-dom';
 import './App.css';
+import axios from 'axios';
+
+import SignUp from './SignUp';
+import Users from './Users';
+
+const Home = props => {
+  return (
+    <div>
+      <h3>... You're still here?  It's over.  Go home.  Go - Ferris Bueller</h3>
+    </div>
+  )
+}
 
 class App extends Component {
+  logout = () => {
+    localStorage.removeItem('jwt');
+    window.location.reload();
+
+  }
+
+  SubmitHandler = (event) => {
+    event.preventDefault();
+    this.setState();
+
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <nav>
+            <NavLink to='/' exact> Home</NavLink>
+            <NavLink to='/SignUp'> Sign Up </NavLink>
+            <NavLink to='/Users'>Users Page </NavLink>
+            <button onClick={this.logout} onSubmit={this.SubmitHandler}> Sign Out </button>
+          </nav>
         </header>
+        <main>
+          <Route path='/' component={Home} exact></Route>
+          <Route path='/SignUp' component={SignUp} exact></Route>
+          <Route path='/Users' component={Users} exact></Route>
+        </main>
       </div>
     );
   }
