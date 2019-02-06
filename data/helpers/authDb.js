3851e1de990f12fc311d2bd16397bd16b9774481
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-02-06 07:24:01 -0600

    authDb done

---
+++ data/helpers/authDb.js
@@ -0,0 +1,14 @@
+const knex = require('knex')
+
+const dbConfig = require('../../knexfile')
+const db = knex(dbConfig.development)
+
+module.exports = {
+  register: (creds) => {
+    return db('users').insert(creds)
+  },
+
+  login: (username) => {
+    return db('users').where('usernsme', username).first()
+  }
+}
