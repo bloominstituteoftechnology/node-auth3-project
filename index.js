199ed1f0f087ed4caa4312f42288340aaffb00d1
Mitchell Robles <mitchellr92@live.com>
2019-02-06 21:06:25 -0800

    basic endpoints set up. will continue work tomorrow

--- index.js
+++ index.js
@@ -10,12 +10,61 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+server.use(
+    session({
+        name: "session1",
+        secret: "placeholder",
+        cookie: {
+            maxAge: 1 * 24 * 60 * 60 * 1000
+        },
+        httpOnly: true,
+        resave: false,
+        saveUninitialized: false
+    })
+);
+
 const PORT = 5500;
 
-server.post();
+server.post('/api/register', (req, res) => {
+    const user = req.body;
+    user.password = bcrypt.hashSync(user.password, 14);
+    db.insertUser(user)
+        .then(([id]) => {
+            res.status(201).json({ id });
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
 
-server.post();
+server.post('/api/login', (req, res) => {
+    const user = req.body;
+    db.findUser(user.username)
+        .then(users => {
+            if (users.length && bcrypt.compareSync(user.password, users[0].password)) {
+                req.session.userId = users[0].id;
+                res.json('Logged in!')
+            } else {
+                res.status(404).json({ err: 'Credentials are invalid' })
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
 
-server.post();
+server.get('/api/users', (req, res) => {
+    if (req.session && req.session.userId) {
+        db.getUsers()
+            .then(users => {
+                res.json(users)
+            })
+            .catch(err => {
+                res.status(500).send(err)
+            });
+    } else {
+        res.status(400).send('You shall not pass!')
+    }
+});
 
 server.listen(PORT, console.log(`Now listen on port ${PORT}`));
