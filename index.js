3b40c4d4c1582b0470692fa675d147d44ffcf1ca
cocoitali <cmburatto@gmail.com>
2019-02-09 11:06:31 -0700

    MVP : created auth file, began moving auth functions into it

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const jwt = require('jsonwebtoken')
 const mwConfig = require('./data/mwConfig')
 const db = require('./data/dbConfig.js')
 
+const { authenticate } = require('./auth/authenticate')
+
 const PORT = 5200
 const server = express()
 server.use(express.json())
@@ -12,101 +14,103 @@ server.use(express.json())
 mwConfig(server)
 
 server.post('/register', (req, res) => {
-  const creds = req.body
-  const hash = bcrypt.hashSync(creds.password, 14)
-  creds.password = hash
-  db('users')
-    .insert(creds)
-    .then(ids => {
-      res.status(201).json(ids)
-    })
-    .catch(() => {
-      res.status(500).json({ error: 'Unable to register user.' })
-    })
+	const creds = req.body
+	const hash = bcrypt.hashSync(creds.password, 14)
+	creds.password = hash
+	db('users')
+		.insert(creds)
+		.then(ids => {
+			res.status(201).json(ids)
+		})
+		.catch(() => {
+			res.status(500).json({ error: 'Unable to register user.' })
+		})
 })
 
 function generateToken(user) {
-  const payload = {
-    userId: user.id,
-    username: user.username
-  }
+	const payload = {
+		username: user.username,
+		userId: user.id,
+		roles: ['admin', 'student',] //example: should come from database user.roles
+	}
 
-  const secret = process.env.JWT_SECRET
+	const secret = process.env.JWT_SECRET
 
-  const options = {
-    expiresIn: '10m'
-  }
-  return jwt.sign(payload, secret, options)
+	const options = {
+		expiresIn: '45m'
+	}
+	return jwt.sign(payload, secret, options)
 }
 
 server.post('/login', (req, res) => {
-  const creds = req.body
-  db('users')
-    .where({ username: creds.username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(creds.password, user.password)) {
-        //login is successful
-        //create token
-        const token = generateToken(user)
-        res
-          .status(200)
-          .json({ message: `${user.username} is logged in`, token })
-      } else {
-        res.status(401).json({ message: 'You shall not pass!' })
-      }
-    })
-    .catch(() =>
-      res.status(500).json({ message: 'Please try logging in again.' })
-    )
+	const creds = req.body
+	db('users')
+		.where({ username: creds.username })
+		.first()
+		.then(user => {
+			if (user && bcrypt.compareSync(creds.password, user.password)) {
+				//login is successful
+				//create token
+				const token = generateToken(user)
+				res
+					.status(200)
+					.json({ message: `${user.username} is logged in`, token })
+			} else {
+				res.status(401).json({ message: 'You shall not pass!' })
+			}
+		})
+		.catch(() =>
+			res.status(500).json({ message: 'Please try logging in again.' })
+		)
 })
 
-function protected(req, res, next) {
-  //the auth token is normally sent in the Authorization header
-  const token = req.headers.authorization
-  if (token) {
-    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
-      if (err) {
-        res.status(401).json({ message: 'invalid token' })
-      } else {
-        req.decodedToken = decodedToken
-        next()
-      }
-    })
-  } else {
-    res.status(401).json({ message: 'no token provided' })
-  }
+function checkRole(role) { //piece of mw
+	return function(req, res, next) {
+		if(req.decodedToken.roles.includes(role)) {
+			next()
+		} else {
+			res.status(403).json({ message: `I know who you are, but you need to be an ${role}`}) //403 i know who you are, but you don't have access
+		}	
+	}
 }
 
 //protect this endpoint so only logged in users can see the data
-server.get('/users', protected, (req, res) => {
-  //  add : async -----if uncomment the below code
-  // const users = await db('users')
-  db('users')
-    .select('id', 'username') //<----NEVER EVER SEND THE PASSWORD BACK TO THE CLIENT, THIS IS WHAT NOT TO DO!!!
-    .then(users => {
-      res.json({ users, decodedToken: req.decodedToken })
-    })
-    .catch(() => {
-      res.status(500).json({ message: 'You shall not pass!' })
-    })
+server.get('/users', authenticate, checkRole('admin'), (req, res) => {
+	db('users')
+		.select('id', 'username') //<----NEVER EVER SEND THE PASSWORD BACK TO THE CLIENT, THIS IS WHAT NOT TO DO!!!
+		.then(users => {
+			res.json({ users, decodedToken: req.decodedToken })
+		})
+		.catch(() => {
+			res.status(500).json({ message: 'You shall not pass!' })
+		})
 })
 
-server.get('/users/:id', protected, (req, res) => {
-  //  add : async -----if uncomment the below code
-  // const users = await db('users')
-  db('users')
-    .where({ id: req.params.id })
-    .first()
-    //   .select('id', 'username', 'password') //<----NEVER EVER SEND THE PASSWORD BACK TO THE CLIENT, THIS IS WHAT NOT TO DO!!!
-    .then(users => {
-      res.json(users)
-    })
-    .catch(() => {
-      res.status(500).json({ message: 'You shall not pass!' })
-    })
+server.get('/users/:id', authenticate, (req, res) => {
+	db('users')
+		.where({ id: req.params.id })
+		.first()
+		.then(user => {
+			res.json(user)
+		})
+		.catch(() => {
+			res.status(500).json({ message: 'You shall not pass!' })
+		})
+})
+
+//THIS ENDPOINT NOT TESTING AS IT SHOULD IN POSTMAN
+server.get('/users/me', authenticate, checkRole('student'),(req, res) => { //Luis CS15 1:45:00
+	db('users')
+		.where({ username: req.decodedToken.username })
+		.first()
+		.then(user => {
+			res.json(user)
+		})
+		.catch(() => {
+			res.status(500).json({ message: 'You shall not pass!' })
+		})
 })
 
 server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`)
+	console.log(`Listening on port ${PORT}`)
 })
