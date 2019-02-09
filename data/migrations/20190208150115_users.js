640104de944649727ce53f9d1bda5ce825217c04
Lorenzo Evans <lorenzo.evans94@gmail.com>
2019-02-08 20:29:27 -0500

    Added migration, knexfile, jwt, implementing token logic now.

---
+++ data/migrations/20190208150115_users.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+ return knex.schema.createTable('users', table => {
+  table.increments()
+  table.string('username')
+       .notNullable()
+  table.string('password')
+       .notNullable()
+  table.string('department')
+       .notNullable()
+ })
+};
+
+exports.down = function(knex, Promise) {
+ return knex.schema.dropTableIfExists('users')
+};
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const helmet = require("helmet")
-const logger = require("logger")
+const logger = require("morgan")
 const hash = require("bcryptjs")
+const knex = require("knex")
+const jwt = require('jsonwebtoken')
 const server = express()
 const port = process.env.port || 3492
+const config = require('./knexfile')
+const db = knex(config.development)
 
 server.use(
  helmet(),
@@ -11,6 +15,20 @@ server.use(
  express.json()
 )
 
+const gatekeeper = () => {
+}
+
+const makeToken = () => {
+ const payload = {
+  username: user.username
+ }
+ const secret = 'ollyollyoxenfree'
+ const options = {
+  expiresIn: '4h',
+  jwtid: '090909'
+ }
+ return jwt.sign(payload, secret, options)
+}
 
 server.post('/api/register', (req, res) => {
 
