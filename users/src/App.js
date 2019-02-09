d63b311366ab336aaa016c3a1d5abafc7588e116
Mitchell Robles <mitchellr92@live.com>
2019-02-08 21:18:05 -0800

    simple routes for home and list of users added

--- users/src/App.js
+++ users/src/App.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
+import { NavLink, Route } from 'react-router-dom';
+import Users from './users/Users.js';
 import './App.css';
+
+const Home = props => {
+  return (<div>
+    <h1>Home</h1>
+  </div>)
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        <div>Hello World!</div>  
+          <nav>
+            <NavLink to='/' exact>
+              Home
+            </NavLink>
+            {/* &nbsp; | &nbsp;
+            <NavLink to='/signin'>
+              Sign In
+             </NavLink> */}
+            &nbsp; | &nbsp;
+            <NavLink to='/users'>
+              Users
+            </NavLink>
+          </nav>
+          <main>
+            <Route path='/' component={Home} exact>
+
+            </Route>
+            {/* <Route path='/signin' component={Signin} exact>
+
+            </Route> */}
+            <Route path='/users' component={Users} exact>
+
+            </Route>
+          </main>
         </header>
       </div>
     );
