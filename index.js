5c0b753fb1fe749ed823c1c7685ccc6f90c2d679
Tommmy Collison <tommy@collison.ie>
2019-02-09 17:46:17 -0800

    adds login code, jokes successfully console logging

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const session = require('express-session')
 const db = require('./database/dbConfig.js');
 const jwt = require('jsonwebtoken')
 const secret = 'secret!';
-const PORT = 9876;
+
 
 const server = express();
 
@@ -55,7 +55,6 @@ function protected(req, res, next){
 function generateToken(user){
   const payload = {
     username: user.username,
-    roles: user.roles
   }
 
   const options = {
@@ -88,29 +87,6 @@ server.post('/api/register', (req, res) => {
 })
 
 
-
-// server.post('/api/register/', (req, res) => {
-//   const user = req.body;
-//   console.log(`Tommy's session:`, req.session)
-
-//   user.password = bcrypt.hashSync(user.password, 14)
-//   db('users').insert(user)
-//   .then(ids => {
-//     const id = ids[0];
-//     // find the user using the ID
-//     db('users')
-//     .where({ id })
-//     .first()
-//     .then(user => {
-//       const token = generateToken(user);
-//       res.status(201).json({id: user.id, token})
-//     })
-//     .catch(err => res.status(500).send(err))
-//   })
-//   .catch(err => {res.status(500).send(err);
-//   })
-// })
-
 server.post('/api/login', (req, res) => {
   // checking that username and password matches
   const credentials = req.body;
@@ -139,27 +115,6 @@ server.get('/api/users', protected, (req, res) => {
   .catch(error => res.send(error))
 });
 
-// protect this route, only authenticated users should see it
-// server.get('/api/users', (req, res) => {
-//   console.log(`[Users] Tommy's session:`, req.session)
-//   if (req.session && req.session.userId){
-//   db('users')
-//     .select('id', 'username')
-//     .then(users => {
-//       res.json(users);
-//     })
-//     .catch(err => res.send(err));
-//   } else {
-//     res.status(400).send('access denied')
-  // }
-
-  // db('users')
-  //   .select('id', 'username', 'password')
-  //   .then(users => {
-  //     res.json(users);
-  //   })
-  //   .catch(err => res.send(err));
-// });
 
 server.post('/api/logout', (req, res) => {
   req.session.destroy(error => {
