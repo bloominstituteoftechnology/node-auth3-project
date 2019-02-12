e6d69c63989ffec64d69814c8440e1a68a78ba23
cocoitali <cmburatto@gmail.com>
2019-02-11 23:06:08 -0700

    MVP : added cors to interact with react app

--- data/mwConfig.js
+++ data/mwConfig.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const helmet= require('helmet')
 const logger = require('morgan')
-// const cors = require('cors')
+const cors = require('cors')
 
 module.exports = server => {
-    server.use(express.json(),logger('tiny'), helmet()) //cors(),
+    server.use(express.json(),logger('tiny'), helmet(),cors())
 }
+//cors allows server to interact with a url that is different from that of the server
