673c26f58ccdae411594d4610d9b8bab8b1ed721
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2019-03-06 18:13:04 -0500

    added login and register to auth file

---
+++ auth/auth-router.js
@@ -0,0 +1,63 @@
+const authRouter = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken'); 
+
+// const secrets = require('../secret/secrets.js');
+const secret =
+  process.env.JWT_SECRET 
+
+const Users = require('../users/user-model');
+
+
+authRouter.post('/register', (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 12); 
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+authRouter.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user); 
+
+        
+        res.status(200).json({
+          message: `Welcome ${user.username}!, this is a token...`,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+    // roles: ['Student'],
+  };
+
+  const options = {
+    expiresIn: '1d',
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+
+module.exports = authRouter;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
-// const authRouter = require('../auth/auth-router.js');
+const authRouter = require('./auth/auth-router.js');
 // const usersRouter = require('../users/users-router.js');
 
 const server = express();
@@ -11,7 +11,7 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-// server.use('/api', authRouter);
+server.use('/api', authRouter);
 // server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
