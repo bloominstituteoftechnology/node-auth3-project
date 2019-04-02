de81fbc38e280fe7af6ba5b43739a1cac9f53ee0
Trip4077 <bj2336@email.vccs.edu>
2019-04-02 11:33:40 -0400

    server file created, middleware imported

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const helmet = reqiure('helmet');
+const logger = require('morgan');
+const cors = require('cors');
+
+const server = express();
+
+server.use(express.json(), helmet(), cors());
+server.use(logger('dev'));
+
+server.get('/', (req, res) => {
+    res.send("With Great Power, Comes Great Responsibility");
+});
+
+module.exports = server;
