64ab64a519daa07c8836302b761172447839907b
Kseniya Platonava <ksu2008@gmail.com>
2019-04-02 22:34:23 -0400

    Add Knex, Migration

---
+++ data/migrations/20190402223212_createAuth2Table.js
@@ -0,0 +1,16 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('users', function(tbl) {
+    tbl.increments();
+
+     tbl
+      .string('username', 128)
+      .notNullable()
+      .unique();
+    tbl.string('password', 128).notNullable();
+    tbl.string('department', 128).notNullable();
+  });
+};
+
+ exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
+};
