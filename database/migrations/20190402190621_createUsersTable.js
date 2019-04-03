19a05c2038bc49297278d47f0e12f1e60eb831bd
120356aa <yurlovandrew@gmail.com>
2019-04-02 19:08:32 -0700

    created users table

---
+++ database/migrations/20190402190621_createUsersTable.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', tbl => {
+    tbl.increments();
+    tbl.string('username', 128)
+      .unique()
+      .notNullable();
+    tbl.string('password', 128)
+      .notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
+};
