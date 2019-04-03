04be5bdf658c68b87603a5e1016a6806cc0c2841
Adam Mathieson <mathieson1222@gmail.com>
2019-04-03 13:46:36 -0700

    adds secrets.js and .post for registering user

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,5 +3,20 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const Users = require("../users/users-model");
+const secret = require("./secrets").jwtSecret;
+
+router.post("/register", (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
