f8191a004977df68d19f5f5c74a0c7defc4d128f
Adam Mathieson <mathieson1222@gmail.com>
2019-04-03 14:12:24 -0700

    adds login endpoint with jwt

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -19,4 +19,36 @@ router.post("/register", (req, res) => {
     });
 });
 
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(202).json({ message: `Welcome ${user.username}`, token });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+
 module.exports = router;
