ee894cde4cf5b2d134d64961f1a93b1d4dad32b4
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-04-04 15:19:18 -0400

    React app created

---
+++ test/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Route} from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <h1> Hello</h1>
+    );
+  }
+}
+
+export default App;
