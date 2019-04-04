51eddc6b8050a88218dc40ad3361e502d4e47a04
Trip4077 <bj2336@email.vccs.edu>
2019-04-04 00:19:22 -0400

    Login Component Complete, Token Stored

--- client/src/login/Login.js
+++ client/src/login/Login.js
@@ -1,11 +1,67 @@
 import React from 'react';
+import axios from 'axios';
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    handleInputChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+          })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        const endpoint = 'http://localhost:5050/api/auth/login';
+
+        axios.post(endpoint, this.state) 
+            .then(res => {
+                localStorage.setItem('token', res.data.token)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
     render() {
         return(
-            <div>
-                Login
-            </div>
+            <>
+                <h2>Login</h2>
+                <form onSubmit={this.handleSubmit}>
+                <div>
+                    <label htmlFor="username">Username</label>
+                    <input
+                        name="username"
+                        id="username"
+                        value={this.state.username}
+                        onChange={this.handleInputChange}
+                        type="text"
+                        />
+
+                    <label htmlFor="password">Password</label>            
+                    <input
+                        name="password"
+                        id="password"
+                        value={this.state.password}
+                        onChange={this.handleInputChange}
+                        type="text"
+                        />
+                </div>
+                
+                <div>
+                    <button type="submit">Login</button>
+                </div>   
+
+                </form>
+            </>
         )
     }
 }
