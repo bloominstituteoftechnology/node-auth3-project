3b8866296a03f5abd3c3d56592078e08607e8149
120356aa <yurlovandrew@gmail.com>
2019-04-04 19:24:26 -0700

    updated app js finished login

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
+import { Route, NavLink, withRouter } from 'react-router-dom';
+
 import './App.css';
+import Login from './components/login.js';
+import Users from './components/users.js';
 
 class App extends Component {
+  logout = () => {
+    localStorage.removeItem('jwt');
+    this.props.history.push('/login');
+  }
+
   render() {
     return (
       <div className="App">
+        <header>
+          <nav>
+            <NavLink to='/login'>Login</NavLink>
+            <NavLink to='/users'>Users</NavLink>
+            <button onClick={this.logout}>Logout</button>
+          </nav>
+        </header>
 
+        <main>
+          <Route path='/login' component={Login} />
+          <Route path='/users' component={Users} />
+        </main>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
---
+++ client/src/components/login.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import Axios from 'axios';
+
+class Login extends React.Component {
+  state = {
+    username: '',
+    password: '',
+  }
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const endpoint = 'http://localhost:5000/api/auth/login';
+
+    Axios
+      .post(endpoint, this.state)
+      .then(res => {
+        localStorage.setItem('jwt', res.data.token);
+        this.props.history.push('/users');
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Login</h2>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <input
+              name='username'
+              id='username'
+              value={this.state.username}
+              onChange={this.handleInputChange}
+              type='text'
+              placeholder='Username'
+            />
+            <br/>
+            <input
+              name='password'
+              id='password'
+              value={this.state.password}
+              onChange={this.handleInputChange}
+              type='text'
+              placeholder='Password'
+            />
+          </div>
+          <div>
+            <button type='submit'>Login</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
