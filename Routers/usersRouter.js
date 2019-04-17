5ce3775793d0b36cff8d60ca9364f39caa4aef4d
cferrell2 <cferrell12182@gmail.com>
2019-04-16 22:48:45 -0400

    Add .env file to store secret

--- Routers/usersRouter.js
+++ Routers/usersRouter.js
@@ -1,117 +1,125 @@
 //create router
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 //grab helper functions
-const usersDb = require('../data/helpers');
+const usersDb = require("../data/helpers");
 
 //import bcryptjs for hashing
-const bcrypt = require('bcryptjs');
-//import jsonwebtoken 
-const jwt = require('jsonwebtoken');
-
+const bcrypt = require("bcryptjs");
+//import jsonwebtoken
+const jwt = require("jsonwebtoken");
 
 //**TOKEN GENERATOR FUNCTION */
-const secret = 'williewonka';  //placed in global for use in verification middleware
-function tokenGenerator(user){
-    const payload = {
-        username: user.username,
-        department: user.department
-    }
-    
-    const options = {
-        expiresIn: '1h',
-        jwtid: '12345'  //jti
-    }
-    
-    return jwt.sign(payload, secret, options);
+const secret = process.env.JWT_SECRET; //placed in global for use in verification middleware
+function tokenGenerator(user) {
+  const payload = {
+    username: user.username,
+    //or subject: user.id
+    department: user.department
+    //...other data
+  };
+
+  const options = {
+    expiresIn: "1h",
+    jwtid: "12345" //jti
+  };
+
+  return jwt.sign(payload, secret, options);
 }
 
 //***VERIFICATION OF TOKEN MIDDLEWARE**** */
-function verifyUser(req, res, next){
-    //read token string from authorization header
-    //if user not logged in there will be no token
-    const token = req.headers.authorization;
+function verifyUser(req, res, next) {
+  //read token string from authorization header
+  //if user not logged in there will be no token
+  const token = req.headers.authorization;
 
-    if(token){
-        //verify the token
-        jwt.verify(token, secret, (err, decodedToken) =>{
-            if(err){
-                res.status(401).json({error: "Invalid Token"})
-            }else{
-                next();
-            }
-        })
-    } else{
-        res.status(401).json("No Token Provided")
-    }
-
+  if (token) {
+    //verify the token
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        //record the error event in Db or elsewhere
+        res.status(401).json({ error: "Invalid Token" });
+      } else {
+        //* MAKE THE PAYLOAD/DECODED TOKEN AVAILABLE TO REST OF API
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json("No Token Provided");
+  }
 }
 
 //**ROUTE HANDLERS/ENDPOINTS FOR APPLICATION */
 //REGISTER USER
-router.post('/register', (req, res) =>{
-    const credentials = req.body;
-    const hashedPW = bcrypt.hashSync(credentials.password, 14);
-    credentials.password = hashedPW;
+router.post("/register", (req, res) => {
+  const credentials = req.body;
+  const hashedPW = bcrypt.hashSync(credentials.password, 14);
+  credentials.password = hashedPW;
 
-    usersDb.addUser(credentials)
-    .then(ids =>{
-        const id = ids[0];
-        usersDb.getUserById(id)
-            .then(user =>{
-                //generate token
-                const token = tokenGenerator(user);
-                //pass token to client
-                res.status(201).json({userId: user.id, username:user.username,
-                                        department: user.department,token: token});
-            })
-            .catch(err =>{
-                res.status(500).json({error: 'Unable to add user'});
-            })
-        
+  usersDb
+    .addUser(credentials)
+    .then(ids => {
+      const id = ids[0];
+      usersDb
+        .getUserById(id)
+        .then(user => {
+          //generate token
+          const token = tokenGenerator(user);
+          //pass token to client
+          res.status(201).json({
+            userId: user.id,
+            username: user.username,
+            department: user.department,
+            token: token
+          });
+        })
+        .catch(err => {
+          res.status(500).json({ error: "Unable to add user" });
+        });
     })
-    .catch(err =>{
-        res.status(500).json({error: "Unable to add user"})
-    })
-})
+    .catch(err => {
+      res.status(500).json({ error: "Unable to add user" });
+    });
+});
 
 //LOGIN USER
-router.post('/login', (req, res) =>{
-    const credentials = req.body;
+router.post("/login", (req, res) => {
+  const credentials = req.body;
 
-    usersDb.getUserByUsername(credentials.username)
-    .then(user =>{
-        if(user && bcrypt.compareSync(credentials.password, user.password)){
-            //generate token
-            const token = tokenGenerator(user);
-            //pass token to client
-            res.status(200).json({userId: user.id, username:user.username,
-                                department: user.department,token: token})
-        }else{
-            res.status(401).json({error: "Unable to verify user"})
-        }
+  usersDb
+    .getUserByUsername(credentials.username)
+    .then(user => {
+      if (user && bcrypt.compareSync(credentials.password, user.password)) {
+        //generate token
+        const token = tokenGenerator(user);
+        //pass token to client
+        res.status(200).json({
+          userId: user.id,
+          username: user.username,
+          department: user.department,
+          token: token
+        });
+      } else {
+        res.status(401).json({ error: "Unable to verify user" });
+      }
     })
-    .catch(err =>{
-        res.status(500).json({error: "Unable to log in"})
-    })
-})
-
-
-
+    .catch(err => {
+      res.status(500).json({ error: "Unable to log in" });
+    });
+});
 
 //GET ALL USERS
-router.get('/users', verifyUser, (req,res) =>{
-    usersDb.getUsers()
-    .then(users =>{
-        res.status(200).json(users)
+router.get("/users", verifyUser, (req, res) => {
+  usersDb
+    .getUsers()
+    .then(users => {
+      res.status(200).json({ users: users });
     })
-    .catch(err =>{
-        res.status(500).json({error: "Unable to get users"})
-    })
-})
-
-
+    .catch(err => {
+      res.status(500).json({ error: "Unable to get users" });
+    });
+});
 
 module.exports = router;
-    
--- index.js
+++ index.js
@@ -1,23 +1,26 @@
+//import dotenv
+require("dotenv").config();
+
 //create express server
-const express = require('express');
+const express = require("express");
 const server = express();
 
 //3rd party & built in middleware
 server.use(express.json());
-const cors = require('cors');
+const cors = require("cors");
 server.use(cors());
 
 //test route to check server
-server.get('/', (req, res) =>{
-    res.json('Working!!');
-})
+server.get("/", (req, res) => {
+  res.json("Working!!");
+});
 
-//import route handlers 
-const usersRoutes = require('./Routers/usersRouter');
-server.use('/api/', usersRoutes);
+//import route handlers
+const usersRoutes = require("./Routers/usersRouter");
+server.use("/api/", usersRoutes);
 
 //Listener
 const PORT = 4000;
-server.listen(PORT, ()=>{
-    console.log(`Server up and running on port ${PORT}`);
-})
+server.listen(PORT, () => {
+  console.log(`Server up and running on port ${PORT}`);
+});
