0475339e11b006f660643524dc81b73710294b79
cferrell2 <cferrell12182@gmail.com>
2019-04-17 21:52:42 -0400

    Add logout method in App.js of client

--- authii-client/src/App.js
+++ authii-client/src/App.js
@@ -1,15 +1,12 @@
-import React, { Component } from 'react';
-import {Route, Link} from 'react-router-dom';
+import React, { Component } from "react";
+import { Route, Link, withRouter } from "react-router-dom";
 
-import Users from './components/Users';
-import Signin from './components/Signin';
-import Register from './components/Register';
-import './App.css';
+import Users from "./components/Users";
+import Signin from "./components/Signin";
+import Register from "./components/Register";
+import "./App.css";
 
 class App extends Component {
-
-
-
   render() {
     return (
       <div className="App">
@@ -21,17 +18,22 @@ class App extends Component {
             &nbsp;|&nbsp;
             <Link to="/users">User List</Link>
             &nbsp;|&nbsp;
+            <button onClick={this.logout}>Logout</button>
           </nav>
           <main>
-            <Route path="/register" component={Register}></Route>
-            <Route path="/signin" component={Signin}></Route>
-            <Route path="/users" component={Users}></Route>
-
+            <Route path="/register" component={Register} />
+            <Route path="/signin" component={Signin} />
+            <Route path="/users" component={Users} />
           </main>
         </header>
       </div>
     );
   }
+
+  logout = () => {
+    localStorage.removeItem("jwt");
+    this.props.history.push("/signin");
+  };
 }
 
-export default App;
+export default withRouter(App);
