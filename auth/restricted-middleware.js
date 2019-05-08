ade30711d4cc1b1bd754ca8c0f1d4b5b0a454127
Alexnadria Supriana <alexa9410@gmail.com>
2019-05-08 18:46:23 -0400

    Create restricted-middleware.js

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const secrets = require("../config/secrets.js");
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ You: "can't do that!" });
+      } else {
+        req.decodedJwt = decodedToken;
+        console.log("decoded token", req.decodedJwt);
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ You: "can't pass beyond this point!" });
+  }
+};
