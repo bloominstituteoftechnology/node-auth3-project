43700a2c6d62c43835a318c6a5989f20cb5b16d5
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-21 11:40:13 -0700

    endpoints skeleton

--- server.js
+++ server.js
@@ -3,9 +3,20 @@ const express = require("express");
 const server = express();
 
 server.use(express.json());
+server.use("/api", apiRouter);
 
+// =============== TEST ===============
 server.get("/", (req, res) => {
   res.send("Hello from GET /");
 });
+
+// =============== REGISTER ===============
+server.post("/api/register", (req, res) => {});
+
+// =============== LOGIN ===============
+server.post("/api/login", (req, res) => {});
+
+// =============== GET USERS ===============
+server.get("/api/users", (req, res) => {});
 
 module.exports = server;
