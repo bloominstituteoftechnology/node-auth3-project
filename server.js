bd6cc4cfbf7d5e99ce3b67db3f1c2aa3b68ab1f6
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-21 13:02:12 -0700

    implements post endpoint for login

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const UsersModel = require("./users-model");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const UsersModel = require("./users-model");
+const secret = require("./secret");
 
 const server = express();
 
@@ -33,9 +36,38 @@ server.post("/api/register", (req, res) => {
 });
 
 // =============== LOGIN ===============
-server.post("/api/login", (req, res) => {});
+server.post("/api/login", (req, res) => {
+  const { username, password } = req.body;
+
+  UsersModel.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && cbrypt.compareSync(password, user.password)) {
+        const token = getJwt(user);
+        res.status(200).json({ message: `Welcome ${user.username}`, token });
+      } else {
+        res.status(401).json({ message: "You shall not pass!" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "There was an error logging in." });
+    });
+});
 
 // =============== GET USERS ===============
 server.get("/api/users", (req, res) => {});
+
+// =============== JWT ===============
+function getJwt(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secret.jwtSecret, options);
+}
 
 module.exports = server;
