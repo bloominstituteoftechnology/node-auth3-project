d3e4506e82a7e4890fa1743198a86bff8576fe0e
Dave Vazquez <dmvaz.li@gmail.com>
2019-08-27 21:30:16 -0400

    updated file paths

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './database/auth.db3'
+      filename: './data/auth.db3'
     },
     pool: {
       afterCreate: (conn, done) => {
@@ -11,10 +11,10 @@ module.exports = {
       }
     },
     migrations: {
-      directory: './database/migrations'
+      directory: './data/migrations'
     },
     seeds: {
-      directory: './database/seeds'
+      directory: './data/seeds'
     }
   }
 };
