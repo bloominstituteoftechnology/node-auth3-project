4726dbc0b9af250672bba63ba68db8dfb5356cba
Dave Vazquez <dmvaz.li@gmail.com>
2019-08-27 23:04:37 -0400

    implemented authorize middleware using jwts

--- auth/authorize.js
+++ auth/authorize.js
@@ -1,1 +1,32 @@
-module.exports = () => {};
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const tokenHeader = req.headers.authorization;
+
+  if (tokenHeader) {
+    const [bearer, token] = tokenHeader.split(' ');
+    console.log(bearer, token);
+    if (bearer.toUpperCase() === 'BEARER' && token) {
+      jwt.verify(
+        token,
+        process.env.LOGIN_SECRET || 'sooperdoopersecret',
+        (err, decodedToken) => {
+          if (err) {
+            res
+              .status(401)
+              .json({ message: 'error verifying token', error: err });
+          } else {
+            req.decodedJWT = decodedToken;
+            next();
+          }
+        }
+      );
+    } else {
+      res
+        .status(401)
+        .json({ message: 'invalid scheme, or no token after scheme name.' });
+    }
+  } else {
+    res.status(401).json({ message: 'missing Authorization header' });
+  }
+};
