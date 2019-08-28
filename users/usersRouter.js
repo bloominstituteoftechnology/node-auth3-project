695e5405a5e31154c1c41ad252c5c7cff11d90dd
Dave Vazquez <dmvaz.li@gmail.com>
2019-08-27 23:05:15 -0400

    GET '/api/users' endpoint implemented

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -1,6 +1,17 @@
 const express = require('express');
-const Users = require('../data/db-config');
+const Users = require('./usersModel');
 
 const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await Users.find();
+    res.status(200).json({
+      users
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
