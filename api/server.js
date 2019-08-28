225aff5c1c45e94d3997130c653ec1640614c375
Dave Vazquez <dmvaz.li@gmail.com>
2019-08-27 21:49:36 -0400

    added imports for routes, mounted routes to uris

--- api/server.js
+++ api/server.js
@@ -5,12 +5,19 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 const server = express();
+const authorize = require('../auth/authorize');
+const authRouter = require('../auth/authRouter');
+const usersRouter = require('../users/usersRouter');
 
 // MIDDLEWARE
 server.use(express.json());
 server.use(helmet());
 server.use(morgan('dev'));
 server.use(cors());
+
+// ROUTERS
+server.use('/api/auth', authorize, authRouter);
+server.use('/api/users', usersRouter);
 
 // CUSTOM ERROR-HANDLING MIDDLEWARE
 server.use('/', (err, req, res, next) => {
