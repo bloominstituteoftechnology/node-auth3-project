689e41e57fab26d2a7ad43b7f52c9d67ffca5c3b
Dave Vazquez <dmvaz.li@gmail.com>
2019-09-01 11:51:40 -0400

    find() function now returns department column

--- users/usersModel.js
+++ users/usersModel.js
@@ -8,7 +8,7 @@ module.exports = {
 };
 
 function find() {
-  return db('users').select('id', 'username');
+  return db('users').select('id', 'username', 'department');
 }
 
 function findBy(filter) {
