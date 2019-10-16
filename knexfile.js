10fbcb0a80317045c03d35072d8a986e681b6301
zachary-peasley <zpman95@hotmail.com>
2019-10-16 15:56:12 -0500

    Add datebase, auth folder, and migrations

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,21 @@
-// Update with your config settings.
-
 module.exports = {
-
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
+    client: "sqlite3",
 
-  staging: {
-    client: 'postgresql',
+    useNullAsDefault: true,
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      filename: "./database/users.db3"
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
     },
     pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
     }
   }
-
 };
