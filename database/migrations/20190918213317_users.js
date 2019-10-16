9e2a09f5387f51daed24080ea49bfe4a7fb24e51
Mimi Hoang <miminhoang1@gmail.com>
2019-10-16 16:23:09 -0400

    MvP

--- database/migrations/20190918213317_users.js
+++ database/migrations/20190918213317_users.js
@@ -1,15 +1,14 @@
 exports.up = function(knex) {
-    return knex.schema.createTable('users', users => {
-      users.increments();
-  
-      users
-        .string('username', 128)
-        .notNullable()
-        .unique();
-      users.string('password', 128).notNullable();
-    });
-  };
-  
+    return knex.schema.createTable('users', tbl => {
+      tbl.increments();
+    tbl
+      .string("username", 128)
+      .unique()
+      .notNullable();
+    tbl.string("password", 128).notNullable();
+    tbl.string("department");
+  });
+};
   exports.down = function(knex, Promise) {
     return knex.schema.dropTableIfExists('users');
   };
