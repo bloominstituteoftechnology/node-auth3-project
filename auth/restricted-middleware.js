bc00961436bf01bf99d4eae86e480df08fae8515
zachary-peasley <zpman95@hotmail.com>
2019-10-16 16:42:31 -0500

    router, middleware, user-router

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const secrets = require('../secrets.js');
+
+module.exports = (req, res, next) => {
+    const token =req.headers.authorization;
+
+    if (token) {
+        jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+            if (err) {
+                res.status(401).json({ message: "you may not pass"})
+            } else {
+                req.user = {username: decodedToken.username};
+                next();
+            }
+        })
+    } else {
+        res.status(400).json({ message: "No credentials provide"})
+    }
+}
