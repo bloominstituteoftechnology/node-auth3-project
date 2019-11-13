1cb569755d4006206733f377f7c2bbe986c194e3
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-11-13 14:40:10 -0600

    model for users implemented

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
+const Users = require('../models/user-model.js')
 
+router.post('/register', (req,res) =>{
+    const user = req.body;
 
+    
+})
 
+router.post('/login', (req,res) =>{
 
-
-
+})
 
 module.exports = router;
