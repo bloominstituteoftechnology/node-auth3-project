03486e57ae5f8e1d65b295715a1a864deb878ca6
AceIsHuman <menoman1995@yahoo.com>
2019-12-10 20:51:05 -0600

    connect usersRouter

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const helmet = require('helmet');
+const usersRouter = require('./users/usersRouter.js');
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet);
+server.use('/api', usersRouter);
 
 module.exports = server;
