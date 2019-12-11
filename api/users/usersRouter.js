cf18657038d0066bdaaf1740a75efde23e6dfac3
AceIsHuman <menoman1995@yahoo.com>
2019-12-10 21:53:18 -0600

    create endpoint to login

--- api/users/usersRouter.js
+++ api/users/usersRouter.js
@@ -15,4 +15,25 @@ router.post('/register', async (req, res) => {
     }
 });
 
+// USER LOGIN
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const _user = await Users.findBy({ username });
+        if (_user && bcrypt.compareSync(password, _user.password)) {
+            const user = {
+                id: _user.id,
+                username: _user.username,
+                department: _user.department,
+                token: 'mytoken'
+            }
+            res.status(200).json(user);
+        } else {
+            res.status(401).json({ message: "You shall not pass!" });
+        }
+    } catch(error) {
+        res.status(401).json({ message: "You shall not pass!" });
+    }
+});
+
 module.exports = router;
