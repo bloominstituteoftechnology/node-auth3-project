4ae246626d22cc114a22f9e61c35e24573c5f533
CLYPR Technologies <cbanks@clypr.co>
2020-03-19 20:28:16 -0400

    completed usersModel and usersRouter and set up token

---
+++ api/users/usersRouter.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+
+const Users = require('./usersModel.js');
+const bcrypt = require('bcryptjs');
+
+// REGISTER A USER
+router.post('/register', async (req, res) => {
+    const user = req.body;
+    if (!user.username && !user.password && !user.department) 
+        return res.status(401).json({ message: "Provide a username, password, and a department." });
+    user.password = bcrypt.hashSync(user.password);
+
+    try {
+        const _user = await Users.insert(user);
+        res.status(201).json(_user);
+
+    } catch(error) {
+        res.status(400).json({ message: "This user already exists.", error });
+    }
+});
+
+// USER LOGIN
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const _user = await Users.findBy({ username });
+        if (_user && bcrypt.compareSync(password, _user.password)) {
+            const user = {
+                id: _user.id,
+                username: _user.username,
+                department: _user.department,
+                token: 'mytoken'
+            }
+            res.status(200).json(user);
+        } else {
+            res.status(401).json({ message: "Try again!" });
+        }
+    } catch(error) {
+        res.status(401).json({ message: "Try again!" });
+    }
+});
+
+module.exports = router; 
---
+++ utils/generateToken.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function generateToken(user) {
+    const payload = {
+        userid: user.id,
+        username: user.username,
+        department: user.department
+    };
+
+    const options = {
+        expiresIn: '1h'
+    };
+
+    return jwt.sign(payload, process.env.JWT_SECRET, options);
+} 
